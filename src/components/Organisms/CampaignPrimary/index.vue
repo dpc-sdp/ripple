ff6cbec5e0875e6490e13ee0683f526bef986d88
Alan <alan@salsadigital.com.au>
2018-07-02 14:23:57 +1000

    [SDPA-527] Implement fix for CampaignPrimary image not displaying on iPad. (#168)

--- src/components/Organisms/CampaignPrimary/index.vue
+++ src/components/Organisms/CampaignPrimary/index.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="rpl-campaign-primary">
     <a v-if="link" :href="link.url">
-      <svg v-if="$breakpoint.m && image" class="rpl-campaign-primary__image" width="699" height="411" viewBox="0 0 699 411" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+      <svg v-if="$breakpoint.m && image" class="rpl-campaign-primary__image" width="699" height="411" viewBox="0 0 699 411" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true">
         <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="699" height="411">
           <path d="M699 0L114.075 170.226L0 411H699V0Z" fill="white" />
         </mask>
         <g mask="url(#mask0)">
-          <image width="699" height="411" :href="image" />
+          <image width="699" height="411" :href="image" :xlink:href="image" />
         </g>
       </svg>
     </a>

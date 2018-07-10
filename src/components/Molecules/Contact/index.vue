b767fab95d28f216505c43337b6c10c49d952cef
salsasolange <30677308+salsasolange@users.noreply.github.com>
2018-07-10 11:32:18 +1000

    [SDPA-646] Update variables as required. (#180)

--- src/components/Molecules/Contact/index.vue
+++ src/components/Molecules/Contact/index.vue
@@ -65,8 +65,9 @@ export default {
   $rpl-contact-background: transparent;
   $rpl-contact-title-ruleset: ('l', 1.2em, 'bold');
   $rpl-contact-title-margin: 0 0 $rpl-space-4;
-  $rpl-contact-text-color: rpl_color('extra_dark_neutral');
-  $rpl-contact-link-color: rpl_color('extra_dark_neutral');
+  $rpl-contact-title-color: rpl_color('extra_dark_neutral') !default;
+  $rpl-contact-text-color: rpl_color('extra_dark_neutral') !default;
+  $rpl-contact-link-color: rpl_color('extra_dark_neutral') !default;
   $rpl-contact-name-ruleset: ('xs', 1.4em, 'medium');
   $rpl-contact-ruleset: ('xs', 1.4em, 'regular');
   $rpl-contact-department-ruleset: ('xs', 1.4em, 'regular');
@@ -95,6 +96,7 @@ export default {
 
     &__title {
       @include rpl_typography_ruleset($rpl-contact-title-ruleset);
+      color: $rpl-contact-title-color;
       margin: $rpl-contact-title-margin;
     }
 

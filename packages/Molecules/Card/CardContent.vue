f63ca9711cdf5644a77b29bbbc22fad646551711
Alan <alan@salsadigital.com.au>
2018-08-23 09:57:24 +1000

    Feature/sdpa 662 card navigation (#34)
    * [SDPA-662] Update Card Navigation and Card Content.
    
    * CardContent now has more configuration options.
    * CardNavigation now uses CardContent as it's base.
    * Added documentation and story for CardContent usage.
    
    * [SDPA-662] Update test snapshot.
    
    * [SDPA-662] Updated readme to include HTML.

--- packages/Molecules/Card/CardContent.vue
+++ packages/Molecules/Card/CardContent.vue
@@ -3,7 +3,10 @@
     class="rpl-card-content"
     :href="link.url"
     :class="{
-      'rpl-card-content--no-image': !image
+      'rpl-card-content--no-image': !image,
+      'rpl-card-content--has-border': (border && !image),
+      'rpl-card-content--default': (type === 'default'),
+      'rpl-card-content--simple': (type === 'simple')
     }"
     v-if="link"
   >
@@ -29,8 +32,10 @@ objectFitImages('img.rpl-card-content__image')
 export default {
   name: 'RplCardContent',
   props: {
+    link: Object,
     image: String,
-    link: Object
+    border: { type: Boolean, default: true },
+    type: { type: String, default: 'default' }
   },
   components: {
     RplLink,
@@ -49,23 +54,34 @@ export default {
   @import "scss/card";
 
   $rpl-card-content-border-color: rpl_color('mid_neutral_1') !default;
-  $rpl-card-content-border: 1px solid $rpl-card-content-border-color !default;
+  $rpl-card-content-border-width: 1px !default;
+  $rpl-card-content-border: $rpl-card-content-border-width solid $rpl-card-content-border-color !default;
   $rpl-card-content-border-radius: rem(4px) !default;
   $rpl-card-content-background: rpl_color('white') !default;
   $rpl-card-content-no-image-padding: (rem(56px) - $rpl-card-vertical-padding) 0 0 0 !default;
   $rpl-card-content-no-image-background-image: rpl_gradient('decorative_gradient') !default;
-  $rpl-card-content-details-padding-xs: $rpl-card-vertical-padding $rpl-component-padding-xs 0 !default;
-  $rpl-card-content-details-padding-s: $rpl-card-vertical-padding $rpl-component-padding-s 0 !default;
-  $rpl-card-content-details-padding-m: $rpl-card-vertical-padding $rpl-card-horizontal-padding-m 0 !default;
-  $rpl-card-content-link-ruleset: ('xs', 1em, 'semibold') !default;
+  $rpl-card-content-default-link-ruleset: ('xs', 1em, 'semibold') !default;
+  $rpl-card-content-simple-link-ruleset: (
+    'xs': ('xs', 1em, 'semibold'),
+    's': ('s', 1em, 'semibold')
+  ) !default;
   $rpl-card-content-link-color: rpl_color('extra_dark_neutral') !default;
   $rpl-card-content-link-color-hover: rpl_color('primary') !default;
-  $rpl-card-content-link-padding-xs: $rpl-space-3 $rpl-component-padding-xs !default;
-  $rpl-card-content-link-padding-s: $rpl-space-3 $rpl-component-padding-s !default;
-  $rpl-card-content-link-padding-m: $rpl-space-3 $rpl-card-horizontal-padding-m !default;
   $rpl-card-content-link-line-space-xs: $rpl-component-padding-xs !default;
   $rpl-card-content-link-line-space-s: $rpl-component-padding-s !default;
   $rpl-card-content-link-line-space-m: $rpl-card-horizontal-padding-m !default;
+  $rpl-card-content-default-details-padding-xs: $rpl-card-vertical-padding $rpl-component-padding-xs 0 !default;
+  $rpl-card-content-default-details-padding-s: $rpl-card-vertical-padding $rpl-component-padding-s 0 !default;
+  $rpl-card-content-default-details-padding-m: $rpl-card-vertical-padding $rpl-card-horizontal-padding-m 0 !default;
+  $rpl-card-content-default-link-padding-xs: $rpl-space-3 $rpl-component-padding-xs !default;
+  $rpl-card-content-default-link-padding-s: $rpl-space-3 $rpl-component-padding-s !default;
+  $rpl-card-content-default-link-padding-m: $rpl-space-3 $rpl-card-horizontal-padding-m !default;
+  $rpl-card-content-simple-details-padding-xs: ($rpl-space * 6) ($rpl-space * 6) 0 !default;
+  $rpl-card-content-simple-details-padding-s: ($rpl-space * 8) ($rpl-space * 8) 0 !default;
+  $rpl-card-content-simple-details-padding-m: ($rpl-space * 8) ($rpl-space * 8) 0 !default;
+  $rpl-card-content-simple-link-padding-xs: $rpl-space-3 ($rpl-space * 6) ($rpl-space * 6) ($rpl-space * 6) !default;
+  $rpl-card-content-simple-link-padding-s: $rpl-space-3 ($rpl-space * 8) ($rpl-space * 8) ($rpl-space * 8) !default;
+  $rpl-card-content-simple-link-padding-m: $rpl-space-3 ($rpl-space * 8) ($rpl-space * 8) ($rpl-space * 8) !default;
 
   .rpl-card-content {
     $root: &;
@@ -76,17 +92,7 @@ export default {
     box-sizing: border-box;
     border-bottom: $rpl-card-content-border;
     background-color: $rpl-card-content-background;
-    @include rpl_breakpoint('m') {
-      border-radius: $rpl-card-content-border-radius;
-      border: $rpl-card-content-border;
-      height: rem(460px);
-    }
-    @include rpl_breakpoint('l') {
-      height: rem(500px);
-    }
-    @include rpl_breakpoint('xl') {
-      height: rem(460px);
-    }
+    border: $rpl-card-content-border;
 
     &:hover,
     &:focus {
@@ -101,8 +107,30 @@ export default {
       }
     }
 
-    &--no-image {
-      padding: $rpl-card-content-no-image-padding;
+    &--default {
+      border-width: 0;
+      @include rpl_breakpoint('m') {
+        border-radius: $rpl-card-content-border-radius;
+        border-width: $rpl-card-content-border-width;
+        height: rem(460px);
+      }
+      @include rpl_breakpoint('l') {
+        height: rem(500px);
+      }
+      @include rpl_breakpoint('xl') {
+        height: rem(460px);
+      }
+
+      &#{$root}--no-image {
+        padding: $rpl-card-content-no-image-padding;
+      }
+    }
+
+    &--simple {
+      border-radius: $rpl-card-content-border-radius;
+    }
+
+    &--has-border {
       &:before {
         content: '';
         position: absolute;
@@ -143,46 +171,72 @@ export default {
     }
 
     &__details {
-      padding: $rpl-card-content-details-padding-xs;
-      @include rpl_breakpoint('s') {
-        padding: $rpl-card-content-details-padding-s;
+      #{$root}--default & {
+        padding: $rpl-card-content-default-details-padding-xs;
+        @include rpl_breakpoint('s') {
+          padding: $rpl-card-content-default-details-padding-s;
+        }
+        @include rpl_breakpoint('m') {
+          padding: $rpl-card-content-default-details-padding-m;
+        }
       }
-      @include rpl_breakpoint('m') {
-        padding: $rpl-card-content-details-padding-m;
+
+      #{$root}--simple & {
+        padding: $rpl-card-content-simple-details-padding-xs;
+        @include rpl_breakpoint('s') {
+          padding: $rpl-card-content-simple-details-padding-s;
+        }
+        @include rpl_breakpoint('m') {
+          padding: $rpl-card-content-simple-details-padding-m;
+        }
       }
     }
 
     &__link {
-      @include rpl_typography_ruleset($rpl-card-content-link-ruleset);
       color: $rpl-card-content-link-color;
-      padding: $rpl-card-content-link-padding-xs;
       align-self: flex-end;
       display: block;
       position: relative;
       width: 100%;
-      @include rpl_breakpoint('s') {
-        padding: $rpl-card-content-link-padding-s;
-      }
-      @include rpl_breakpoint('m') {
-        padding: $rpl-card-content-link-padding-m;
-      }
 
-      &::before {
-        content: '';
-        background-color: $rpl-card-content-border-color;
-        height: 1px;
-        display: block;
-        position: absolute;
-        top: 0;
-        left: $rpl-card-content-link-line-space-xs;
-        right: $rpl-card-content-link-line-space-xs;
+      #{$root}--default & {
+        @include rpl_typography_ruleset($rpl-card-content-default-link-ruleset);
+        padding: $rpl-card-content-default-link-padding-xs;
         @include rpl_breakpoint('s') {
-          left: $rpl-card-content-link-line-space-s;
-          right: $rpl-card-content-link-line-space-s;
+          padding: $rpl-card-content-default-link-padding-s;
         }
         @include rpl_breakpoint('m') {
-          left: $rpl-card-content-link-line-space-m;
-          right: $rpl-card-content-link-line-space-m;
+          padding: $rpl-card-content-default-link-padding-m;
+        }
+
+        &::before {
+          content: '';
+          background-color: $rpl-card-content-border-color;
+          height: 1px;
+          display: block;
+          position: absolute;
+          top: 0;
+          left: $rpl-card-content-link-line-space-xs;
+          right: $rpl-card-content-link-line-space-xs;
+          @include rpl_breakpoint('s') {
+            left: $rpl-card-content-link-line-space-s;
+            right: $rpl-card-content-link-line-space-s;
+          }
+          @include rpl_breakpoint('m') {
+            left: $rpl-card-content-link-line-space-m;
+            right: $rpl-card-content-link-line-space-m;
+          }
+        }
+      }
+
+      #{$root}--simple & {
+        @include rpl_typography_ruleset($rpl-card-content-simple-link-ruleset);
+        padding: $rpl-card-content-simple-link-padding-xs;
+        @include rpl_breakpoint('s') {
+          padding: $rpl-card-content-simple-link-padding-s;
+        }
+        @include rpl_breakpoint('m') {
+          padding: $rpl-card-content-simple-link-padding-m;
         }
       }
 

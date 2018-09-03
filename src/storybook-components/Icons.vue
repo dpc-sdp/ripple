cff8df6de1ff448b0a763cc49c7604ac945f56b1
Alan <alan@salsadigital.com.au>
2018-09-03 11:11:23 +1000

    [SDPA-880] Updated: Icon Library to display as grid.

--- src/storybook-components/Icons.vue
+++ src/storybook-components/Icons.vue
@@ -1,10 +1,10 @@
 <template>
-  <table class="icons">
-    <tr v-for="(icon, index) in icons" :key="index">
-      <td><rpl-icon :symbol="icon" :color="color" :size="size" /></td>
-      <td><code>{{ icon }}</code></td>
-    </tr>
-  </table>
+  <ul class="icons">
+    <li v-for="(icon, index) in icons" :key="index">
+      <rpl-icon :symbol="icon" :color="color" :size="size" />
+      <code>{{ icon }}</code>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -24,13 +24,43 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  @import '~@dpc-sdp/ripple-global/scss/variables';
+  @import "~@dpc-sdp/ripple-global/style";
+
   .rpl-icon {
     display: block;
+    margin-left: auto;
+    margin-right: auto;
   }
   .icons {
-    td {
-      padding-right: $rpl-space-4;
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+
+    li {
+      display: flex;
+      flex-wrap: wrap;
+      align-content: space-between;
+      box-sizing: border-box;
+      text-align: center;
+      border: 1px solid rpl_color('mid_neutral_2');
+      border-radius: 4px;
+      padding: $rpl-space-2;
+      margin: 0 $rpl-space-2 $rpl-space-2 $rpl-space-2;
+      width: 100%;
+      @include rpl-breakpoint('s') {
+        width: calc(50% - #{$rpl-space-4} - 2px);
+      }
+      @include rpl-breakpoint('l') {
+        width: calc(25% - #{$rpl-space-4} - 2px);
+      }
+      @include rpl-breakpoint('xxl') {
+        width: calc(12.5% - #{$rpl-space-4} - 2px);
+      }
+    }
+
+    code {
+      width: 100%;
+      font-size: rpl-font-size('xxs');
     }
   }
 </style>

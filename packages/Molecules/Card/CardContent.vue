1a313625d9a5fce3e1ac5ee30edb707e6bbe6f29
Alan <alan@salsadigital.com.au>
2018-09-11 12:11:32 +1000

    [SDPA-792] Added CardContent updates from SDPA-614 branch.

--- packages/Molecules/Card/CardContent.vue
+++ packages/Molecules/Card/CardContent.vue
@@ -6,7 +6,9 @@
       'rpl-card-content--no-image': !image,
       'rpl-card-content--has-border': (border && !image),
       'rpl-card-content--default': (type === 'default'),
-      'rpl-card-content--simple': (type === 'simple')
+      'rpl-card-content--simple': (type === 'simple'),
+      'rpl-card-content--inline': (type === 'inline'),
+      'rpl-card-content--has-link-text': link.text
     }"
     v-if="link"
   >
@@ -15,8 +17,11 @@
     </div>
     <div class="rpl-card-content__details">
       <slot></slot>
+      <div class="rpl-card-content__link" v-if="link.text && (type === 'inline')">
+        <rpl-text-icon :text="link.text" :symbol="iconSymbol" color="primary" size="0.7" />
+      </div>
     </div>
-    <div class="rpl-card-content__link" v-if="link">
+    <div class="rpl-card-content__link" v-if="link.text && (type !== 'inline')">
       <rpl-text-icon :text="link.text" :symbol="iconSymbol" color="primary" size="0.7" />
     </div>
   </rpl-link>
@@ -65,23 +70,30 @@ export default {
     'xs': ('xs', 1em, 'semibold'),
     's': ('s', 1em, 'semibold')
   ) !default;
+  $rpl-card-content-inline-link-ruleset: ('xs', 1em, 'semibold') !default;
   $rpl-card-content-link-color: rpl_color('extra_dark_neutral') !default;
   $rpl-card-content-link-color-hover: rpl_color('primary') !default;
   $rpl-card-content-link-line-space-xs: $rpl-component-padding-xs !default;
   $rpl-card-content-link-line-space-s: $rpl-component-padding-s !default;
   $rpl-card-content-link-line-space-m: $rpl-card-horizontal-padding-m !default;
-  $rpl-card-content-default-details-padding-xs: $rpl-card-vertical-padding $rpl-component-padding-xs 0 !default;
-  $rpl-card-content-default-details-padding-s: $rpl-card-vertical-padding $rpl-component-padding-s 0 !default;
-  $rpl-card-content-default-details-padding-m: $rpl-card-vertical-padding $rpl-card-horizontal-padding-m 0 !default;
+  $rpl-card-content-default-details-padding-xs: $rpl-card-vertical-padding $rpl-component-padding-xs !default;
+  $rpl-card-content-default-details-padding-s: $rpl-card-vertical-padding $rpl-component-padding-s !default;
+  $rpl-card-content-default-details-padding-m: $rpl-card-vertical-padding $rpl-card-horizontal-padding-m !default;
   $rpl-card-content-default-link-padding-xs: $rpl-space-3 $rpl-component-padding-xs !default;
   $rpl-card-content-default-link-padding-s: $rpl-space-3 $rpl-component-padding-s !default;
   $rpl-card-content-default-link-padding-m: $rpl-space-3 $rpl-card-horizontal-padding-m !default;
-  $rpl-card-content-simple-details-padding-xs: ($rpl-space * 6) ($rpl-space * 6) 0 !default;
-  $rpl-card-content-simple-details-padding-s: ($rpl-space * 8) ($rpl-space * 8) 0 !default;
-  $rpl-card-content-simple-details-padding-m: ($rpl-space * 8) ($rpl-space * 8) 0 !default;
+  $rpl-card-content-simple-details-padding-xs: ($rpl-space * 6) ($rpl-space * 6) !default;
+  $rpl-card-content-simple-details-padding-s: ($rpl-space * 8) ($rpl-space * 8) !default;
+  $rpl-card-content-simple-details-padding-m: ($rpl-space * 8) ($rpl-space * 8) !default;
   $rpl-card-content-simple-link-padding-xs: $rpl-space-3 ($rpl-space * 6) ($rpl-space * 6) ($rpl-space * 6) !default;
   $rpl-card-content-simple-link-padding-s: $rpl-space-3 ($rpl-space * 8) ($rpl-space * 8) ($rpl-space * 8) !default;
   $rpl-card-content-simple-link-padding-m: $rpl-space-3 ($rpl-space * 8) ($rpl-space * 8) ($rpl-space * 8) !default;
+  $rpl-card-content-inline-padding-xs: ($rpl-space * 6) ($rpl-space * 6) !default;
+  $rpl-card-content-inline-padding-s: ($rpl-space * 8) ($rpl-space * 8) !default;
+  $rpl-card-content-inline-padding-m: ($rpl-space * 8) ($rpl-space * 8) !default;
+  $rpl-card-content-inline-link-padding-xs: $rpl-space-3 0 0 0 !default;
+  $rpl-card-content-inline-link-padding-s: $rpl-space-3 0 0 0 !default;
+  $rpl-card-content-inline-link-padding-m: $rpl-space-3 0 0 0 !default;
 
   .rpl-card-content {
     $root: &;
@@ -130,6 +142,16 @@ export default {
       border-radius: $rpl-card-content-border-radius;
     }
 
+    &--inline {
+      border-width: 0;
+      @include rpl_breakpoint('m') {
+        flex-wrap: nowrap;
+        border-radius: $rpl-card-content-border-radius;
+        border-width: $rpl-card-content-border-width;
+        padding: $rpl-card-content-inline-padding-m;
+      }
+    }
+
     &--has-border {
       &:before {
         content: '';
@@ -144,14 +166,26 @@ export default {
 
     &__image-wrapper {
       width: 100%;
-      @include rpl_breakpoint('m') {
-        height: rem(200px);
+
+      #{$root}--default &,
+      #{$root}--simple & {
+        @include rpl_breakpoint('m') {
+          height: rem(200px);
+        }
+        @include rpl_breakpoint('l') {
+          height: rem(232px);
+        }
+        @include rpl_breakpoint('xl') {
+          height: rem(200px);
+        }
       }
-      @include rpl_breakpoint('l') {
-        height: rem(232px);
-      }
-      @include rpl_breakpoint('xl') {
-        height: rem(200px);
+
+      #{$root}--inline & {
+        @include rpl_breakpoint('m') {
+          margin-right: $rpl-space * 6;
+          width: 40%;
+          max-width: rem(304px);
+        }
       }
     }
 
@@ -159,14 +193,24 @@ export default {
       @include object_fit_image(cover);
       width: 100%;
       display: table;
-      @include rpl_breakpoint('m') {
-        height: rem(200px);
+
+      #{$root}--default &,
+      #{$root}--simple & {
+        @include rpl_breakpoint('m') {
+          height: rem(200px);
+        }
+        @include rpl_breakpoint('l') {
+          height: rem(232px);
+        }
+        @include rpl_breakpoint('xl') {
+          height: rem(200px);
+        }
       }
-      @include rpl_breakpoint('l') {
-        height: rem(232px);
-      }
-      @include rpl_breakpoint('xl') {
-        height: rem(200px);
+
+      #{$root}--inline & {
+        @include rpl_breakpoint('m') {
+          border-radius: $rpl-card-content-border-radius;
+        }
       }
     }
 
@@ -188,6 +232,27 @@ export default {
         }
         @include rpl_breakpoint('m') {
           padding: $rpl-card-content-simple-details-padding-m;
+        }
+      }
+
+      #{$root}--inline & {
+        padding: $rpl-card-content-default-details-padding-xs;
+        @include rpl_breakpoint('s') {
+          padding: $rpl-card-content-default-details-padding-s;
+        }
+        @include rpl_breakpoint('m') {
+          padding: 0;
+          width: 60%;
+        }
+      }
+
+      #{$root}--has-link-text & {
+        padding-bottom: 0;
+        @include rpl_breakpoint('s') {
+          padding-bottom: 0;
+        }
+        @include rpl_breakpoint('m') {
+          padding-bottom: 0;
         }
       }
     }
@@ -237,6 +302,17 @@ export default {
         }
         @include rpl_breakpoint('m') {
           padding: $rpl-card-content-simple-link-padding-m;
+        }
+      }
+
+      #{$root}--inline & {
+        @include rpl_typography_ruleset($rpl-card-content-inline-link-ruleset);
+        padding: $rpl-card-content-inline-link-padding-xs;
+        @include rpl_breakpoint('s') {
+          padding: $rpl-card-content-inline-link-padding-s;
+        }
+        @include rpl_breakpoint('m') {
+          padding: $rpl-card-content-inline-link-padding-m;
         }
       }
 

d2c9046dbd09e0cf2042be49dd74a3088d16c556
Alan <alan@salsadigital.com.au>
2018-09-20 10:40:38 +1000

    [SDPA-971] Added element-ui date picker.

--- packages/Molecules/Form/fields/fieldRpldatepicker.vue
+++ packages/Molecules/Form/fields/fieldRpldatepicker.vue
@@ -1,29 +1,43 @@
 <template>
   <div class="rpl-date-picker">
-    <date-picker
-      lang="en-au"
+    <el-date-picker
+      v-if="schema.range"
       v-model="date"
-      :range="schema.range"
-      format="DD/MM/YYYY"
-      rangeSeparator="-"
+      type="daterange"
+      format="dd/MM/yyyy"
+      range-separator="To"
+      start-placeholder="Start date"
+      end-placeholder="End date"
       @change="dateChange"
     >
-      <template slot="calendar-icon"><rpl-icon symbol="calendar" color="primary" aria-label="Toggle Calendar"/></template>
-      <template slot="mx-clear-icon"><rpl-icon symbol="close" color="primary" aria-label="Clear date"/></template>
-    </date-picker>
+      <template slot="sidebar">Woo</template>
+    </el-date-picker>
+    <el-date-picker
+      v-if="!schema.range"
+      v-model="date"
+      type="date"
+      format="dd/MM/yyyy"
+      placeholder="Pick a date"
+      @change="dateChange"
+    ></el-date-picker>
   </div>
 </template>
 
 <script>
 import { abstractField } from 'vue-form-generator'
 import moment from 'moment'
-import DatePicker from 'vue2-datepicker'
 import RplIcon from '@dpc-sdp/ripple-icon'
+import { DatePicker } from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/en'
+import locale from 'element-ui/lib/locale'
+
+// configure language
+locale.use(lang)
 
 export default {
   mixins: [abstractField],
   components: {
-    DatePicker,
+    'ElDatePicker': DatePicker,
     RplIcon
   },
   data () {
@@ -49,24 +63,145 @@ export default {
 @import "~@dpc-sdp/ripple-global/style";
 @import "../scss/form";
 
-.rpl-date-picker {
-  .mx-input-append {
-    display: flex;
-    align-items: center;
-    background-color: $rpl-form-element-bg-color;
-    cursor: pointer;
-  }
+.el-picker-panel {
+  $root: &;
+  background: rpl-color('white');
+  border: 1px solid #0052c2;
+  border-radius: 4px;
+  padding: 20px;
 
-  .mx-input-wrapper:hover {
-    .mx-clear-wrapper {
+  &.el-date-range-picker {
+    #{$root}__body {
       display: flex;
+      flex-wrap: nowrap;
     }
-  }
 
-  .mx-datepicker {
-    width: auto;
-    display: block;
+    #{$root}__content {
+      &:nth-child(2) {
+        margin-left: $rpl-space * 5;
+      }
+    }
   }
 }
 
+.el-date-picker__header,
+.el-date-range-picker__header {
+  color: rpl-color('dark_neutral');
+
+  button {
+    border: 0;
+    background-color: transparent;
+    width: 26px;
+    height: 26px;
+    padding: 0;
+  }
+
+  [class*=arrow-left] {
+    float: left;
+  }
+
+  [class*=arrow-right] {
+    float: right;
+  }
+}
+
+.el-icon-d-arrow-left {
+  &:before {
+    content: '<<';
+  }
+}
+
+.el-icon-arrow-left {
+  &:before {
+    content: '<';
+  }
+}
+
+.el-icon-arrow-right {
+  &:before {
+    content: '>';
+  }
+}
+
+.el-icon-d-arrow-right {
+  &:before {
+    content: '>>';
+  }
+}
+
+.el-date-picker__header {
+  margin-bottom: $rpl-space-4;
+  text-align: center;
+}
+
+.el-date-range-picker__header {
+  margin-bottom: $rpl-space-4;
+  div {
+    text-align: center;
+    margin-left: 26px * 2;
+    margin-right: 26px * 2;
+  }
+}
+
+th {
+  text-align: center;
+  color: rpl-color('dark_neutral');
+  opacity: 0.65;
+  font-weight: normal;
+}
+
+.el-date-editor--daterange {
+  display: flex;
+  align-items: center;
+}
+
+.el-range-separator {
+  margin: 0 $rpl-space-2;
+}
+
+.el-date-table__row {
+  td {
+    width: 34px + 16px;
+    height: 34px;
+    text-align: center;
+    color: rpl-color('dark_neutral');
+
+    div {
+      margin: auto;
+      height: 34px;
+      width: 34px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+
+    &.in-range {
+      background-color: rpl-color('light_neutral');
+      color: rpl-color('extra_dark_neutral');
+    }
+
+    &.prev-month,
+    &.next-month {
+      color: rpl-color('mid_neutral_1');
+    }
+
+    &.start-date {
+      background-image: linear-gradient(to right, rpl-color('white') 0%, rpl-color('white') 49.9%, rpl-color('light_neutral') 50%, rpl-color('light_neutral') 100%);
+    }
+
+    &.end-date {
+      background-image: linear-gradient(to right, rpl-color('light_neutral') 0%, rpl-color('light_neutral') 49.9%, rpl-color('white') 50%, rpl-color('white') 100%);
+    }
+
+    &.start-date,
+    &.end-date,
+    &.current {
+      div {
+        color: rpl-color('white');
+        border-radius: 100%;
+        background-image: rpl-gradient('primary_gradient_90');
+      }
+    }
+  }
+}
 </style>

06a846e78a92b3cc6053e3f490fd87ad40d29490
Alan <alan@salsadigital.com.au>
2018-09-26 11:16:33 +1000

    Feature/component generate (#87)
    * Added script to generate a new component.
    
    * Remove commented out code.
    
    * Added contribution documentation.
    
    * Snake case fix to components with > 1 space.
    
    * Rename to 'Component' to 'Package'.
    
    * Updated documentation.

---
+++ scripts/generate-package/stories_template.js
@@ -0,0 +1,22 @@
+import { storiesOf } from '@storybook/vue'
+import { withReadme } from 'storybook-readme'
+import VueInfoAddon from 'storybook-addon-vue-info'
+
+import {
+  withKnobs
+} from '@storybook/addon-knobs/vue'
+
+import {{packageRippleName}} from './{{packageFileName}}.vue'
+import readme from './README.md'
+import { demoData } from '../../../src/storybook-packages/_data/demoData'
+
+storiesOf('{{packageAtomFolder}}/{{packageSpacelessName}}', module)
+  .addDecorator(VueInfoAddon)
+  .addDecorator(withKnobs)
+  .add('{{packageNaturalName}}', withReadme(readme, () => ({
+    packages: { {{packageRippleName}} },
+    template: `<{{packageClassName}} />`,
+    data () {
+      return demoData.{{packageDemoDataName}}()
+    }
+  })))

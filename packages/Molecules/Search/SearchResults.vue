ca3cbcff624a8a2ca80305940e9302ce328eccff
Guy Owen <guy.owen@dpc.vic.gov.au>
2018-11-23 15:53:56 +1100

    Added support for RplCardPromotion to Search Results.

--- packages/Molecules/Search/SearchResults.vue
+++ packages/Molecules/Search/SearchResults.vue
@@ -19,6 +19,12 @@
           :key="index">
           <rpl-card-event v-bind="searchResult" />
         </rpl-col>
+        <rpl-col cols="full" :colsBp="{m:6,l:4, xxxl:3}" class="rpl-search-results__item rpl-search-results__item--news rpl-component-gutter"
+          v-if="type === 'RplCardPromotion'"
+          v-for="(searchResult, index) of searchResults"
+          :key="index">
+          <rpl-card-promotion v-bind="searchResult" />
+        </rpl-col>
       </template>
       <div v-if="searchResults.length === 0" class="rpl-search-results__no-results-msg">
         {{ noResultsMsg }}

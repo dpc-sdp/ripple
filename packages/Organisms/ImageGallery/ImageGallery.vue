ce00d01ca58220e881e173d5ce626deba1be2d00
Tim Yao <31641325+tim-yao@users.noreply.github.com>
2018-12-06 09:31:10 +1100

    VICPOLCOR-135: Fix issue - expanded image not displayed correctly. (#149) (#153)
    * VICPOLCOR-135: Fix issue - expanded image not displayed correctly.
    
    * VICPOLCOR-135: Add same event to modal carousel to be sync.

--- packages/Organisms/ImageGallery/ImageGallery.vue
+++ packages/Organisms/ImageGallery/ImageGallery.vue
@@ -9,6 +9,7 @@
       :loop="true"
       :mouseDrag="false"
       :paginationEnabled="false"
+      @pageChange="onPageChange"
     >
       <slide v-for="(item, index) in galleryData" :key="index" class="rpl-image-gallery__thumbnail">
         <rpl-fitted-img class="rpl-image-gallery__thumbnail-image" :img-src="item.thumbnail" :img-alt="item.alt" />
@@ -35,6 +36,7 @@
           :loop="true"
           :mouseDrag="false"
           :paginationEnabled="false"
+          @pageChange="onPageChange"
         >
           <slide v-for="(item, index) in galleryData" :key="index" class="rpl-image-gallery__large">
             <div class="rpl-image-gallery__large-details-and-image">
@@ -110,6 +112,9 @@ export default {
     },
     prevSlide () {
       this.navTo = ((this.navTo > 0) ? (this.navTo - 1) : this.totalSlides)
+    },
+    onPageChange (slideNumber) {
+      this.navTo = slideNumber
     }
   }
 }

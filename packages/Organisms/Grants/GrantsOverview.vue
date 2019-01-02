291e554e6a952c837e332bc28e00833c9d970688
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2019-01-02 12:41:58 +1100

    [SDPA-1540] Adds better validation for grant funding level.

--- packages/Organisms/Grants/GrantsOverview.vue
+++ packages/Organisms/Grants/GrantsOverview.vue
@@ -44,20 +44,30 @@ export default {
       let list = []
       if (this.funding) {
         const calcFunding = (funding) => {
-          if (funding.from && funding.to) {
+          if (funding.from > 0 && funding.to > 0) {
             if (funding.from === funding.to) {
               return formatMoney(funding.from)
             } else {
               return `${formatMoney(funding.from)} - ${formatMoney(funding.to)}`
             }
+          } else if (funding.from > 0 || funding.to > 0) {
+            if (funding.from > 0) {
+              return formatMoney(funding.from)
+            }
+            if (funding.to > 0) {
+              return formatMoney(funding.to)
+            }
           }
         }
+        const fundingLevel = calcFunding(this.funding)
 
-        list.push({
-          symbol: 'dollar_negative',
-          size: '1.666',
-          text: calcFunding(this.funding)
-        })
+        if (fundingLevel) {
+          list.push({
+            symbol: 'dollar_negative',
+            size: '1.666',
+            text: fundingLevel
+          })
+        }
       }
       if (this.audience) {
         list.push({

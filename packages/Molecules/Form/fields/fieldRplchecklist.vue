00f68e986228f1eb8c3e6660d2ae99b99d4f79e0
dylankelly <dylan.kelly@dpc.vic.gov.au>
2019-01-31 09:35:39 +1100

    [SDPA-1789] Adds vue-on-click-outside to close rplchecklist on click out (#218)

--- packages/Molecules/Form/fields/fieldRplchecklist.vue
+++ packages/Molecules/Form/fields/fieldRplchecklist.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="rpl-checklist wrapper">
+  <div v-on-click-outside="onClickOutside" class="rpl-checklist wrapper">
     <!-- List Box -->
     <div v-if="schema.listBox" class="rpl-checklist__combobox form-control" :disabled="disabled">
       <div class="rpl-checklist__list">
@@ -44,9 +44,10 @@ import RplIcon from '@dpc-sdp/ripple-icon'
 import RplCheckbox from '../Checkbox.vue'
 import { isObject } from 'lodash'
 import { abstractField, schema } from 'vue-form-generator'
+import { mixin as onClickOutside } from 'vue-on-click-outside'
 
 export default {
-  mixins: [abstractField],
+  mixins: [abstractField, onClickOutside],
   components: {
     RplIcon,
     RplCheckbox
@@ -129,6 +130,9 @@ export default {
     },
     onExpandCombo () {
       this.comboExpanded = !this.comboExpanded
+    },
+    onClickOutside (event) {
+      this.comboExpanded = false
     },
     updateSize () {
       let str = this.schema.placeholder

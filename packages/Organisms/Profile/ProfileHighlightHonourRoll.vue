61c877abe93c82d41fb4974d6f3f2d7d17784239
Alan <alan@salsadigital.com.au>
2019-02-01 16:44:41 +1100

    [SDPA-1671] Added profile. (#215)
    * [SDPA-1671] Added Profile components and Profile template.
    
    * [SDPA-1671] Update package version and test.
    
    * [SDPA-1671] Added Mobile padding to Profile highlight.

---
+++ packages/Organisms/Profile/ProfileHighlightHonourRoll.vue
@@ -0,0 +1,55 @@
+<template>
+  <rpl-profile-highlight class="rpl-profile-highlight-honour-roll" :image="image">
+    <template slot="details">
+      <div v-if="inductedYear">
+        <span class="rpl-profile-highlight-honour-roll__label">Inducted:</span>
+        <span class="rpl-profile-highlight-honour-roll__value">{{ inductedYear }}</span>
+      </div>
+      <div v-if="category">
+        <span class="rpl-profile-highlight-honour-roll__label">Category:</span>
+        <span class="rpl-profile-highlight-honour-roll__value">{{ category }}</span>
+      </div>
+    </template>
+  </rpl-profile-highlight>
+</template>
+
+<script>
+import RplProfileHighlight from './ProfileHighlight.vue'
+
+export default {
+  name: 'RplProfileHighlightHonourRoll',
+  components: {
+    RplProfileHighlight
+  },
+  props: {
+    image: String,
+    inductedYear: String,
+    category: String
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "~@dpc-sdp/ripple-global/scss/settings";
+  @import "~@dpc-sdp/ripple-global/scss/tools";
+
+  $rpl-rpl-profile-highlight-honour-roll-label-ruleset: ('s', 1.5em, 'bold') !default;
+  $rpl-rpl-profile-highlight-honour-roll-value-ruleset: ('s', 1.5em, 'medium') !default;
+
+  .rpl-profile-highlight-honour-roll {
+    @include rpl_mobile_padding;
+
+    @include rpl_breakpoint('m') {
+      padding-left: 0;
+      padding-right: 0;
+    }
+
+    &__label {
+      @include rpl_typography_ruleset($rpl-rpl-profile-highlight-honour-roll-label-ruleset);
+    }
+
+    &__value {
+      @include rpl_typography_ruleset($rpl-rpl-profile-highlight-honour-roll-value-ruleset);
+    }
+  }
+</style>

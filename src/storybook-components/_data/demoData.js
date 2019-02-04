8570d4b482f14f5619959a8afde68a7ed9afcd28
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2019-02-04 16:35:07 +1100

    update story demodata

--- src/storybook-components/_data/demoData.js
+++ src/storybook-components/_data/demoData.js
@@ -1404,191 +1404,201 @@ const demoData = {
       },
 
       schema: {
-        groups: [
+        fields: [
           {
-            fields: [
-              {
-                type: 'input',
-                inputType: 'hidden',
-                model: 'hidden'
-              },
-              {
-                type: 'input',
-                inputType: 'text',
-                label: 'Text',
-                hint: 'This is a hint text',
-                required: true,
-                validator (value) {
-                  if (value === '') {
-                    return ['This field is required']
-                  }
-                  return []
-                },
-                placeholder: 'Enter some text...',
-                model: 'text'
-              },
-              {
-                type: 'input',
-                inputType: 'email',
-                label: 'Email',
-                hint: 'This is a hint text',
-                model: 'email'
-              },
-              {
-                type: 'input',
-                inputType: 'tel',
-                label: 'Tel',
-                model: 'tel'
-              },
-              {
-                type: 'input',
-                inputType: 'number',
-                label: 'Number',
-                placeholder: 'x10',
-                min: 0,
-                step: 10,
-                max: 100,
-                model: 'number'
-              },
-              {
-                type: 'radios',
-                label: 'Radio',
-                model: 'radio',
-                hint: 'This is a hint text',
-                values: [
-                  'Yes',
-                  'No'
-                ]
-              },
-              {
-                type: 'textArea',
-                label: 'text area',
-                model: 'textArea',
-                placeholder: 'Start typing...',
-                hint: 'This is a hint text',
-                rows: 4,
-                required: true,
-                validator (value) {
-                  if (value === '') {
-                    return ['This field is required']
-                  }
-                  return []
-                },
-                visible (model) {
-                  return model && model.was_this_page_helpful !== null
-                }
-              },
-              {
-                type: 'rpldatepicker',
-                range: true,
-                label: 'Date Range',
-                model: 'dateRange',
-                startPlaceholder: 'Start',
-                endPlaceholder: 'End'
-              },
-              {
-                type: 'rpldatepicker',
-                ranged: false,
-                label: 'Date',
-                model: 'date',
-                placeholder: 'Enter a date'
-              },
-              {
-                type: 'rplslider',
-                label: 'Set a funding range',
-                hint: '$0 - $80,000',
-                model: 'rangeslider',
-                step: 5000,
-                min: 0,
-                max: 80000,
-                prefix: '$',
-                default: true
-              },
-              {
-                type: 'rpldivider'
-              },
-              {
-                type: 'rplmarkup',
-                plugins: examplePlugins,
-                markup: '<h3>Markup field</h3> <article class="embedded-entity embedded-entity--media embedded-entity--media--document"><article class="media media--type-document media--view-mode-embedded"><div class="field field--name-field-media-file field--type-file field--label-hidden field__item"><span class="file file--mime-application-vnd-openxmlformats-officedocument-wordprocessingml-document file--x-office-document"><a href="https://nginx-php-content-vic-develop.lagoon.vicsdp.amazee.io/sites/default/files/2018-10/Detailed%20Guide%20on%20the%20mandatory%20IR%20management%20criteria.docx" aria-label=" Detailed Guide on the mandatory IR management criteria  File type: Word. Size: 75.22 KB." class="x-office-document tide-external-link" target="_blank"><span class="file--title"> Detailed Guide on the mandatory IR management criteria </span><span class="file--type">Word</span><span class="file--size">75.22 KB</span></a></span></div></article></article> <p>A paragraph of <strong>text</strong> with a <a href="https://vic.gov.au">link</a>.</p>'
-              },
-              {
-                type: 'rplcheckbox',
-                inlineLabel: 'Check box selected',
-                model: 'checkbox',
-                default: true
-              },
-              {
-                type: 'rplchecklist',
-                label: 'Multi-select list box',
-                model: 'checklistlistbox',
-                listBox: true,
-                hint: 'Implemented using rplchecklist with listBox: true',
-                placeholder: 'Select multiple topics',
-                values: [{value: 'topic_a', name: 'Topic A'}, {value: 'topic_b', name: 'Topic B'}, {value: 'topic_c', name: 'Topic C'}, {value: 'topic_d', name: 'Topic D'}]
-              },
-              {
-                type: 'rplchecklist',
-                label: 'Multi-select drop down',
-                validator (value) {
-                  if (Array.isArray(value) && value.length > 0) {
-                    return []
-                  }
-                  return ['Add a selection']
-                },
-                min: 1,
-                required: true,
-                model: 'checklistdropdown',
-                hint: 'Implemented using rplchecklist',
-                placeholder: 'Select multiple topics',
-                values: [{value: 'topic_a', name: 'Topic A'}, {value: 'topic_b', name: 'Topic B'}, {value: 'topic_c', name: 'Topic C'}, {value: 'topic_d', name: 'Topic D'}]
-              },
-              {
-                type: 'rplselect',
-                model: 'select',
-                required: true,
-                validator: ['required'],
-                label: 'Single-select drop down',
-                hint: 'Implemented using vue-multiselect',
-                placeholder: 'Select a single topic',
-                selectOptions: {
-                  trackBy: 'id',
-                  label: 'name',
-                  closeOnSelect: true,
-                  searchable: false,
-                  showLabels: false
-                },
-                values: [{id: 'topic_a', name: 'Topic A'}, {id: 'topic_b', name: 'Topic B'}, {id: 'topic_c', name: 'Topic C'}, {id: 'topic_d', name: 'Topic D'}]
-              },
-              {
-                type: 'rplselect',
-                model: 'multiselect',
-                required: true,
-                validator (value, field) {
-                  if (value && value.length >= field.min) {
-                    return []
-                  }
-                  return ['Add a selection']
-                },
-                min: 1,
-                label: 'Multi-select drop down',
-                hint: 'Implemented using vue-multiselect',
-                placeholder: 'Select several topics',
-                selectOptions: {
-                  trackBy: 'id',
-                  label: 'name',
-                  multiple: true,
-                  closeOnSelect: true,
-                  searchable: false,
-                  showLabels: false
-                },
-                values: [{id: 'topic_a', name: 'Topic A'}, {id: 'topic_b', name: 'Topic B'}, {id: 'topic_c', name: 'Topic C'}, {id: 'topic_d', name: 'Topic D'}]
-              },
-              {
-                type: 'rpldivider'
+            type: 'input',
+            inputType: 'hidden',
+            model: 'hidden'
+          },
+
+          {
+            type: 'input',
+            inputType: 'text',
+            label: 'Text',
+            hint: 'This is a hint text',
+            required: true,
+            validator (value) {
+              if (value === '') {
+                return ['This field is required']
               }
+              return []
+            },
+            placeholder: 'Enter some text...',
+            model: 'text'
+          },
+
+          {
+            type: 'input',
+            inputType: 'email',
+            label: 'Email',
+            hint: 'This is a hint text',
+            model: 'email'
+          },
+
+          {
+            type: 'input',
+            inputType: 'tel',
+            label: 'Tel',
+            model: 'tel'
+          },
+
+          {
+            type: 'input',
+            inputType: 'number',
+            label: 'Number',
+            placeholder: 'x10',
+            min: 0,
+            step: 10,
+            max: 100,
+            model: 'number'
+          },
+
+          {
+            type: 'radios',
+            label: 'Radio',
+            model: 'radio',
+            hint: 'This is a hint text',
+            values: [
+              'Yes',
+              'No'
             ]
           },
+
+          {
+            type: 'textArea',
+            label: 'text area',
+            model: 'textArea',
+            placeholder: 'Start typing...',
+            hint: 'This is a hint text',
+            rows: 4,
+            required: true,
+            validator (value) {
+              if (value === '') {
+                return ['This field is required']
+              }
+              return []
+            },
+            visible (model) {
+              return model && model.was_this_page_helpful !== null
+            }
+          },
+
+          {
+            type: 'rpldatepicker',
+            range: true,
+            label: 'Date Range',
+            model: 'dateRange',
+            startPlaceholder: 'Start',
+            endPlaceholder: 'End'
+          },
+
+          {
+            type: 'rpldatepicker',
+            ranged: false,
+            label: 'Date',
+            model: 'date',
+            placeholder: 'Enter a date'
+          },
+
+          {
+            type: 'rplslider',
+            label: 'Set a funding range',
+            hint: '$0 - $80,000',
+            model: 'rangeslider',
+            step: 5000,
+            min: 0,
+            max: 80000,
+            prefix: '$',
+            default: true
+          },
+
+          {
+            type: 'rpldivider'
+          },
+
+          {
+            type: 'rplmarkup',
+            plugins: examplePlugins,
+            markup: '<h3>Markup field</h3> <article class="embedded-entity embedded-entity--media embedded-entity--media--document"><article class="media media--type-document media--view-mode-embedded"><div class="field field--name-field-media-file field--type-file field--label-hidden field__item"><span class="file file--mime-application-vnd-openxmlformats-officedocument-wordprocessingml-document file--x-office-document"><a href="https://nginx-php-content-vic-develop.lagoon.vicsdp.amazee.io/sites/default/files/2018-10/Detailed%20Guide%20on%20the%20mandatory%20IR%20management%20criteria.docx" aria-label=" Detailed Guide on the mandatory IR management criteria  File type: Word. Size: 75.22 KB." class="x-office-document tide-external-link" target="_blank"><span class="file--title"> Detailed Guide on the mandatory IR management criteria </span><span class="file--type">Word</span><span class="file--size">75.22 KB</span></a></span></div></article></article> <p>A paragraph of <strong>text</strong> with a <a href="https://vic.gov.au">link</a>.</p>'
+          },
+
+          {
+            type: 'rplcheckbox',
+            inlineLabel: 'Check box selected',
+            model: 'checkbox',
+            default: true
+          },
+
+          {
+            type: 'rplchecklist',
+            label: 'Multi-select list box',
+            model: 'checklistlistbox',
+            listBox: true,
+            hint: 'Implemented using rplchecklist with listBox: true',
+            placeholder: 'Select multiple topics',
+            values: [{value: 'topic_a', name: 'Topic A'}, {value: 'topic_b', name: 'Topic B'}, {value: 'topic_c', name: 'Topic C'}, {value: 'topic_d', name: 'Topic D'}]
+          },
+
+          {
+            type: 'rplchecklist',
+            label: 'Multi-select drop down',
+            validator (value) {
+              if (Array.isArray(value) && value.length > 0) {
+                return []
+              }
+              return ['Add a selection']
+            },
+            min: 1,
+            required: true,
+            model: 'checklistdropdown',
+            hint: 'Implemented using rplchecklist',
+            placeholder: 'Select multiple topics',
+            values: [{value: 'topic_a', name: 'Topic A'}, {value: 'topic_b', name: 'Topic B'}, {value: 'topic_c', name: 'Topic C'}, {value: 'topic_d', name: 'Topic D'}]
+          },
+
+          {
+            type: 'rplselect',
+            model: 'select',
+            required: true,
+            validator: ['required'],
+            label: 'Single-select drop down',
+            hint: 'Implemented using vue-multiselect',
+            placeholder: 'Select a single topic',
+            selectOptions: {
+              trackBy: 'id',
+              label: 'name',
+              closeOnSelect: true,
+              searchable: false,
+              showLabels: false
+            },
+            values: [{id: 'topic_a', name: 'Topic A'}, {id: 'topic_b', name: 'Topic B'}, {id: 'topic_c', name: 'Topic C'}, {id: 'topic_d', name: 'Topic D'}]
+          },
+          {
+            type: 'rplselect',
+            model: 'multiselect',
+            required: true,
+            validator (value, field) {
+              if (value && value.length >= field.min) {
+                return []
+              }
+              return ['Add a selection']
+            },
+            min: 1,
+            label: 'Multi-select drop down',
+            hint: 'Implemented using vue-multiselect',
+            placeholder: 'Select several topics',
+            selectOptions: {
+              trackBy: 'id',
+              label: 'name',
+              multiple: true,
+              closeOnSelect: true,
+              searchable: false,
+              showLabels: false
+            },
+            values: [{id: 'topic_a', name: 'Topic A'}, {id: 'topic_b', name: 'Topic B'}, {id: 'topic_c', name: 'Topic C'}, {id: 'topic_d', name: 'Topic D'}]
+          }
+        ],
+        groups: [
           {
             legend: 'Event location (grouped fields)',
             styleClasses: ['rpl-fieldset--pad'],
@@ -1650,11 +1660,12 @@ const demoData = {
             ]
           },
           {
-            styleClasses: ['rpl-fieldset--pad'],
             fields: [
               {
-                type: 'submit',
-                buttonText: 'Submit',
+                type: 'rplsubmitloader',
+                buttonText: 'Submit (with animation)',
+                loading: false,
+                autoUpdate: true,
                 styleClasses: ['form-group--inline']
               },
               {
@@ -1662,79 +1673,16 @@ const demoData = {
                 buttonText: 'Clear search filters',
                 styleClasses: ['form-group--inline']
               }
-              return ['Add a selection']
-            },
-            min: 1,
-            required: true,
-            model: 'checklistdropdown',
-            hint: 'Implemented using rplchecklist',
-            placeholder: 'Select multiple topics',
-            values: [{value: 'topic_a', name: 'Topic A'}, {value: 'topic_b', name: 'Topic B'}, {value: 'topic_c', name: 'Topic C'}, {value: 'topic_d', name: 'Topic D'}]
-          },
-
-          {
-            type: 'rplselect',
-            model: 'select',
-            required: true,
-            validator: ['required'],
-            label: 'Single-select drop down',
-            hint: 'Implemented using vue-multiselect',
-            placeholder: 'Select a single topic',
-            selectOptions: {
-              trackBy: 'id',
-              label: 'name',
-              closeOnSelect: true,
-              searchable: false,
-              showLabels: false
-            },
-            values: [{id: 'topic_a', name: 'Topic A'}, {id: 'topic_b', name: 'Topic B'}, {id: 'topic_c', name: 'Topic C'}, {id: 'topic_d', name: 'Topic D'}]
-          },
-          {
-            type: 'rplselect',
-            model: 'multiselect',
-            required: true,
-            validator (value, field) {
-              if (value && value.length >= field.min) {
-                return []
-              }
-              return ['Add a selection']
-            },
-            min: 1,
-            label: 'Multi-select drop down',
-            hint: 'Implemented using vue-multiselect',
-            placeholder: 'Select several topics',
-            selectOptions: {
-              trackBy: 'id',
-              label: 'name',
-              multiple: true,
-              closeOnSelect: true,
-              searchable: false,
-              showLabels: false
-            },
-            values: [{id: 'topic_a', name: 'Topic A'}, {id: 'topic_b', name: 'Topic B'}, {id: 'topic_c', name: 'Topic C'}, {id: 'topic_d', name: 'Topic D'}]
-          },
-          {
-            type: 'submit',
-            buttonText: 'Submit'
-          },
-          {
-            type: 'rplsubmitloader',
-            buttonText: 'Submit (with animation)',
-            loading: false,
-            autoUpdate: true
-          },
-          {
-            type: 'rplclearform',
-            buttonText: 'Clear search filters'
+            ]
           }
         ]
       },
-
+      tag: 'rpl-fieldset',
       formOptions: {
         validateAfterLoad: false,
         validateAfterChanged: true
       },
-      tag: 'rpl-fieldset',
+
       formState: {}
     }
   }),

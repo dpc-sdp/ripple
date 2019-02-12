126d668b3634327bbc3989835ab3381802878299
Tim Yao <31641325+tim-yao@users.noreply.github.com>
2019-02-12 16:06:12 +1100

    [SDPA-1918] Added button event example with storybook layout changes (#245)

--- packages/Atoms/Button/stories.js
+++ packages/Atoms/Button/stories.js
@@ -1,6 +1,7 @@
 import { storiesOf } from '@storybook/vue'
-import { withReadme } from 'storybook-readme'
-import VueInfoAddon from 'storybook-addon-vue-info'
+import { withDocs } from 'storybook-readme'
+import VueInfoAddon, { withInfo } from 'storybook-addon-vue-info'
+import { action } from '@storybook/addon-actions'
 
 import {
   withKnobs
@@ -13,10 +14,26 @@ import { demoData } from '../../../src/storybook-components/_data/demoData'
 storiesOf('Atoms/Button', module)
   .addDecorator(VueInfoAddon)
   .addDecorator(withKnobs)
-  .add('Button', withReadme(readme, () => ({
+  .addDecorator(withDocs(readme))
+  .add('Button', () => ({
     components: { RplButton },
-    template: '<rpl-button :href="href" :theme="theme" :disabled="disabled">{{ content }}</rpl-button>',
+    template: `<rpl-button :href="href" :theme="theme" :disabled="disabled">{{ content }}</rpl-button>`,
     data () {
       return demoData.button()
+    }
+  }))
+
+storiesOf('Atoms/Button', module)
+  .addDecorator(withKnobs)
+  .add('Button with click action', withInfo({
+    summary: 'We can use <code>@click.native.prevent</code> to add custom action. Check "Action logger" to see the click log.'
+  })(() => ({
+    components: { RplButton },
+    template: `<rpl-button :href="href" :theme="theme" :disabled="disabled" @click.native.prevent="log">{{ content }}</rpl-button>`,
+    data () {
+      return demoData.button()
+    },
+    methods: {
+      log: action('Button got clicked.')
     }
   })))

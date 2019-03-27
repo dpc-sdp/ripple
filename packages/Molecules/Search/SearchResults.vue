c25cc92b81e09dbc5e55709a8f13a25e24eb65cf
Alan <alan@salsadigital.com.au>
2019-03-27 14:34:50 +1100

    [SDPA-2281] Allow spacing on pagination for back to top button.

--- packages/Molecules/Search/SearchResults.vue
+++ packages/Molecules/Search/SearchResults.vue
@@ -141,5 +141,14 @@ export default {
     &__error-msg {
       @include rpl_typography('heading_l');
     }
+
+    .rpl-pagination {
+      // Allow space (72px) for the back-to-top button in BaseLayout.
+      width: calc(100% - #{$rpl-space * 18});
+
+      @include rpl_breakpoint(m) {
+        width: 100%;
+      }
+    }
   }
 </style>

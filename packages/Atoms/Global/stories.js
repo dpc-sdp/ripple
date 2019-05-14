fb7df170ecbbcedb287d1ca3ee5e10a95ee2d38a
Alan <alan@salsadigital.com.au>
2019-05-14 16:37:06 +1000

    [SDPA-1097] Define line heights for each font-size with a background. (#328)
    * [SDPA-1097] Define line heights for each font-size with a background.
    * Added new variables to define spacing in line-height.
    * Updated Typography story to allow custom text (test with different text).
    * Added new story to test fonts with more options (size / line-height / weight / background).
    
    * [SDPA-1097] Update story to fix issue with Divider.

--- packages/Atoms/Global/stories.js
+++ packages/Atoms/Global/stories.js
@@ -2,7 +2,15 @@ import { storiesOf } from '@storybook/vue'
 import { withReadme } from 'storybook-readme'
 import VueInfoAddon from 'storybook-addon-vue-info'
 
+import {
+  text,
+  boolean,
+  selectV2,
+  withKnobs
+} from '@storybook/addon-knobs/vue'
+
 import SColors from './../../../src/storybook-components/Colors.vue'
+import SFonts from './../../../src/storybook-components/Fonts.vue'
 import STypography from './../../../src/storybook-components/Typography.vue'
 import SBreakpoints from './../../../src/storybook-components/Breakpoints.vue'
 import RplDivider from './components/Divider.vue'
@@ -36,9 +44,43 @@ storiesOf('Atoms/Global', module)
     }
   })))
 
+storiesOf('Atoms/Global', module)
+  .addDecorator(withKnobs)
+  .add('Fonts', withReadme(readme, () => ({
+    components: { SFonts },
+    template: '<s-fonts :size="size" :lineHeight="lineHeight" :weight="weight" :text="text" :background="background" />',
+    data () {
+      return {
+        size: selectV2('Size', [
+          'tera',
+          'xgiga',
+          'giga',
+          'mega',
+          'xl',
+          'l',
+          'm',
+          's',
+          'xs',
+          'xxs'
+        ], 'tera'),
+        lineHeight: text('Line Height', '1.1em'),
+        weight: selectV2('Weight', [
+          'regular',
+          'medium',
+          'semibold',
+          'bold'
+        ], 'bold'),
+        background: boolean('Background', false),
+        text: text('Titles', 'The quick brown fox jumps over the lazy dog')
+      }
+    }
+  })))
+
+storiesOf('Atoms/Global', module)
+  .addDecorator(withKnobs)
   .add('Typography', withReadme(readme, () => ({
     components: { STypography },
-    template: '<s-typography :samples="samples"/>',
+    template: '<s-typography :samples="samples" :text="text" :paragraph="paragraph"/>',
     data () {
       return {
         samples: [
@@ -56,11 +98,14 @@ storiesOf('Atoms/Global', module)
           'body_default',
           'body_small',
           'copy_extra_small'
-        ]
+        ],
+        text: text('Titles', 'The quick brown fox jumps over the lazy dog'),
+        paragraph: text('Paragraphs', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A vitae quod nobis earum saepe inventore rerum dicta voluptatem ullam iste rem deleniti odit culpa tempora dolor animi, non, amet ducimus.')
       }
     }
   })))
 
+storiesOf('Atoms/Global', module)
   .add('Breakpoints', withReadme(readme, () => ({
     components: { SBreakpoints },
     template: '<s-breakpoints :breakpoints="breakpoints"/>',
@@ -79,6 +124,7 @@ storiesOf('Atoms/Global', module)
     }
   })))
 
+storiesOf('Atoms/Global', module)
   .addDecorator(VueInfoAddon)
   .add('Divider', withReadme(readme, () => ({
     components: { RplDivider },

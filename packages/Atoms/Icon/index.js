c51cafa02de65268eba703f92b7adde26ea51253
Alan <alan@salsadigital.com.au>
2019-05-16 14:37:50 +1000

    Updated tests.

--- packages/Atoms/Icon/index.js
+++ packages/Atoms/Icon/index.js
@@ -2,8 +2,12 @@ import RplIcon from './Icon.vue'
 import RplTextIcon from './TextIcon.vue'
 import { addIconsToLibrary } from './icon-library'
 
-// Add default icons.
-addIconsToLibrary(require.context('./assets/img/', true, /\.svg$/))
+// In Jest there is no webpack require.context support, also we don't transform files.
+// Let's skip this part in tests.
+if (process.env.NODE_ENV !== 'test') {
+  // Add default icons.
+  addIconsToLibrary(require.context('./assets/img/', true, /\.svg$/))
+}
 
 export {RplIcon}
 export {RplTextIcon}

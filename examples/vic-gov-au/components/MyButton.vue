aea43943156384fb1afa74bb50f91ad27876de7a
Tim Yao <31641325+tim-yao@users.noreply.github.com>
2019-07-01 17:00:54 +1000

    Added vicpol form redirect into core (#405)
    * Added vicpol form redirect into core
    
    * poc for extends

---
+++ examples/vic-gov-au/components/MyButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>This is custom template if needed</div>
+</template>
+
+<script>
+import RplButton from '@dpc-sdp/ripple-button'
+export default {
+  extends: RplButton,
+  name: 'MyButton',
+  created: function () {
+    console.log('This works.')
+  }
+}
+</script>

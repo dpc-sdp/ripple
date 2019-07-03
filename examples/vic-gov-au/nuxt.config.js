10f21e05222d63eb4d70c9066841f2ce6dc64563
Tim Yao <tim.yao@dpc.vic.gov.au>
2019-07-03 16:12:47 +1000

    fixed wrong env setting

--- examples/vic-gov-au/nuxt.config.js
+++ examples/vic-gov-au/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   debug: process.env.DISPLAY_ERROR === '1' || false,
 
   serverMiddleware: [
-    process.env.CONTENT_API_AUTH_USER === '1' && '~/middleware/basic-auth'
+    process.env.BASIC_AUTH === '1' && '~/middleware/basic-auth'
   ].filter(i => i),
 
   /*

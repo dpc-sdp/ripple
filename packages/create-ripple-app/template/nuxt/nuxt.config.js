4e41ff259eaa42d47c6ab05087238c588b48cf7a
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2019-07-22 15:23:54 +1000

    Adds ability to define config from json file.

--- packages/create-ripple-app/template/nuxt/nuxt.config.js
+++ packages/create-ripple-app/template/nuxt/nuxt.config.js
@@ -84,24 +84,24 @@ export default {
     customFilters: tideFilters,
     // Tide submodules, 1 for enable, 0 for disable.
     modules: {
-      <% if (siteModule === 'yes') { %> site: 1,<%}%>
+      <% if (site === 'yes') { %> site: 1,<%}%>
       // Content types
-      <% if (pageModule === 'yes') { %> page: 1,<%}%>
-      <% if (landingPageModule === 'yes') { %> landingPage: 1,<%}%>
-      <% if (eventModule === 'yes') { %> event: 1,<%}%>
-      <% if (newsModule === 'yes') { %> news: 1,<%}%>
-      <% if (grantModule === 'yes') { %> grant: 1,<%}%>
-      <% if (profileModule === 'yes') { %> profile: 1,<%}%>
-      <% if (publicationModule === 'yes') { %> publication: 1,<%}%>
+      <% if (page === 'yes') { %> page: 1,<%}%>
+      <% if (landingPage === 'yes') { %> landingPage: 1,<%}%>
+      <% if (event === 'yes') { %> event: 1,<%}%>
+      <% if (news === 'yes') { %> news: 1,<%}%>
+      <% if (grant === 'yes') { %> grant: 1,<%}%>
+      <% if (profile === 'yes') { %> profile: 1,<%}%>
+      <% if (publication === 'yes') { %> publication: 1,<%}%>
       // Feature flags
-      <% if (mediaModule === 'yes') { %> media: 1,<%}%>
-      <% if (webformModule === 'yes') { %> webform: 1,<%}%>
-      <% if (searchModule === 'yes') { %> search: 1,<%}%>
-      <% if (monsidoModule === 'yes') { %> monsido: 1,<%}%>
-      <% if (authenticatedContentModule === 'yes') { %> authenticatedContent: 1,<%}%>
-      <% if (dataDrivenComponentModule === 'yes') { %> dataDrivenComponent: 1,<%}%>
-      <% if (alertModule === 'yes') { %> alert: 1,<%}%>
-      <% if (gtmModule === 'yes') { %> gtm: 1,<%}%>
+      <% if (media === 'yes') { %> media: 1,<%}%>
+      <% if (webform === 'yes') { %> webform: 1,<%}%>
+      <% if (search === 'yes') { %> search: 1,<%}%>
+      <% if (monsido === 'yes') { %> monsido: 1,<%}%>
+      <% if (authenticatedContent === 'yes') { %> authenticatedContent: 1,<%}%>
+      <% if (dataDrivenComponent === 'yes') { %> dataDrivenComponent: 1,<%}%>
+      <% if (alert === 'yes') { %> alert: 1,<%}%>
+      <% if (gtm === 'yes') { %> gtm: 1,<%}%>
     },
     gtm: {
       // Set Google Tag Manager ID here

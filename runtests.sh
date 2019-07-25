8772478e698f55a0f3aa883dc9547dc6e0debd07
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2019-07-25 14:36:10 +1000

    Adds variable run and test commands

--- runtests.sh
+++ runtests.sh
@@ -1,4 +1,14 @@
 for path in examples/*; do
   [ -d "${path}" ] || continue # if not a directory, skip
-  yarn run cross-env NODE_ENV=test start-server-and-test "'cd $path && yarn start:build'" 3000 "'cy:run-smoke --project ${path}/test/e2e'"
+  STARTSCRIPT=start:build
+  TESTSCRIPT=cy:run-smoke
+  if [ "$1" != "" ]; then
+    STARTSCRIPT=$1
+    echo "STARTSCRIPT = $STARTSCRIPT"
+  fi
+  if [ "$2" != "" ]; then
+    TESTSCRIPT=$2
+    echo "TESTSCRIPT = $TESTSCRIPT"
+  fi
+  yarn run cross-env NODE_ENV=test start-server-and-test "'cd $path && yarn $STARTSCRIPT'" 3000 "'$TESTSCRIPT --project ${path}/test/e2e'"
 done

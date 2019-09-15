50dcc44b5ee054771ebf785ba829c60e53aaa2d2
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2019-09-16 00:11:13 +1000

    remove delete step

--- scripts/cucumberconvertor.js
+++ scripts/cucumberconvertor.js
@@ -1,18 +1,11 @@
-const fs = require('fs')
 const cucumberJunitConvert = require('cucumber-junit-convert')
 const glob = require('glob-fs')({ gitignore: false })
 
-const tests = glob.readdirSync('./test-results/cucumber/*.json')
-
-tests.forEach(f => {
+const files = glob.readdirSync('./test-results/cucumber/*.json')
+files.forEach(f => {
   const filename = f.replace(/\.[^/.]+$/, '')
   cucumberJunitConvert.convert({
     inputJsonFile: `./${filename}.json`,
     outputXmlFile: `./${filename}.xml`
   })
-  try {
-    fs.unlinkSync(f)
-  } catch (err) {
-    console.error(err)
-  }
 })

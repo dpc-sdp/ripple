9df90ce91e74706e97e4c89c01b7d204e55e4fdc
MdNadimHossain <getovic@gmail.com>
2019-09-23 11:55:33 +1000

    [SDPA-3216] Updated unit test for html-utilities to test new special char.

--- packages/ripple-nuxt-tide/test/unit/html-utilities.test.js
+++ packages/ripple-nuxt-tide/test/unit/html-utilities.test.js
@@ -2,6 +2,6 @@ import htmlUtilities from '../../lib/core/html-utilities'
 
 describe('html utilities', () => {
   test(`should decode special characters`, async () => {
-    expect(htmlUtilities.decodeSpecialCharacters('&amp;&lt;&gt;&nbsp;')).toBe(`&<> `)
+    expect(htmlUtilities.decodeSpecialCharacters('&amp;&lt;&gt;&nbsp;&apos;&quot;&#039;')).toBe(`&<> '"'`)
   })
 })

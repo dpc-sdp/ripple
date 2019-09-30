ba8080d856fbd6865dad8232ecdbe942435c7979
MdNadimHossain <getovic@gmail.com>
2019-09-30 11:50:51 +1000

    [SDPA-3192] Added Wordwrap mixin and used in rpl_banner_title.

--- packages/components/Organisms/HeroBanner/HeroBanner.vue
+++ packages/components/Organisms/HeroBanner/HeroBanner.vue
@@ -180,6 +180,7 @@ export default {
     'xxxl': ('pos': 'right', 'v-offset': $rpl-hero-banner-background-graphic-v-offset-xxl),
     '2660px': ('pos': 'right')
   ) !default;
+  $rpl-hero-banner-title-word-wrap: break-word !default;
 
   @mixin rpl_position_background_graphic($vars) {
     $pos: map-get($vars, 'pos');
@@ -276,6 +277,7 @@ export default {
     &__title {
       @include rpl_typography_ruleset($rpl-hero-banner-title-typography-ruleset);
       @include rpl_text_color($rpl-hero-banner-title-color);
+      @include rpl_word_wrap($rpl-hero-banner-title-word-wrap);
       margin: 0;
 
       &--dark {

2aee25a19436bad2d75b945556c0dcc98e17d4b6
Tim Yao <31641325+tim-yao@users.noreply.github.com>
2019-10-01 16:42:50 +1000

    [SDPSUP-669] Added example for using Ripple in Vue.js(Not Nuxt) (#535)
    * [SDPSUP-669] Added example for using Vue.js(Not Nuxt)

---
+++ examples/vue-example-app/test/e2e/integration/smoke/ripple/step_definition.js
@@ -0,0 +1,17 @@
+/* global cy */
+
+import { Given, Then } from 'cypress-cucumber-preprocessor/steps'
+
+Given(`I visit the Vue router path {string}`, url => {
+  cy.visit(url, {
+    failOnStatusCode: false
+  })
+})
+
+Then(`the example ripple components should exist`, () => {
+  cy.get('.rpl-link').should('exist')
+  cy.get('.rpl-text-link').should('exist')
+  cy.get('svg.rpl-icon').should('exist')
+  cy.get('.rpl-card-navigation').should('exist')
+  cy.get('.rpl-button').should('exist')
+})

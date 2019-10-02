312b4f6af482e98a4945e3437598a374d51a1d33
Tim Yao <tim.yao@dpc.vic.gov.au>
2019-10-03 09:55:09 +1000

    Updated search code examples to make them working

--- examples/basic-examples/tide/modules/example-search/components/SearchSimple.vue
+++ examples/basic-examples/tide/modules/example-search/components/SearchSimple.vue
@@ -25,7 +25,6 @@
 </template>
 
 <script>
-import moment from 'moment'
 
 import { RplDivider } from '@dpc-sdp/ripple-global'
 import { RplSearchForm, RplSearchResults } from '@dpc-sdp/ripple-search'
@@ -33,7 +32,7 @@ import { RplSearchForm, RplSearchResults } from '@dpc-sdp/ripple-search'
 // Layout.
 import { RplRow, RplCol } from '@dpc-sdp/ripple-grid'
 import { RplPageLayout } from '@dpc-sdp/ripple-layout'
-import formData from './formdata.js'
+import formData from './formdata-simple.js'
 import { searchMixin } from '@dpc-sdp/ripple-nuxt-tide/modules/search'
 
 export default {
@@ -53,28 +52,20 @@ export default {
     const searchForm = await formData.getFormData(app.$tideSearch.setFilterOptions)
     return {
       sidebar: false,
-      searchComponent: 'RplCardEvent',
+      searchComponent: 'default',
       searchForm,
       searchOptions: {
         currentSiteOnly: true,
         defaultHits: false,
-        responseSize: 9,
+        responseSize: 10,
         qFields: [
           'body',
-          'field_event_details_event_locality',
-          'field_landing_page_summary',
-          'field_paragraph_body',
-          'field_paragraph_summary',
           'title'
         ],
         sFields: [
-          'field_event_category_name',
-          'field_event_date_end_value',
-          'field_event_date_start_value',
-          'field_event_details_event_locality',
-          'field_event_details_event_requirements_name',
+          'changed',
+          'created',
           'field_landing_page_summary',
-          'field_media_image_absolute_path',
           'field_node_primary_site',
           'title',
           'type',
@@ -87,33 +78,18 @@ export default {
     }
   },
   methods: {
-    getComputedFilters () {
-      let filterValues = this.$tideSearch.getFiltersValues(this.searchForm.filterForm)
-      // Test date filter based on start / end fields.
-      if (filterValues.field_event_date_end_value) {
-        const setFilterDate = moment(filterValues.field_event_date_end_value.values)
-        filterValues.field_event_date_end_value.values = setFilterDate.startOf('day').toISOString()
-        filterValues['field_event_date_start_value'] = {
-          operator: 'lte',
-          type: 'date',
-          values: setFilterDate.endOf('day').toISOString()
-        }
-      }
-      return filterValues
+    validDate: (date) => {
+      return typeof date === 'string'
     },
     mapSearchResults (source) {
-      let pSite = ''
-      if (source.field_node_primary_site) {
-        pSite = source.field_node_primary_site[0]
-      }
+      const site = this.$store.state.tide.siteData.drupal_internal__tid
+      let date = source.changed ? source.changed[0] : source.created[0]
       return {
         title: source.title[0] || '',
-        dateStart: source.field_event_date_start_value[0] || '',
-        dateEnd: source.field_event_date_end_value[0] || '',
-        location: source.field_event_details_event_locality[0] || '',
-        summary: typeof source.field_landing_page_summary !== 'undefined' ? this.truncateText(source.field_landing_page_summary[0]) : this.truncateText(source.body[0]),
-        image: source.field_media_image_absolute_path ? source.field_media_image_absolute_path[0] : '',
-        link: source.url && this.getLink(source.url, this.$store.state.tide.siteData.drupal_internal__tid, pSite, this.$store.state.tideSite.sitesDomainMap, { text: 'text', url: 'url' }, 'See event details')
+        link: source.url && this.getLink(source.url, site, source.field_node_primary_site, this.$store.state.tideSite.sitesDomainMap, { text: 'linkText', url: 'linkUrl' }, 'See event details'),
+        date: this.validDate(date) ? date : '',
+        description: typeof source.field_landing_page_summary !== 'undefined' ? this.truncateText(source.field_landing_page_summary[0]) : this.truncateText(source.body[0]),
+        tags: []
       }
     }
   }

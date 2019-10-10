35bd6c98ab6721566066f5ce5d3c740978946b01
Tim Yao <tim.yao@dpc.vic.gov.au>
2019-10-10 16:20:41 +1100

    [SDPA-3338] Fixed markup decode issue

--- packages/components/Organisms/Markup/markup-transpiler.js
+++ packages/components/Organisms/Markup/markup-transpiler.js
@@ -1,13 +1,15 @@
 import cheerio from 'cheerio'
 
-const markupTranspiler = (html, plugins, options = { decodeEntities: false }) => {
+const markupTranspiler = (html, plugins = {}, options = {}) => {
   const $ = cheerio.load(html, options)
   const $body = $('body')
 
-  // Load plugins to transpile embedded components
-  for (const [index, plugin] of plugins.entries()) {
-    $.prototype[`plugin${index}`] = plugin
-    $body[`plugin${index}`]()
+  if (Object.keys(plugins).length > 1) {
+    // Load plugins to transpile embedded components
+    for (const [index, plugin] of plugins.entries()) {
+      $.prototype[`plugin${index}`] = plugin
+      $body[`plugin${index}`]()
+    }
   }
 
   return $('body').html()

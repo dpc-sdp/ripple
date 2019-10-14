931ce96ace20faf7f90c148ee17c54b7f9e4a03b
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2019-10-14 20:42:55 +1100

    Export form components

--- packages/components/Molecules/Form/index.js
+++ packages/components/Molecules/Form/index.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueFormGenerator from 'vue-form-generator'
 import RplForm from './index.vue'
+import RplCheckbox from './Checkbox.vue'
+import RplSelect from './RplSelect.vue'
 
 const RplFormEventBus = new Vue()
 export { RplFormEventBus }
 export { VueFormGenerator }
+export { RplCheckbox }
+export { RplSelect }
 
 export default RplForm

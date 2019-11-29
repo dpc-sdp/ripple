6233056607790c90d87ef17a39d8e3c26794f0f9
Vincent Oosterwijk <vincent@today.design>
2019-11-29 16:35:45 +1100

    Remove data-driven-component

--- packages/ripple-nuxt-tide/modules/data-driven-component/components/index.js
+++
@@ -1,5 +0,0 @@
-import TideVicFreeWifiMap from './TideVicFreeWifiMap'
-import TideMapPreventionFamilyViolence from './TideMapPreventionFamilyViolence'
-
-export { TideVicFreeWifiMap }
-export { TideMapPreventionFamilyViolence }

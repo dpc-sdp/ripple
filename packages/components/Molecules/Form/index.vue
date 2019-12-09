4bde2d68078ec0b33fc127d962704e6262182f8d
Ming S Quah <quahmingsheng@me.com>
2019-12-09 14:06:06 +1100

    [SDPSUP-1212] : Add word count form validation (#611)

--- packages/components/Molecules/Form/index.vue
+++ packages/components/Molecules/Form/index.vue
@@ -71,6 +71,19 @@ export default {
   },
   mounted () {
     RplFormEventBus.$on('clearform', this.clearForm)
+
+    // TODO: We should abstract all future custom validators to a separate file and import them here.
+    VueFormGenerator.validators.rplWordCount = function (value, field) {
+      let wordCount = value.trim().split(/\s+/).length
+
+      if (wordCount > field.rplWordCountMax) {
+        return ['You have too many words. Maximum is ' + field.rplWordCountMax + ', you have ' + wordCount + ' words.']
+      }
+      if (wordCount < field.rplWordCountMin) {
+        return ['You have too few words. Minimum is ' + field.rplWordCountMin + ', you have ' + wordCount + ' words.']
+      }
+      return []
+    }
   },
   methods: {
 

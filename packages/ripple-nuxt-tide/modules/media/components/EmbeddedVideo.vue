941a6253070f4614ff743415b5de5406cd2b4119
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2020-01-08 15:02:22 +1100

    [SDPA-3421] Fix embedded video issues with no transcript and add tests

--- packages/ripple-nuxt-tide/modules/media/components/EmbeddedVideo.vue
+++ packages/ripple-nuxt-tide/modules/media/components/EmbeddedVideo.vue
@@ -1,7 +1,7 @@
 <template>
   <rpl-row row-gutter class="app-content tide-content tide-content--media tide-content--media-embedded-video">
     <rpl-col cols="full">
-      <rpl-embedded-video variant="full" :src="page.field_media_video_embed_field" :transcript="page.field_media_transcript.processed" />
+      <rpl-embedded-video variant="full" :src="page.field_media_video_embed_field" :transcript="page.field_media_transcript && page.field_media_transcript.processed" />
     </rpl-col>
   </rpl-row>
 </template>

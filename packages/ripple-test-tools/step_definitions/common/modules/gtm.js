b5dd3aec9af490c1c8c0e747a0ad1a89bdef78fd
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2020-03-11 12:13:14 +1100

    Add GTM test

---
+++ packages/ripple-test-tools/step_definitions/common/modules/gtm.js
@@ -0,0 +1,7 @@
+/* global cy, Cypress */
+
+const { Then } = require('cypress-cucumber-preprocessor/steps')
+
+Then(`the GTM script should be installed with id {string}`, (gtmid) => {
+  cy.window().its('google_tag_manager').should('have.property', gtmid.toUpperCase())
+})

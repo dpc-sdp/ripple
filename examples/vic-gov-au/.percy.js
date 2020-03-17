498d1dc5c57e593f6aad116556934bb9bd942a6c
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2020-03-18 09:41:14 +1100

    Adds percy config to handle authorization

---
+++ examples/vic-gov-au/.percy.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'version': 1,
+  'agent': {
+    'asset-discovery': {
+      'allowed-hostnames': [
+        '*.amazee.io',
+        '*.vic.gov.au'
+      ],
+      'request-headers': {
+        'Authorization': 'Basic ' + (
+          Buffer.from(`${process.env.CONTENT_API_AUTH_USER}:${process.env.CONTENT_API_AUTH_PASS}`).toString('base64')
+        )
+      },
+      'network-idle-timeout': 5000, // ms
+      'page-pool-size-min': 5, // pages
+      'page-pool-size-max': 30 // pages
+    }
+  }
+};

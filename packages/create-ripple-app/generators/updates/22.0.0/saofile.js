4267f0fbe7ab0fc59cf3cb570b3314b8adeab475
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2020-03-25 09:41:35 +1100

    Add siteid variable to smoke tests

--- packages/create-ripple-app/generators/updates/22.0.0/saofile.js
+++ packages/create-ripple-app/generators/updates/22.0.0/saofile.js
@@ -1,7 +1,6 @@
 const fs = require('fs')
 const path = require('path')
 const common = require('./../../common')
-const jscodeshift = require('jscodeshift')
 const templateDir = './../../../template'
 
 module.exports = {
@@ -38,59 +37,6 @@ module.exports = {
         patterns: {
           '_cypress.json': 'cypress.json'
         }
-      },
-      {
-        type: 'modify',
-        files: 'nuxt.config.js',
-        handler (data, filepath) {
-          /*
-          // https://astexplorer.net/
-          // example of using jscodeshift for changing the site value and not modifying anything else
-          const j = jscodeshift
-          const defaultNode = j(data).find(j.ExportDefaultDeclaration)
-          const nuxtConfig = defaultNode.nodes()[0].declaration.properties
-          const tide = nuxtConfig.filter(e => e.key.name === 'tide')
-          const site = tide[0].value.properties.filter(p => p.key.name === 'site')[0]
-          site.value.value = 123
-          const out = defaultNode.toSource({ quote: 'single' })
-          return out
-          */
-          const j = jscodeshift
-          const rootData = j(data)
-
-          const defaultNode = rootData.find(j.ExportDefaultDeclaration)
-          const build = defaultNode.find(j.Property, { key: { name: 'build' } })
-          if (build.length) {
-            console.warn('We found existing build configs. You need to manually merge new build configs in nuxt.config.js.')
-          }
-          const buildConfigs = `build: {
-  // Currently 'lodash' is mainly brought by Elastic search JS lib.
-  // Below lodash optimization can be reviewed after we migrate to new ES JS client.
-  babel: {
-    plugins: [
-      'lodash'
-    ]
-  },
-  extend (config, { isServer, loaders: { vue } }) {
-    const webpack = require('webpack')
-    const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
-    config.plugins.push(new LodashModuleReplacementPlugin({
-      'caching': true,
-      'collections': true,
-      'paths': true,
-      'shorthands': true
-    }))
-    // Load moment 'en-au' locale only for performance.
-    // https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
-    // You need to change it if your site is not in Australia.
-    config.plugins.push(new webpack.ContextReplacementPlugin(/moment[/\\\\]locale$/, /en-au/))
-  }
-}`
-          defaultNode.find(j.Property, { key: { name: 'css' } })
-            .insertAfter(buildConfigs)
-          const out = rootData.toSource({ quote: 'single' })
-          return out
-        }
       }
     ]
 
@@ -99,6 +45,7 @@ module.exports = {
         {
           type: 'add',
           files: ['**'],
+          transform: true,
           templateDir: `${templateDir}/_tests/_common`
         }
       )
@@ -108,6 +55,7 @@ module.exports = {
       actions.push({
         type: 'add',
         files: ['**'],
+        transform: true,
         templateDir: `${templateDir}/_tests/_smoke`
       })
     }
@@ -120,6 +68,7 @@ module.exports = {
             {
               type: 'add',
               files: [`./test/e2e/integration/core-modules/${tideModule}/**`, `./test/e2e/fixtures/${tideModule}/**`],
+              transform: true,
               templateDir: `${templateDir}/_tests/_modules`
             }
           )

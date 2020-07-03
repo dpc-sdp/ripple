4b4fc18a548867442b5ad56dde1a4ee63fd2845e
Tim Yao <tim.yao@dpc.vic.gov.au>
2020-07-03 15:16:44 +1000

    Added update script for updating sites

---
+++ packages/create-ripple-app/generators/updates/1.25.0/saofile.js
@@ -0,0 +1,23 @@
+const common = require('./../../common')
+const templateDir = './../../../template'
+
+module.exports = {
+  ...common,
+  templateDir,
+  actions () {
+    const actions = [
+      {
+        type: 'modify',
+        files: 'assets/_theme.scss',
+        handler (data, filepath) {
+          data = data.replace("success': #009ca6", "success': #027a83")
+          return data
+        }
+      }
+    ]
+    return actions
+  },
+  async completed () {
+    console.log('SDP 1.25.0: [SDPAA-666] replaced success color')
+  }
+}

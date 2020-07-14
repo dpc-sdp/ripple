bd91e64c87d521f316b8df5907c8da820ba06f0e
Tim Yao <tim.yao@dpc.vic.gov.au>
2020-07-14 11:17:09 +1000

    Added upgrade script

--- packages/create-ripple-app/generators/updates/1.25.0/saofile.js
+++ packages/create-ripple-app/generators/updates/1.25.0/saofile.js
@@ -13,11 +13,21 @@ module.exports = {
           data = data.replace("success': #009ca6", "success': #027a83")
           return data
         }
+      },
+      {
+        type: 'modify',
+        files: 'test/e2e/integration/modules/search/sitesearch.feature',
+        handler (data, filepath) {
+          data = data.replace('Search website', 'Search')
+          return data
+        }
       }
     ]
     return actions
   },
   async completed () {
-    console.log('SDP 1.25.0: [SDPA-4441] replaced success color')
+    console.log('SDP 1.25.0:')
+    console.log('[SDPA-4441] replaced success color')
+    console.log('[SDPA-3942] search text')
   }
 }

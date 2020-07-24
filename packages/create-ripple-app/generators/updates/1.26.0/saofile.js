404ef33d461a0257598f3931c4740e6b0a68f100
Tim Yao <tim.yao@dpc.vic.gov.au>
2020-07-24 17:11:58 +1000

    [SW-258] Revert extractCSS config

--- packages/create-ripple-app/generators/updates/1.26.0/saofile.js
+++ packages/create-ripple-app/generators/updates/1.26.0/saofile.js
@@ -1,5 +1,6 @@
 const common = require('./../../common')
 const templateDir = './../../../template'
+const jscodeshift = require('jscodeshift')
 
 module.exports = {
   ...common,
@@ -13,6 +14,19 @@ module.exports = {
           data = data.replace(' style="mix-blend-mode: overlay;"', '')
           return data
         }
+      },
+      {
+        type: 'modify',
+        files: 'nuxt.config.js',
+        handler (data, filepath) {
+          const j = jscodeshift
+          const rootData = j(data)
+
+          const defaultNode = rootData.find(j.ExportDefaultDeclaration)
+          defaultNode.find(j.Property, { key: { name: 'extractCSS' } }).remove()
+          const out = rootData.toSource({ quote: 'single' })
+          return out
+        }
       }
     ]
     return actions
@@ -20,5 +34,6 @@ module.exports = {
   async completed () {
     console.log('SDP 1.26.0:')
     console.log('[SDPA-4339] Fixed oops banner')
+    console.log('[SW-258] Fixed extractCSS issue')
   }
 }

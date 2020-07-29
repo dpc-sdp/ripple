2659ae1979a3b550ef94161f498bd36ff00ef9a6
Ruby Lamadora <rubylamadora@Rubys-MBP.lan>
2020-07-29 13:48:44 +1000

    Dynamically load IE polyfill

--- packages/ripple-nuxt-tide/lib/layouts/default.vue
+++ packages/ripple-nuxt-tide/lib/layouts/default.vue
@@ -124,6 +124,13 @@ export default {
       this.anchorScrollFix(this.$route.hash)
     }
     RplLinkEventBus.$on('navigate', this.onNavigate)
+
+    if (window.MSInputMethodContext && document.documentMode) {
+      const script = document.createElement('script')
+      script.type = 'text/javascript'
+      script.src = 'https://cdn.jsdelivr.net/gh/nuxodin/ie11CustomProperties@4.1.0/ie11CustomProperties.min.js'
+      document.body.appendChild(script)
+    }
   },
   methods: {
     onNavigate () {

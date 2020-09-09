110052537129e60ba67b7cd9ab2637216a9df014
Ruby Lamadora <rubylamadora@Rubys-MBP.lan>
2020-09-09 15:54:20 +1000

    Add a sample page to demonstrate icon

--- examples/vic-gov-au/pages/CustomIcon.vue
+++ examples/vic-gov-au/pages/CustomIcon.vue
@@ -1,10 +1,16 @@
 <template>
-  <rpl-icon symbol="icon_circle_arrow_right" color="primary" size="m" />
+  <div class="custom-icon">
+    <p>Adding a new icon example:</p>
+    <rpl-icon symbol="icon_circle_arrow_right" color="primary" size="m" />
+    <p>Replacing existing "search" icon with custom:</p>
+    <rpl-icon symbol="search" color="primary" size="m" />
+  </div>
 </template>
 
 <script>
 import RplIcon from '../../../packages/components/Atoms/Icon/Icon'
 import './../assets/ripple-icon/icon_circle_arrow_right'
+import './../assets/ripple-icon/icon_search'
 
 export default {
   name: 'CustomIcon',
@@ -13,3 +19,8 @@ export default {
   }
 }
 </script>
+<style>
+  .custom-icon {
+    padding: 200px 100px;
+  }
+</style>

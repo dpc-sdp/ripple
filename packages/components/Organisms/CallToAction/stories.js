a97de9c0db75c4aee346d57b6700581fc7a83aa6
dylankelly <dylan.kelly@dpc.vic.gov.au>
2020-09-23 13:56:50 +1000

    [SDPA-4624] Change CTA img fit to contain (#784)
    Co-authored-by: Tim Yao <31641325+tim-yao@users.noreply.github.com>

--- packages/components/Organisms/CallToAction/stories.js
+++ packages/components/Organisms/CallToAction/stories.js
@@ -11,7 +11,25 @@ storiesOf('Organisms/CallToAction', module)
   .addDecorator(withKnobs)
   .add('Call to Action', () => ({
     components: { RplCallToAction },
-    template: `<rpl-call-to-action :title="title" :summary="summary" :link="link" :image="image" />`,
+    template: `<div class="rpl-content"> <rpl-call-to-action :title="title" :summary="summary" :link="link" :image="image" /></div>`,
+    props: {
+      title: {
+        default: text('Title', 'Card prompting an action')
+      },
+      summary: {
+        default: text('Summary', '<p>Sed ut <b>perspiciatis</b> unde omnis iste natus error sit voluptatem accusantium dolore que laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p><p>Nemo enim ipsam voluptatem.</p>')
+      },
+      link: {
+        default: () => object('Call to action', { text: 'Call to action', url: '#' })
+      },
+      image: {
+        default: () => object('Image', { src: 'http://placehold.it/336x249', alt: '' })
+      }
+    }
+  }))
+  .add('without sidebar', () => ({
+    components: { RplCallToAction },
+    template: `<div class="rpl-content"> <rpl-call-to-action class="rpl-call-to-action--without-sidebar" :title="title" :summary="summary" :link="link" :image="image" /></div>`,
     props: {
       title: {
         default: text('Title', 'Card prompting an action')

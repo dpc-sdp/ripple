962998a5ce2eb77fa4c388d4797cd7dea7480dd2
Alan <alan@salsadigital.com.au>
2020-09-23 14:39:48 +1000

    [SDPA-2995] Change preview paths to use /oauth/.

--- packages/ripple-nuxt-tide/modules/authenticated-content/module.js
+++ packages/ripple-nuxt-tide/modules/authenticated-content/module.js
@@ -15,11 +15,10 @@ module.exports = function () {
       path: '/user/reset/:id/:time/:hash',
       component: '@dpc-sdp/ripple-nuxt-tide/modules/authenticated-content/pages/Reset.vue'
     })
-    // TODO - This route now conflicts with preview sub-module.
-    // routes.push({
-    //   name: 'tideauthenticated_login',
-    //   path: '/login',
-    //   component: '@dpc-sdp/ripple-nuxt-tide/modules/authenticated-content/pages/Login.vue'
-    // })
+    routes.push({
+      name: 'tideauthenticated_login',
+      path: '/login',
+      component: '@dpc-sdp/ripple-nuxt-tide/modules/authenticated-content/pages/Login.vue'
+    })
   })
 }

b69838889b61a3263a9d805db6e320f4d7a4b235
Alan <alan@salsadigital.com.au>
2020-09-29 17:17:21 +1000

    [SDPA-2995] Preview - Move redirect logic to helper and add unit tests.

--- packages/ripple-nuxt-tide/modules/preview/lib/helpers.js
+++ packages/ripple-nuxt-tide/modules/preview/lib/helpers.js
@@ -8,4 +8,29 @@ async function tidePreview (context) {
   }
 }
 
+async function tidePreviewLogin ($route, $auth, storage) {
+  const isLoginInProgress = ($route.query.code !== undefined)
+  if (!$auth.loggedIn && !isLoginInProgress) {
+    // Store destination.
+    if ($route.query.destination) {
+      storage.setItem('login-destination', $route.query.destination)
+    }
+    await $auth.loginWith('drupal')
+  }
+}
+
+function tidePreviewSuccessRedirect ($router, storage) {
+  const destination = storage.getItem('login-destination')
+  if (destination) {
+    // Go to destination.
+    storage.removeItem('login-destination')
+    $router.replace({ path: destination })
+  } else {
+    // Go to home.
+    $router.replace({ path: '/' })
+  }
+}
+
 export { tidePreview }
+export { tidePreviewLogin }
+export { tidePreviewSuccessRedirect }

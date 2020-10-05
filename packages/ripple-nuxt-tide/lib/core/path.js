e96c5d4265de3104c1307a0b7a9285e6d8529104
Alan <alan@salsadigital.com.au>
2020-10-05 14:33:03 +1100

    [SDPA-2995] [SDPA-3202] - Move preview / share calls into module. Update calls to include headerConfig.

--- packages/ripple-nuxt-tide/lib/core/path.js
+++ packages/ripple-nuxt-tide/lib/core/path.js
@@ -6,4 +6,13 @@ function isPreviewPath (path) {
   return (path.indexOf('/preview/') === 0)
 }
 
+/**
+ * Check if path is for a share link page.
+ * @param {String} path current route.path
+ */
+function isShareLinkPath (path) {
+  return (path.indexOf('/share_link/') === 0)
+}
+
 export { isPreviewPath }
+export { isShareLinkPath }

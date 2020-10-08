6c530217fc45e810cfabd762ccfd9a2bb23312f1
Alan <alan@salsadigital.com.au>
2020-10-08 16:55:52 +1100

    DO NOT MERGE.

--- examples/vic-gov-au/nuxt.config.js
+++ examples/vic-gov-au/nuxt.config.js
@@ -118,7 +118,7 @@ export default {
   * See https://www.npmjs.com/package/@dpc-sdp/ripple-nuxt-tide
   */
   tide: {
-    baseUrl: process.env.CONTENT_API_SERVER,
+    baseUrl: 'https://nginx-php-content-vic-pr-971.lagoon.vicsdp.amazee.io/',
     auth: {
       username: process.env.CONTENT_API_AUTH_USER,
       password: process.env.CONTENT_API_AUTH_PASS

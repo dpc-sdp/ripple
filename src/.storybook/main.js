b486eccbcbcd16dddc2c8720f80ba2c27a450eee
Ruby Lamadora <ruby.lamadora@dpc.vic.gov.au>
2020-12-01 11:00:54 +1100

    Upgrade storybook to v6

---
+++ src/.storybook/main.js
@@ -0,0 +1,85 @@
+const path = require("path");
+
+const resolve = (dir) => {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  stories: ['../../packages/components/**/stories.@(js|mdx)'],
+  logLevel: 'debug',
+  addons: [
+    '@storybook/addon-knobs',
+    '@storybook/addon-a11y',
+    '@storybook/addon-viewport',
+    '@storybook/addon-backgrounds',
+    {
+      name: '@storybook/addon-docs', // new addon for docs
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null
+      }
+    },
+  ],
+  webpackFinal: async (config, { mode }) => {
+    // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    config.resolve.alias.vue = 'vue/dist/vue.js'
+
+    // remove file-loader as it has conflicting config with url-loader
+    config.module.rules = config.module.rules.filter(rule => {
+      if (rule.loader && rule.loader.includes('file-loader')) {
+        return false
+      }
+      return rule
+    })
+
+    config.module.rules.push(
+      {
+        test: /\.(scss|css)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+        include: [
+          resolve('node_modules'),
+          resolve('../packages/components'),
+          resolve('storybook-components'),
+          resolve('storybook-components/scss'),
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        loader: 'url-loader',
+        exclude: [
+          resolve('static/custom_icons/')
+        ],
+      },
+      {
+        test: /\.svg$/,
+        include: [
+          resolve('static/custom_icons/')
+        ],
+        use: [
+          'svg-sprite-loader',
+          'svgo-loader'
+        ]
+      },
+      {
+        test: /\.(js|vue)$/,
+        loader: 'eslint-loader',
+        enforce: 'pre',
+        include: [
+          resolve('../packages/components/'),
+          resolve('storybook-components'),
+          resolve('test')],
+        options: {
+          formatter: require('eslint-friendly-formatter'),
+          emitWarning: true
+        }
+      }
+    )
+
+    // Return the altered config
+    return config
+  },
+}
--- src/.storybook/preview.js
+++ src/.storybook/preview.js
@@ -1,5 +1,4 @@
 import { addDecorator, configure, addParameters } from '@storybook/vue'
-import { withA11y } from '@storybook/addon-a11y'
 import Vue from 'vue'
 
 // Apply our Ripple global styles on storybook
@@ -10,11 +9,14 @@ import '../storybook-components/scss/story.scss'
 import RplGlobal from '@dpc-sdp/ripple-global'
 import RplMarkupExamplePlugins from '@dpc-sdp/ripple-markup/examplePlugins'
 
-addDecorator(withA11y)
-
 addParameters({
+  a11y: {
+    element: '#root',
+    config: {},
+    options: {},
+    manual: true,
+  },
   docs: {
-    // Set a smaller default height.
     iframeHeight: '60px',
     // Disable Docs globally for now until we got time to update them.
     // We are still able to add Docs in component level.
@@ -24,13 +26,14 @@ addParameters({
     // However multi stories with same knob name in one docs page will be overridden.
     // We need to migrate knobs to control in v6.
     // https://github.com/storybookjs/storybook/blob/next/addons/controls/README.md#how-do-i-migrate-from-addon-knobs
-    // inlineStories: true
+    inlineStories: true
   },
-  backgrounds: [
-    // We need a story background color which is different with all SDP theme color,
-    // so we can tell if a component has the right background color.
-    { name: 'light', value: '#edfafc', default: true }
-  ],
+  backgrounds: {
+    default: 'light',
+    values: [
+      { name: 'light', value: '#edfafc' }
+    ],
+  },
   knobs: {
     // To allow knob text type to support v-html prop.
     escapeHTML: false,
@@ -39,5 +42,3 @@ addParameters({
 
 // Install Ripple Global plugin
 Vue.use(RplGlobal, { imgQueryString: false, rplMarkup: {plugins: RplMarkupExamplePlugins, options: { decodeEntities: false }}})
-
-configure(require.context('./../../packages/components', true, /(stories\.js|mdx)$/), module)

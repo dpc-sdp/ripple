8f4d2662c113c4b3c5af80b6566d838f0bf30d96
Tim Yao <tim.yao@dpc.vic.gov.au>
2020-12-01 17:32:38 +1100

    [SDPA-4647] Added update script for deps update

---
+++ packages/create-ripple-app/generators/updates/1.31.0/saofile.js
@@ -0,0 +1,29 @@
+const common = require('./../../common')
+const templateDir = './../../../template'
+
+module.exports = {
+  ...common,
+  templateDir,
+  actions () {
+    const actions = [
+      {
+        type: 'modify',
+        files: 'package.json',
+        handler (data, filepath) {
+          data.dependencies['nuxt'] = `2.14.7`
+          data.dependencies['run-script-os'] = `^1.1.3`
+          data.devDependencies['babel-eslint'] = `^10.1.0`
+          data.devDependencies['eslint-plugin-import'] = `^2.22.1`
+          data.devDependencies['sass-resources-loader'] = `^2.1.1`
+          data.devDependencies['start-server-and-test'] = `^1.11.6`
+          return data
+        }
+      }
+    ]
+    return actions
+  },
+  async completed () {
+    console.log('SDP 1.31.0:')
+    console.log('[SDPA-4647] Update dependencies')
+  }
+}

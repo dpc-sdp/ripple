1918877091403f4e7a23d360236064717bc727a3
Ruby Lamadora <ruby.lamadora@dpc.vic.gov.au>
2020-12-03 12:27:33 +1100

    Fix module not found for ripple logo

--- src/.storybook/manager.js
+++ src/.storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
-import logo from './../../docs/sdp-ripple.png';
+import logo from '../../docs/sdp-ripple.png';
 
 const theme = create({
   brandImage: `/${logo}`,

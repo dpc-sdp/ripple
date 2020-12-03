83d2f2c2017c63ae8e65ffc010b53d1077e33d39
Ruby Lamadora <ruby.lamadora@dpc.vic.gov.au>
2020-12-03 12:56:19 +1100

    Move ripple logo inside src to fix node couldn't resolve issue

--- src/.storybook/manager.js
+++ src/.storybook/manager.js
@@ -1,6 +1,6 @@
 import { addons } from '@storybook/addons';
 import { create } from '@storybook/theming/create';
-import logo from '../../docs/sdp-ripple.png';
+import logo from '../static/sdp-ripple.png';
 
 const theme = create({
   brandImage: `/${logo}`,

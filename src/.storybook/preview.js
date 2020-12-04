802a51b77ef33ef31e9d7d073626468ae085fed5
Ruby Lamadora <ruby.lamadora@dpc.vic.gov.au>
2020-12-04 22:30:13 +1100

    Add config to sort stories

--- src/.storybook/preview.js
+++ src/.storybook/preview.js
@@ -35,10 +35,10 @@ export const parameters = {
       { name: 'light', value: '#edfafc' }
     ],
   },
-  knobs: {
-    // To allow knob text type to support v-html prop.
-    escapeHTML: false,
-  }
+  options: {
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
 }
 
 // Install Ripple Global plugin

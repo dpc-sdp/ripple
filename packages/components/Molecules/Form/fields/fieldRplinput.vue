7b584d13376518718893c3a6d79efef87f0f05b9
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2021-11-03 09:28:30 +1100

    Adds search input field for use in fair jobs code

---
+++ packages/components/Molecules/Form/fields/fieldRplinput.vue
@@ -0,0 +1,18 @@
+<!-- fieldAwesome.vue -->
+<template>
+  <rpl-input
+    v-model="value"
+    v-bind="schema"
+  />
+</template>
+<script>
+import RplInput from './../Input.vue'
+import { abstractField } from 'vue-form-generator'
+export default {
+  name: 'RplInputField',
+  components: {
+    RplInput
+  },
+  mixins: [abstractField]
+}
+</script>

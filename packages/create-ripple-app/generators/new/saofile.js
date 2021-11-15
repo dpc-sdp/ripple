f08ca953561f0ed8ef5d3615ef0ccce72a2a0326
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2021-11-15 22:00:57 +1100

    remove unused imports

--- packages/create-ripple-app/generators/new/saofile.js
+++ packages/create-ripple-app/generators/new/saofile.js
@@ -1,5 +1,3 @@
-const fs = require('fs')
-const path = require('path')
 const templateDir = './../../template'
 const common = require('./../common')
 
--- packages/create-ripple-app/generators/updates/1.22.0/saofile.js
+++ packages/create-ripple-app/generators/updates/1.22.0/saofile.js
@@ -1,5 +1,3 @@
-const fs = require('fs')
-const path = require('path')
 const common = require('./../../common')
 const templateDir = './../../../template'
 const log = require('./../../../logger')

5ccabbd8a50aaf50daf7c4acb68cf0bd7b9a9809
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2021-11-24 12:04:03 +1100

    fix integration path spelling

--- examples/vue-example-app/cypress/support/index.js
+++ examples/vue-example-app/cypress/support/index.js
@@ -13,4 +13,8 @@
 // https://on.cypress.io/configuration
 // ***********************************************************
 
-require('@dpc-sdp/ripple-test-tools/commands')
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')

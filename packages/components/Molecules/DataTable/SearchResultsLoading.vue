2e34bd48790f46f5c77de26fdecb2c220c7a0e7f
dylankelly <dylan.kelly@dpc.vic.gov.au>
2021-11-29 20:50:13 +1100

    Adds data listing component and ripple-data-vic-api module (#1082)
    * WIP on accordion table
    
    * [SDPA-5326] Add Complex data table
    
    * Remove duplicate mixin
    
    * Change show more text and make zebra order configurable
    
    * Adds ie fixes and mobile padding change
    
    * Update padding to match design
    
    * Design updates
    
    * remove mixin
    
    * remove padding on more info button
    
    * WIP on data vic api package
    
    * WIP on adding data-vic module
    
    * Adds basic test
    
    * Update test page name
    
    * Adds search input field for use in fair jobs code
    
    * v1.24.5-alpha.2
    
    * ignore lerna debug log
    
    * v1.24.5-alpha.3
    
    * Fix ripple tabs import
    
    * v1.24.5-alpha.4
    
    * Adds a11y label and button role
    
    * v1.24.5-alpha.5
    
    * fix issue with no results message
    
    * v1.24.5-alpha.6
    
    * remove console log
    
    * Adds search input field for use in fair jobs code
    
    * Fix input field not correctly setting ID
    
    * update tabs version

---
+++ packages/components/Molecules/DataTable/SearchResultsLoading.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="rpl-search-results-loading">
+    <div class="rpl-search-results-loading__top">
+      <div class="animated-background rpl-search-results-loading__count"></div>
+      <div class="animated-background rpl-search-results-loading__sort"></div>
+    </div>
+    <div class="animated-background rpl-search-results-loading__table-header"></div>
+    <div class="animated-background rpl-search-results-loading__table"></div>
+  </div>
+</template>
+
+<style lang="scss">
+@import "~@dpc-sdp/ripple-global/scss/settings";
+@import "~@dpc-sdp/ripple-global/scss/tools";
+@keyframes placeHolderShimmer{
+    0%{
+        background-position: -468px 0
+    }
+    100%{
+        background-position: 468px 0
+    }
+}
+
+$rpl-search-results-loading-bg-color: rpl-color('light_neutral');
+.rpl-search-results-loading {
+  &__top {
+    display: flex;
+    flex-direction: row;
+    margin-top: $rpl-space-4 + $rpl-space-2;
+    margin-bottom: $rpl-space-4;
+    align-items: center;
+  }
+  &__count,
+  &__sort,
+  &__table,
+  &__table-header {
+    background-color: $rpl-search-results-loading-bg-color;
+    animation-duration: 1.25s;
+    animation-fill-mode: forwards;
+    animation-iteration-count: infinite;
+    animation-name: placeHolderShimmer;
+    animation-timing-function: linear;
+    background: #F6F6F6;
+    background: linear-gradient(270deg, rpl-color('light_neutral') 8%, #F0F0F0 18%, rpl-color('light_neutral') 33%);
+    background-size: 800px 104px;
+    height: 96px;
+    position: relative;
+  }
+  &__count {
+    height: 24px;
+    width: 260px;
+  }
+  &__sort {
+    margin-left: auto;
+    height: 48px;
+    width: 280px;
+  }
+  &__table-header {
+    height: 30px;
+    width: 100%;
+    margin-top: $rpl-space-4 + $rpl-space-2;
+    margin-bottom: $rpl-space;
+  }
+  &__table {
+    height: 800px;
+    width: 100%;
+    margin-top: $rpl-space-4;
+  }
+}
+</style>

a680946d151e8cf97a3ea2aff56a6c42be3787d5
Christopher Hopper <chopper@deloitte.com.au>
2021-12-17 19:01:34 +1100

    SDPA-4650: Fix coding standards violations

--- packages/ripple-social-providers/build.js
+++ packages/ripple-social-providers/build.js
@@ -1,24 +1,24 @@
-const fs = require("fs");
-const path = require("path");
-const yaml = require("js-yaml");
+const fs = require("fs")
+const path = require("path")
+const yaml = require("js-yaml")
 
 console.log(
   JSON.stringify(
     process.argv.slice(2).reduce((acc, file) => {
       const entries = yaml.load(
         fs.readFileSync(path.resolve(process.cwd(), file), "utf8"), {}
-      );
+      )
 
       for (const entry of entries) {
-        delete entry.example_urls;
-        delete entry.notes;
+        delete entry.example_urls
+        delete entry.notes
 
-        acc.push(entry);
+        acc.push(entry)
       }
 
-      return acc;
+      return acc
     }, []),
     null,
     4
   )
-);
+)

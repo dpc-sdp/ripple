e803a53b3612c411425bf6026be146dcc730d653
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-01-19 14:40:42 +1100

    Tidy up remaining event content type mappings

--- packages/ripple-tide-event/index.js
+++ packages/ripple-tide-event/index.js
@@ -1,5 +1,4 @@
-import { getField, getImageFromField /*, getLandingPageComponents */ } from '@dpc-sdp/ripple-tide-api/src/services/utils'
-// import componentMapping from './component-mapping'
+import { getField, getImageFromField } from '@dpc-sdp/ripple-tide-api/src/services/utils'
 import components from './component-loader'
 
 export default {
@@ -12,13 +11,10 @@ export default {
       text: 'See event details',
       url: getField(src, 'path.url', null)
     }),
-    image: (src) => getImageFromField(src, 'field_featured_image'), // Not working properly
-    // dateStart: ,
-    // dateEnd: ,
+    image: (src) => getImageFromField(src, 'field_featured_image'),
+    dateStart: (src) => src.field_event_details[0]?.field_paragraph_date_range?.value,
+    dateEnd: (src) => src.field_event_details[0]?.field_paragraph_date_range?.end_value,
     location: (src) => src.field_event_details?.field_paragraph_location
-    // location: (src) => getField(src, 'field_event_details')
-    //   .map(eventDetails => getField(eventDetails, 'field_paragraph_location'))
-    // bodyComponents: async function (src) { return await getLandingPageComponents(src, 'field_landing_page_component', componentMapping, this) },
   },
   includes: [
     'field_landing_page_contact.field_paragraph_phones',
@@ -26,6 +22,7 @@ export default {
     'field_event_category',
     'field_event_details.field_event_requirements',
     'field_featured_image',
+    'field_featured_image.field_media_image',
     'field_audience'
   ]
 }

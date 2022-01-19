7bcbb4f2437162e69bf80804eadb6c137d385e16
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-01-19 17:11:22 +1100

    Move old event content type mappings from vue template into mappings file

--- packages/ripple-tide-event/index.js
+++ packages/ripple-tide-event/index.js
@@ -1,6 +1,15 @@
 import { getField, getImageFromField } from '@dpc-sdp/ripple-tide-api/src/services/utils'
 import components from './component-loader'
 
+import moment from 'moment'
+
+// Utils
+const formatDate = (date, format) => {
+  format = (format === undefined) ? 'DD MMMM' : format
+  moment.locale('en-au') // TODO: Is there a better place to get this?
+  return moment(date).format(format)
+}
+
 export default {
   pageComponent: () => import(/* webpackMode: "eager" */ '@dpc-sdp/ripple-tide-event/index.vue'),
   components,
@@ -12,9 +21,25 @@ export default {
       url: getField(src, 'path.url', null)
     }),
     image: (src) => getImageFromField(src, 'field_featured_image'),
-    dateStart: (src) => src.field_event_details[0]?.field_paragraph_date_range?.value,
-    dateEnd: (src) => src.field_event_details[0]?.field_paragraph_date_range?.end_value,
-    location: (src) => src.field_event_details?.field_paragraph_location
+    date: (src) => {
+      const format = (src.field_event_details[0]?.field_show_time) ? 'DD MMMM YYYY hh:mm a' : 'DD MMMM YYYY'
+      const start = formatDate(src.field_event_details[0]?.field_paragraph_date_range?.value, format)
+      const end = formatDate(src.field_event_details[0]?.field_paragraph_date_range?.end_value, format)
+      const range = start + (end ? ` - ${end}` : '')
+
+      return {
+        start,
+        end,
+        range
+      }
+    },
+    location: (src) => src.field_event_details[0]?.field_paragraph_location,
+    prices: (src) => {
+      const from = src.field_event_details[0]?.field_paragraph_event_price_from
+      const to = src.field_event_details[0]?.field_paragraph_event_price_to
+
+      return from && from + (to ? ` - ${to}` : '')
+    }
   },
   includes: [
     'field_landing_page_contact.field_paragraph_phones',
--- packages/ripple-tide-event/index.vue
+++ packages/ripple-tide-event/index.vue
@@ -1,103 +1,118 @@
 <template>
-  <div :class="page.class">
-    <rpl-page-layout
-      :sidebar="hasSidebar"
-      :backgroundColor="page.backgroundColor"
-      :backgroundGraphic="heroBgGraphic"
-      class="app-main"
-    >
-      <template slot="aboveContent">
-        <rpl-hero-banner
-          v-if="heroBanner"
-          v-bind="heroBanner"
-          class="rpl-site-constrain--on-all"
-        />
-        <rpl-acknowledgement v-if="page.acknowledgement" :text="$store.state.site.acknowledgement" theme="standalone" />
-      </template>
-
-      <template slot="aboveContentTwo">
-        <page-components v-if="page.headerComponents" :componentMapping="page.headerComponents" :gutter="false" />
-      </template>
-
-      <rpl-row row-gutter class="app-content tide-content tide-content--landing-page">
-        <!-- <template v-if="anchorLinks && anchorLinks.length > 0">
-          <rpl-col cols="full">
-            <rpl-anchor-links title="On this page:" :links="anchorLinks" />
-          </rpl-col>
-        </template> -->
-
-        <page-components :componentMapping="page.bodyComponents" />
-
-        <rpl-updated-date v-if="page.showLastUpdated" :date="page.changed"></rpl-updated-date>
-
-        <rpl-col v-if="page.showTags" cols="full">
-          <app-topic-tags :topic="this.page.topic" :tags="this.page.tags" />
-        </rpl-col>
-      </rpl-row>
-
-      <template slot="sidebar">
-        <page-components :componentMapping="page.sidebarComponents" />
-      </template>
-    </rpl-page-layout>
-  </div>
+  <rpl-row row-gutter class="app-content tide-content tide-content--event">
+    <rpl-col cols="full">
+      <rpl-markup v-if="page.field_event_description" :html="page.field_event_description.processed"></rpl-markup>
+    </rpl-col>
+    <rpl-col cols="full">
+      <rpl-list :list="eventList" />
+    </rpl-col>
+    <rpl-col cols="full">
+      <rpl-markup v-if="page.body" :html="page.body.processed"></rpl-markup>
+    </rpl-col>
+    <rpl-col cols="full" v-if="cta">
+      <rpl-button :href="cta.url" theme="primary">{{cta.text}}</rpl-button>
+    </rpl-col>
+    <!-- Tags here -->
+    <img v-if="atdwTrackingBeacon" :src="atdwTrackingBeacon" width="1" height="1" border="0" />
+  </rpl-row>
 </template>
 
 <script>
-// Layout.
 import { RplRow, RplCol } from '@dpc-sdp/ripple-grid'
-import { RplPageLayout } from '@dpc-sdp/ripple-layout'
-import RplBreadcrumbs from '@dpc-sdp/ripple-breadcrumbs'
-import RplUpdatedDate from '@dpc-sdp/ripple-updated-date'
-import { RplHeroBanner, RplIntroBanner } from '@dpc-sdp/ripple-hero-banner'
-import { RplAcknowledgement } from '@dpc-sdp/ripple-site-footer'
-
-import RplAnchorLinks from '@dpc-sdp/ripple-anchor-links'
-import PageComponents from '@dpc-sdp/ripple-tide-landing-page/page-components.vue'
-import AppTopicTags from '@dpc-sdp/ripple-tide-landing-page/TopicTags.vue'
+import RplList from '@dpc-sdp/ripple-list'
+import RplMarkup from '@dpc-sdp/ripple-markup'
+import RplButton from '@dpc-sdp/ripple-button'
+import formatdate from '@dpc-sdp/ripple-global/mixins/formatdate'
 
 export default {
+  name: 'TideEventContent',
+  mixins: [formatdate],
   components: {
-    RplHeroBanner,
-    RplAcknowledgement,
-    RplIntroBanner,
-    RplUpdatedDate,
-    RplBreadcrumbs,
-    RplPageLayout,
     RplRow,
+    RplButton,
     RplCol,
-    RplAnchorLinks,
-    PageComponents,
-    AppTopicTags
+    RplList,
+    RplMarkup
   },
   props: {
-    page: {
-      type: Object
-    }
+    page: Object
   },
   computed: {
-    hasSidebar () {
-      return false
+    cta () {
+      if (this.page.field_event_details) {
+        return this.$tideMapping.filter(this.page.field_event_details[0].field_paragraph_link, ['paragraphLink']) || null
+      }
     },
-    heroBanner () {
-      if (this.page.showHeroBanner) {
-        return {
-          title: this.page.title,
-          links: this.page.heroBannerLinks,
-          introText: this.page.summary
+    eventList () {
+      // Hardcode here to use single event only, as disucssed with BA. Will support multiple events later.
+      const eventDetails = this.page.eventDetails[0]
+
+      const dateTimeFormat = (eventDetails.field_show_time) ? 'DD MMMM YYYY hh:mm a' : 'DD MMMM YYYY'
+      const date = this.formatDate(eventDetails.field_paragraph_date_range.value, dateTimeFormat)
+      const endDate = eventDetails.field_paragraph_date_range.end_value ? this.formatDate(eventDetails.field_paragraph_date_range.end_value, dateTimeFormat) : ''
+      const dateRange = date + (endDate ? ` - ${endDate}` : '')
+      const address = this.$tideMapping.filter(eventDetails.field_paragraph_location, ['paragraphLocation'])
+      const prices = eventDetails.field_paragraph_event_price_from && eventDetails.field_paragraph_event_price_from + (eventDetails.field_paragraph_event_price_to ? ` - ${eventDetails.field_paragraph_event_price_to}` : '')
+      const bookLink = this.$tideMapping.filter(this.page.field_node_link, ['paragraphLink'])
+
+      let list = [
+        {
+          symbol: 'calendar',
+          size: '0.8',
+          text: dateRange
         }
+      ]
+
+      if (address && address.length > 0) {
+        list.push({
+          symbol: 'map_marker',
+          size: '1',
+          text: address
+        })
       }
+
+      if (prices) {
+        list.push({
+          symbol: 'dollar_negative',
+          size: '1.2',
+          text: prices
+        })
+      }
+
+      if (bookLink !== null) {
+        list.push({
+          symbol: 'browser',
+          size: '0.8',
+          link: bookLink.url,
+          text: bookLink.url
+        })
+      }
+
+      // This is made by following design & Tide implementation, icons have to be hardcoded here.
+      // If edior changed/added term, the icon won't work.
+      const requirementIcons = {
+        'default': { name: 'star', size: 1 },
+        'Accessible venue': { name: 'accessible', size: 1.3 },
+        'Child friendly': { name: 'child_friendly', size: 1.3 },
+        'Free admission': { name: 'free', size: 1.3 },
+        'Online webinar': { name: 'webinar', size: 1.2 },
+        'Seniors': { name: 'senior', size: 1.3 }
+      }
+
+      const requirements = eventDetails.field_event_requirements || []
+      requirements.map(requirement => {
+        list.push({
+          symbol: requirementIcons[requirement.name] ? requirementIcons[requirement.name].name : requirementIcons.default.name,
+          size: requirementIcons[requirement.name] ? requirementIcons[requirement.name].size : requirementIcons.default.size,
+          text: requirement.name
+        })
+      })
+
+      return list
     },
-    heroBgGraphic () {
-      if (this.page.heroBackgroundImage) {
-        return this.page.heroBackgroundImage
-      } else {
-        return '/img/header-pattern-shape.png'
-      }
+    atdwTrackingBeacon () {
+      return (!this.page.doNotTrack && this.page.field_tracking_beacon) || false
     }
   }
 }
 </script>
-
-<style>
-
-</style>

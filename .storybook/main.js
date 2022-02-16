25a19e1bef8bdc09115f7710a4f877c300857da0
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-02-16 14:34:52 +1100

    chore: :recycle: duplicate storybook config to ripple-ui-core, rework root scripts
    Storybook should now be managed at the package level, the root files are kept for future composition

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,8 @@
 module.exports = {
-  stories: [
-    '../packages/**/*.stories.mdx',
-    '../packages/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  core: { builder: 'storybook-builder-vite' },
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
-  async viteFinal(config, { configType }) {
-    // customize the Vite config here
-    return config
+  refs: {
+    'ui-core': {
+      title: 'UI Core',
+      url: 'http://localhost:6010'
+    }
   }
 }

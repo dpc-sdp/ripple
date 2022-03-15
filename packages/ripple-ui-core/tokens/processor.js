add0d846f52744a347d9eca9c5e234768aa3ec7f
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2022-03-15 21:34:26 +1100

    feat(@dpc-sdp/ripple-ui-core): :sparkles: remove figma generated tokens and add core variables
    Adds first pass at design tokens in code with example usage in button component

--- packages/ripple-ui-core/tokens/processor.js
+++ packages/ripple-ui-core/tokens/processor.js
@@ -2,45 +2,42 @@ const StyleDictionary = require('style-dictionary')
 const baseConfig = require('./config.json')
 
 StyleDictionary.registerTransform({
-  name: 'size/px',
+  name: 'size/pixels',
   type: 'value',
   matcher: (token) => {
-    return (
-      (token.unit === 'pixel' || token.type === 'dimension') &&
-      token.value !== 0
-    )
+    return token.attributes.category === 'space'
   },
   transformer: (token) => {
     return `${token.value}px`
   }
 })
 
-StyleDictionary.registerTransform({
-  name: 'size/percent',
-  type: 'value',
-  matcher: (token) => {
-    return token.unit === 'percent' && token.value !== 0
-  },
-  transformer: (token) => {
-    return `${token.value}%`
-  }
-})
+// StyleDictionary.registerTransform({
+//   name: 'size/percent',
+//   type: 'value',
+//   matcher: (token) => {
+//     return token.unit === 'percent' && token.value !== 0
+//   },
+//   transformer: (token) => {
+//     return `${token.value}%`
+//   }
+// })
 
-StyleDictionary.registerTransformGroup({
-  name: 'custom/css',
-  transforms: StyleDictionary.transformGroup['css'].concat([
-    'size/px',
-    'size/percent'
-  ])
-})
+// StyleDictionary.registerTransformGroup({
+//   name: 'custom/css',
+//   transforms: StyleDictionary.transformGroup['css'].concat([
+//     'size/px',
+//     'size/percent'
+//   ])
+// })
 
-StyleDictionary.registerTransformGroup({
-  name: 'custom/scss',
-  transforms: StyleDictionary.transformGroup['less'].concat([
-    'size/px',
-    'size/percent'
-  ])
-})
+// StyleDictionary.registerTransformGroup({
+//   name: 'custom/scss',
+//   transforms: StyleDictionary.transformGroup['less'].concat([
+//     'size/px',
+//     'size/percent'
+//   ])
+// })
 
 StyleDictionary.registerFilter({
   name: 'validToken',

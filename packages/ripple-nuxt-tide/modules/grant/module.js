995ac6abbec3a13ae245689955eb947d06487302
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-03-16 16:20:29 +1100

    SDPA-6014 - Add feature toggle logic for hardcoded nuxt routes

--- packages/ripple-nuxt-tide/modules/grant/module.js
+++ packages/ripple-nuxt-tide/modules/grant/module.js
@@ -1,9 +1,13 @@
 module.exports = function () {
   this.extendRoutes((routes, resolve) => {
-    routes.push({
-      name: 'tidegrants',
-      path: '/grants',
-      component: '@dpc-sdp/ripple-nuxt-tide/modules/grant/pages/search.vue'
-    })
+    const grantConfig = this.options.tide.modules.grant
+
+    if (!grantConfig?.disableGrantListing) {
+      routes.push({
+        name: 'tidegrants',
+        path: '/grants',
+        component: '@dpc-sdp/ripple-nuxt-tide/modules/grant/pages/search.vue'
+      })
+    }
   })
 }
--- packages/ripple-nuxt-tide/modules/profile/module.js
+++ packages/ripple-nuxt-tide/modules/profile/module.js
@@ -1,10 +1,13 @@
 module.exports = function () {
   this.extendRoutes((routes, resolve) => {
     const profileConfig = this.options.tide.modules.profile
-    routes.push({
-      name: 'tidehonorroll',
-      path: profileConfig.route || '/profiles',
-      component: '@dpc-sdp/ripple-nuxt-tide/modules/profile/pages/search.vue'
-    })
+
+    if (!profileConfig?.disableProfileListing) {
+      routes.push({
+        name: 'tidehonorroll',
+        path: profileConfig.route || '/profiles',
+        component: '@dpc-sdp/ripple-nuxt-tide/modules/profile/pages/search.vue'
+      })
+    }
   })
 }

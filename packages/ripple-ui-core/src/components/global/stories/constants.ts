cc40753e76a4659e86a4dc0d6019b7b5d8eac3ee
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2022-05-14 23:43:53 +1000

    feat(@dpc-sdp/ripple-ui-core): ✨ adds typography utilities and bps

---
+++ packages/ripple-ui-core/src/components/global/stories/constants.ts
@@ -0,0 +1,75 @@
+const base = 'rpl-type'
+export const RplTypeStyles = [
+  {
+    title: 'H1 / Highlight',
+    class: `${base}-h1-highlight`
+  },
+  {
+    title: 'H1 / Heading',
+    class: `${base}-h1`
+  },
+  {
+    title: 'H2 / Heading',
+    class: `${base}-h2`
+  },
+  {
+    title: 'H3 / Heading',
+    class: `${base}-h3`
+  },
+  {
+    title: 'H4 / Heading',
+    class: `${base}-h4`
+  },
+  {
+    title: 'Paragraph / Large',
+    class: `${base}-body-large`
+  },
+  {
+    title: 'Paragraph / Default',
+    class: `${base}-body`
+  },
+  {
+    title: 'Paragraph / Default / Link',
+    class: `${base}-body-link`
+  },
+  {
+    title: 'Paragraph / Default / Bold',
+    class: `${base}-body-bold`
+  },
+  {
+    title: 'Paragraph / Ordered List',
+    class: `${base}-body-ol`
+  },
+  {
+    title: 'Paragraph / Unordered List',
+    class: `${base}-body-ul`
+  },
+  {
+    title: 'Component / Large / Bold',
+    class: `${base}-component-large-bold`
+  },
+  {
+    title: 'Component / Default',
+    class: `${base}-component`
+  },
+  {
+    title: 'Component / Default / Bold',
+    class: `${base}-component-bold`
+  },
+  {
+    title: 'Component / Bold / Link',
+    class: `${base}-component-bold-link`
+  },
+  {
+    title: 'Component / Small',
+    class: `${base}-component-small`
+  },
+  {
+    title: 'Component / Caps',
+    class: `${base}-component-caps`
+  },
+  {
+    title: 'Component / Caps / Bold',
+    class: `${base}-component-caps-bold`
+  }
+] as const

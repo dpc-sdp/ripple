769f55912eb7e675ac4f11fded3887eaf4779cbc
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-05-18 17:53:35 +1000

    feat(@dpc-sdp/ripple-ui-core): :sparkles: add tag component

---
+++ packages/ripple-ui-core/src/components/tag/tag.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default { name: 'RplTag' }
+</script>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { RplTagThemes } from './constants'
+defineProps({
+  theme: {
+    type: String as PropType<typeof RplTagThemes[number]>,
+    default: RplTagThemes[0]
+  },
+  label: {
+    type: String,
+    default: ''
+  }
+})
+</script>
+
+<template>
+  <span :className="`rpl-tag rpl-tag--${theme}`">{{ label }}</span>
+</template>
+
+<style src="./tag.css" />

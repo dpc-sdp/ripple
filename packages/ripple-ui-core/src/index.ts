97b4a5f05159f9592fdb712cb33bbb72e737f03a
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-05-19 14:10:39 +1000

    refactor(@dpc-sdp/ripple-ui-core): :fire: remove placeholder accordion component

--- packages/ripple-ui-core/src/index.ts
+++ packages/ripple-ui-core/src/index.ts
@@ -1,5 +1,4 @@
 // Components
-export { default as RplAccordion } from './components/accordion/index.vue'
 export { default as RplButton } from './components/button/button.vue'
 export { default as RplIcon } from './components/icon/icon.vue'
 export { default as RplIconSprite } from './components/icon/sprite.vue'

b86c442f4f545244e3c9339e1513992a50e7c71d
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-06-07 17:43:59 +1000

    refactor(@dpc-sdp/ripple-ui-core): :recycle: update colour token logic

--- packages/ripple-ui-core/src/components/global/stories/colors.ts
+++ packages/ripple-ui-core/src/components/global/stories/colors.ts
@@ -1,18 +1,39 @@
 import get from 'lodash.get'
 import rplColors from './../../../../tokens/settings/color.yaml'
+import rplTheme from './../../../../tokens/settings/theme.yaml'
 
+export const getColorSwatchesCollection = (tokens, types = []) => {
+  const all = {}
+  types.map((type) => {
+    all[type] = getColorSwatches(tokens, type).value
+  })
+  return all
+}
 export const getColorSwatches = (tokens, type) => {
   if (!tokens || !tokens.hasOwnProperty('clr')) return null
   const category = get(tokens.clr, type)
   return Object.keys(category).reduce((acc, key) => {
-    const { comment, value } = category[key]
+    const { comment } = category[key]
+    let { value } = category[key]
+    if (key === 'value') {
+      value = category[key]
+    }
     const obKey = comment || key
     const getVal = (v) => {
       if (typeof v === 'string') {
         if (v.charAt(0) === '{') {
           const valKey = v.replace('{', '').replace('}', '')
-          const refVal = get(rplColors, valKey)
-          return refVal.value
+          let refVal = ''
+          if (valKey.startsWith('theme')) {
+            refVal = get(rplTheme, valKey)
+          } else {
+            refVal = get(rplColors, valKey).value
+          }
+          if (refVal.startsWith('#')) {
+            return refVal
+          } else {
+            return getVal(refVal)
+          }
         }
         return v
       }
@@ -20,9 +41,8 @@ export const getColorSwatches = (tokens, type) => {
     return {
       ...acc,
       [obKey]: {
-        ...category[key],
         var: `--rpl-clr-${type.replaceAll('.', '-')}${
-          key !== '_' ? '-' + key : ''
+          key !== '_' ? (key === 'value' ? '' : '-' + key) : ''
         }`,
         value: getVal(value)
       }

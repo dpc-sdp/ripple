c70ac9f5d7dac380adc48c1814d3f8ca06e56916
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-06-09 17:35:01 +1000

    build: :hammer: add components to nuxt and webcomponents targets

--- packages/ripple-ui-core/src/index.ts
+++ packages/ripple-ui-core/src/index.ts
@@ -3,5 +3,7 @@ export { default as RplAccordion } from './components/accordion/index.vue'
 export { default as RplButton } from './components/button/button.vue'
 export { default as RplIcon } from './components/icon/icon.vue'
 export { default as RplIconSprite } from './components/icon/sprite.vue'
+export { default as RplTextLink } from './components/text-link/text-link.vue'
+export { default as RplChip } from './components/chip/chip.vue'
 
 export { default as rplEventBus } from './lib/eventbus'

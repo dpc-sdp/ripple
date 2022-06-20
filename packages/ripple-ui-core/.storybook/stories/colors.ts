c5aa293368c59d98ea9b1daf67b40530c99f1f8b
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2022-06-20 19:35:02 +1000

    docs(@dpc-sdp/ripple-ui-core): :art: refactor color swatch functions

--- packages/ripple-ui-core/.storybook/stories/colors.ts
+++ packages/ripple-ui-core/.storybook/stories/colors.ts
@@ -1,41 +1,56 @@
 import { get } from 'lodash-es'
 import rplColors from './../../tokens/settings/color.yaml'
+import theme from './../../tokens/settings/theme.yaml'
+const tokens = { ...theme, ...rplColors }
 
-export const getColorSwatchesCollection = (tokens, types = []) => {
-  const all = {}
-  types.map((type) => {
-    all[type] = getColorSwatches(tokens, type).value
-  })
-  return all
+const getVal = (v: string) => {
+  if (typeof v === 'string') {
+    if (v.charAt(0) === '{') {
+      const valKey = v.replace('{', '').replace('}', '')
+      const refVal = get(tokens, valKey)
+      return getVal(refVal)
+    }
+    if (v.hasOwnProperty('value')) {
+      return v.value
+    }
+    return v
+  }
 }
 
-export const getColorSwatches = (tokens, type) => {
-  if (!tokens || !tokens.hasOwnProperty('clr')) return null
-  const category = get(tokens.clr, type)
-  return Object.keys(category).reduce((acc, key) => {
-    const { comment, value } = category[key]
-    const obKey = comment || key
-    const getVal = (v) => {
-      if (typeof v === 'string') {
-        if (v.charAt(0) === '{') {
-          const valKey = v.replace('{', '').replace('}', '')
-          const refVal = get(rplColors, valKey)
-          return refVal.value
-        }
-        return v
-      }
+const getVarName = (path: string[] | string) => {
+  const prefix = `--rpl-`
+  const normaliseArr = (val) => {
+    if (Array.isArray(val)) return val
+    if (typeof path === 'string') {
+      return path.split('.')
     }
-    return {
-      ...acc,
-      [obKey]: {
-        ...category[key],
-        var: `--rpl-clr-${type.replaceAll('.', '-')}${
-          key !== '_' ? '-' + key : ''
-        }`,
-        value: getVal(value)
-      }
-    }
-  }, {})
+    return []
+  }
+  return `${prefix}${normaliseArr(path)
+    .filter((i) => i !== '_')
+    .join('-')}`
+}
+
+export const getColorSwatches = (cat) => {
+  const category = get(tokens, cat)
+  if (!category) return null
+  return Object.keys(category)
+    .filter((key) => key !== 'value')
+    .map((key) => {
+      const arrPath = [...cat.split('.'), key]
+      return getColorSwatch(arrPath)
+    })
+}
+
+export const getColorSwatch = (path) => {
+  const match = get(tokens, path)
+  if (!match) return null
+  const value = getVal(match.value)
+  return {
+    ...match,
+    var: getVarName(path),
+    value
+  }
 }
 
 export default rplColors

4e6689ef79551b39dc0102c91bdeb1b00fc6fbac
dylankelly <dylan.kelly@dpc.vic.gov.au>
2022-06-23 12:50:38 +1000

    Move tideSearchApi to be enabled by default (#1217)

--- packages/ripple-nuxt-tide/lib/module.js
+++ packages/ripple-nuxt-tide/lib/module.js
@@ -84,11 +84,15 @@ const nuxtTide = function (moduleOptions) {
     this.addServerMiddleware(require('./server-middleware/basic-auth.js'))
   }
 
-  if (options.searchApi) {
+  if (options.searchApi || (!options.searchApi && options.modules?.landingPage?.contentCollection !== false)) {
+    const searchApiOptions = options.searchApi || {}
     this.addServerMiddleware(tideSearchApiMiddleware({
       templates: options.searchTemplates,
       tide: options,
-      ...options.searchApi
+      apiBase: 'search-api',
+      apiVersion: 'v2',
+      cacheAge: 30,
+      ...searchApiOptions
     }))
   }
 

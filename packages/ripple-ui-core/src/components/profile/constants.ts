bebae3f461ecc2d9bb8007e9c004e9b0767984e8
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-08-22 18:12:43 +1000

    feat(@dpc-sdp/ripple-ui-core): :sparkles: add profile component

---
+++ packages/ripple-ui-core/src/components/profile/constants.ts
@@ -0,0 +1,6 @@
+import { RplPropStringRequired } from '../../lib/constants'
+
+export const RplProfileItemArray = {
+  title: typeof RplPropStringRequired as string,
+  content: typeof RplPropStringRequired as string
+} as const
---
+++ packages/ripple-ui-core/src/components/profile/profile.vue
@@ -0,0 +1,33 @@
+<script lang="ts">
+export default { name: 'RplProfile' }
+</script>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { RplProfileItemArray } from './constants'
+import { RplPropStringRequired } from '../../lib/constants'
+
+defineProps({
+  image: RplPropStringRequired,
+  items: {
+    type: Array as PropType<typeof RplProfileItemArray[]>,
+    default: () => []
+  }
+})
+</script>
+
+<template>
+  <div class="rpl-profile">
+    <div class="rpl-profile__media">
+      <img :src="image" alt="" />
+    </div>
+    <div v-if="items.length > 0" class="rpl-profile__items">
+      <dl v-for="(item, index) of items" :key="index" class="rpl-profile__item">
+        <dt class="rpl-profile__title rpl-type-p">{{ item.title }}</dt>
+        <dd class="rpl-profile__content rpl-type-p">{{ item.content }}</dd>
+      </dl>
+    </div>
+  </div>
+</template>
+
+<style src="./profile.css" />

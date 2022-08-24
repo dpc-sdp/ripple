c79b30cf5cc665113458c59884e27719bf561726
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2022-08-24 14:12:25 +1000

    revert(@dpc-sdp/ripple-ui-core): revert content prop

--- packages/ripple-ui-core/src/components/content/content.vue
+++ packages/ripple-ui-core/src/components/content/content.vue
@@ -2,7 +2,6 @@
 export default { name: 'RplContent' }
 export type RplContentProps = {
   html: [string, undefined]
-  contained: boolean
 }
 </script>
 
@@ -11,12 +10,7 @@ defineProps<RplContentProps>()
 </script>
 
 <template>
-  <div
-    v-if="html"
-    class="rpl-content"
-    :class="{ 'rpl-content--contained': contained }"
-    v-html="html"
-  ></div>
+  <div v-if="html" class="rpl-content" v-html="html"></div>
   <div v-else class="rpl-content">
     <slot></slot>
   </div>

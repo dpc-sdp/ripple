13f3c59590d2b018683222defdeff630e61a5d44
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-08-26 11:16:54 +1000

    feat(@dpc-sdp/ripple-ui-core): :sparkles: add barebones related-links component

---
+++ packages/ripple-ui-core/src/components/related-links/constants.ts
@@ -0,0 +1,3 @@
+// This file is used to declare component specific constants. Delete if not needed
+
+export const RplRelatedLinksVariants = ['default', 'neutral'] as const
---
+++ packages/ripple-ui-core/src/components/related-links/related-links.vue
@@ -0,0 +1,20 @@
+<script lang="ts"> export default { name: 'RplRelatedLinks' }</script>
+
+<script setup lang="ts">
+import { PropType } from 'vue'
+import { RplRelatedLinksVariants } from './constants'
+defineProps({
+  variant: {
+    type: String as PropType<typeof RplRelatedLinksVariants[number]>,
+    default: RplRelatedLinksVariants[0]
+  }
+})
+</script>
+
+<template>
+  <div :class="`rpl-related-links rpl-related-links--${variant}`">
+    TODO: RplRelatedLinks functionality
+  </div>
+</template>
+
+<style src="./related-links.css" />

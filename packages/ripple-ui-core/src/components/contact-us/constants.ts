5ded6ce3a1bfdd7650e6d11d7179e57d5edff4fd
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-09-06 08:56:00 +1000

    refactor(@dpc-sdp/ripple-ui-core): :recycle: simplified types for contact-us and list components

--- packages/ripple-ui-core/src/components/contact-us/constants.ts
+++ packages/ripple-ui-core/src/components/contact-us/constants.ts
@@ -1,13 +1,5 @@
-import { RplPropLabel, RplPropUrl } from '../../lib/constants'
-
-export const RplContactUsDetailsArray = {
-  name: typeof RplPropLabel as string,
-  department: typeof RplPropLabel as string,
-  street: typeof RplPropLabel as string
-} as const
-
-export const RplContactUsItemArray = {
-  label: typeof RplPropLabel as string,
-  url: typeof RplPropUrl as string,
-  icon: String
-} as const
+export interface RplContactUsDetails {
+  name: string,
+  department: string,
+  street: string
+}

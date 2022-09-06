97ff9036921357d99f7630556d1e76f06560b737
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-09-06 11:00:09 +1000

    refactor(@dpc-sdp/ripple-ui-core): :recycle: simplified types for timeline component

--- packages/ripple-ui-core/src/components/timeline/constants.ts
+++ packages/ripple-ui-core/src/components/timeline/constants.ts
@@ -1,12 +1,10 @@
-import { RplPropLabel, RplPropUrl } from '../../lib/constants'
-
-export const RplTimelineItemArray = {
-  image: typeof RplPropLabel as string,
-  title: typeof RplPropLabel as string,
-  subtitle: typeof RplPropLabel as string,
-  dateStart: typeof RplPropLabel as string,
-  dateEnd: typeof RplPropLabel as string,
-  current: false as boolean,
-  description: typeof RplPropLabel as string,
-  url: typeof RplPropUrl as string
-} as const
+export interface RplTimelineItem {
+  image: string,
+  title: string,
+  subtitle: string,
+  dateStart: string,
+  dateEnd: string,
+  current: boolean,
+  description: string,
+  url: string
+}

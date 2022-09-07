f25b39ee9b586094a0e40d77b4cc565e99c68eaf
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-09-07 10:49:55 +1000

    revert(@dpc-sdp/ripple-ui-core): removed the unneeded 1px margin from the file icon
    R20-287

--- packages/ripple-ui-core/src/components/file/file.vue
+++ packages/ripple-ui-core/src/components/file/file.vue
@@ -1,10 +1,16 @@
-<script lang="ts"> export default { name: 'RplFile' }</script>
+<script lang="ts">
+export default { name: 'RplFile' }
+</script>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 import RplIcon from '../icon/icon.vue'
 import { isExternalLink } from '../../lib/helpers'
-import { RplPropLabel, RplPropStringRequired, RplPropUrl } from '../../lib/constants'
+import {
+  RplPropLabel,
+  RplPropStringRequired,
+  RplPropUrl
+} from '../../lib/constants'
 
 const props = defineProps({
   name: RplPropStringRequired,
@@ -12,7 +18,7 @@ const props = defineProps({
   extension: RplPropStringRequired,
   size: RplPropStringRequired,
   updated: RplPropLabel,
-  caption: RplPropLabel,
+  caption: RplPropLabel
 })
 
 const isExternal = computed(() => isExternalLink(props.url))
@@ -23,23 +29,40 @@ const isExternal = computed(() => isExternalLink(props.url))
     <a
       tabindex="-1"
       class="rpl-file__link"
-      :aria-label="`${name} File type: ${extension}. Size: ${size}. ${isExternal ? 'Opens in new tab.' : ''}`"
+      :aria-label="`${name} File type: ${extension}. Size: ${size}. ${
+        isExternal ? 'Opens in new tab.' : ''
+      }`"
       :href="url"
       :download="isExternal ? null : ''"
-      :target="isExternal ? '_blank' : null">
-      <div class="rpl-file__icon">
-        <RplIcon name="icon-document-lined" size="l" colour="default" />
-      </div>
+      :target="isExternal ? '_blank' : null"
+    >
+      <RplIcon name="icon-document-lined" size="l" colour="default" />
       <div class="rpl-file__info">
-        <span class="rpl-file__name rpl-type-p rpl-type-weight-bold rpl-u-focusable rpl-u-focusable--inline" tabindex="0">{{ name }}</span>
+        <span
+          class="
+            rpl-file__name
+            rpl-type-p
+            rpl-type-weight-bold
+            rpl-u-focusable
+            rpl-u-focusable--inline
+          "
+          tabindex="0"
+          >{{ name }}</span
+        >
         <div class="rpl-file__meta rpl-type-label-small">
           <span class="rpl-file__type">{{ extension }}</span>
           <span class="rpl-file__size">{{ size }}</span>
-          <div v-if="updated" class="rpl-file__updated">Updated {{ updated }}</div>
+          <div v-if="updated" class="rpl-file__updated">
+            Updated {{ updated }}
+          </div>
         </div>
       </div>
     </a>
-    <figcaption v-if="caption" class="rpl-file__caption rpl-type-p-small" v-html="caption"></figcaption>
+    <figcaption
+      v-if="caption"
+      class="rpl-file__caption rpl-type-p-small"
+      v-html="caption"
+    ></figcaption>
   </figure>
 </template>
 

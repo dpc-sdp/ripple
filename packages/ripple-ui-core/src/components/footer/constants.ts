caec0175cc87e5d94cc48fecc3579e5bebb41b2c
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-09-15 12:53:41 +1000

    chore(@dpc-sdp/ripple-ui-core): :art: removed unneeded comment

--- packages/ripple-ui-core/src/components/footer/constants.ts
+++ packages/ripple-ui-core/src/components/footer/constants.ts
@@ -1,5 +1,3 @@
-// This file is used to declare component specific constants. Delete if not needed
-
 export const vicGovHomeUrl = 'https://vic.gov.au';
 export const vicGovHomeLabel = 'Victoria government home';
 

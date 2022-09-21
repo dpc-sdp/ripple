e9ee4d9803474d91448bf4060e0dcd26ea2d5277
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-09-21 14:11:08 +1000

    feat(@dpc-sdp/ripple-ui-core): added the variable column solution that was requested

--- packages/ripple-ui-core/src/components/stats-grid/stats-grid.vue
+++ packages/ripple-ui-core/src/components/stats-grid/stats-grid.vue
@@ -3,6 +3,7 @@ export default { name: 'RplStatsGrid' }
 </script>
 
 <script setup lang="ts">
+import { computed, useSlots } from 'vue'
 import { StatsGridVariants } from './constants'
 
 interface Props {
@@ -12,6 +13,10 @@ interface Props {
 withDefaults(defineProps<Props>(), {
   variant: 'onLight'
 })
+
+const numItems = computed(() => {
+  return useSlots().default()[0].children.length
+})
 </script>
 
 <template>
@@ -19,7 +24,10 @@ withDefaults(defineProps<Props>(), {
     :class="{
       'rpl-stats-grid': true,
       'rpl-stats-grid--on-light': variant === 'onLight',
-      'rpl-stats-grid--on-dark': variant === 'onDark'
+      'rpl-stats-grid--on-dark': variant === 'onDark',
+      'rpl-stats-grid--2-cols': numItems <= 4,
+      'rpl-stats-grid--3-cols': numItems >= 5 && numItems <= 7,
+      'rpl-stats-grid--4-cols': numItems >= 8
     }"
   >
     <slot />

0acce420bbc72167d5e077dbd540c9fbcafc8b22
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-09-28 15:12:06 +1000

    feat(@dpc-sdp/ripple-ui-core): :sparkles: add new primary nav menu component

---
+++ packages/ripple-ui-core/src/components/primary-nav/nav-menu-list.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+export default { name: 'RplPrimaryNavMenuList' }
+</script>
+
+<script setup lang="ts">
+import RplPrimaryNavMenuAction from './nav-menu-action.vue'
+import { RplPrimaryNavItem } from './constants'
+
+interface Props {
+  parent: RplPrimaryNavItem
+  items: RplPrimaryNavItem[]
+}
+
+const props = withDefaults(defineProps<Props>(), {})
+</script>
+
+<template>
+  <ul class="rpl-primary-nav__nav-menu-list">
+    <!-- Repeat of parent as a clickable link -->
+    <li class="rpl-primary-nav__nav-menu-item">
+      <RplPrimaryNavMenuAction :item="props.parent" />
+    </li>
+
+    <!-- Children -->
+    <li v-for="(child, childIndex) in props.items" :key="childIndex">
+      <RplPrimaryNavMenuAction :item="child" />
+    </li>
+  </ul>
+</template>
--- packages/ripple-ui-core/src/components/primary-nav/nav-menu.vue
+++ packages/ripple-ui-core/src/components/primary-nav/nav-menu.vue
@@ -1,9 +1,10 @@
 <script lang="ts">
-export default { name: 'RplPrimaryNavBarMenu' }
+export default { name: 'RplPrimaryNavMenu' }
 </script>
 
 <script setup lang="ts">
-import RplPrimaryNavMenuAction from './nav-menu-action.vue'
+import { computed } from 'vue'
+import RplPrimaryNavMenuList from './nav-menu-list.vue'
 import { RplPrimaryNavItem } from './constants'
 
 interface Props {
@@ -11,24 +12,61 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {})
+
+const level2ActiveItem = computed(() => {
+  return props.item.items?.find((item) => item.active)
+})
+
+const level3ActiveItem = computed(() => {
+  return level2ActiveItem.value?.items?.find((item) => item.active)
+})
 </script>
 
 <template>
   <div class="rpl-primary-nav__nav-menu">
-    <div class="rpl-primary-nav__section-title rpl-type-h3-fixed">
+    <div class="rpl-primary-nav__nav-menu-section-title rpl-type-h3-fixed">
       {{ props.item.text }}
     </div>
 
-    <ul class="rpl-primary-nav__nav-menu-list">
-      <!-- Repeat of parent as a clickable link -->
-      <li class="rpl-primary-nav__nav-menu-item">
-        <RplPrimaryNavMenuAction :item="props.item" />
-      </li>
+    <!-- Level 2 -->
+    <div
+      class="
+        rpl-primary-nav__nav-menu-column
+        rpl-primary-nav__nav-menu-column--level-2
+      "
+    >
+      <RplPrimaryNavMenuList
+        :parent="props.item"
+        :items="props.item.items ? props.item.items : []"
+      />
+    </div>
 
-      <!-- Children -->
-      <li v-for="(child, childIndex) in props.item.items" :key="childIndex">
-        <RplPrimaryNavMenuAction :item="child" />
-      </li>
-    </ul>
+    <!-- Level 3 -->
+    <div
+      v-if="level2ActiveItem?.items?.length"
+      class="
+        rpl-primary-nav__nav-menu-column
+        rpl-primary-nav__nav-menu-column--level-3
+      "
+    >
+      <RplPrimaryNavMenuList
+        :parent="props.item"
+        :items="props.item.items ? props.item.items : []"
+      />
+    </div>
+
+    <!-- Level 4 -->
+    <div
+      v-if="level3ActiveItem?.items?.length"
+      class="
+        rpl-primary-nav__nav-menu-column
+        rpl-primary-nav__nav-menu-column--level-4
+      "
+    >
+      <RplPrimaryNavMenuList
+        :parent="props.item"
+        :items="props.item.items ? props.item.items : []"
+      />
+    </div>
   </div>
 </template>

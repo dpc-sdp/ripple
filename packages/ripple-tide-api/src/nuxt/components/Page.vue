c9c965c7495effc2ce6b94cd541746ea60b069e0
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-09-29 14:14:18 +1000

    feat(@dpc-sdp/ripple-ui-core): removed unused styles from tide-page

--- packages/ripple-tide-api/src/nuxt/components/Page.vue
+++ packages/ripple-tide-api/src/nuxt/components/Page.vue
@@ -1,10 +1,14 @@
 <template>
-  <slot v-if="page" :name="`${componentName}Page`" v-bind="{page, site}">
+  <slot v-if="page" :name="`${componentName}Page`" v-bind="{ page, site }">
     <component :is="`${componentName}Page`" :page="page">
       <template #aboveHeader>
         <RplIconSprite />
         <slot name="aboveHeader">
-          <RplAlert v-for="alert in site.alerts" v-bind="alert" :key="alert.alertId" />
+          <RplAlert
+            v-for="alert in site.alerts"
+            v-bind="alert"
+            :key="alert.alertId"
+          />
         </slot>
       </template>
       <template #primaryNav>
@@ -14,13 +18,21 @@
       </template>
       <template #breadcrumbs>
         <slot name="breadcrumbs">
-          <RplBreadcrumbs v-if="breadcrumbs" v-bind="breadcrumbs"></RplBreadcrumbs>
+          <RplBreadcrumbs
+            v-if="breadcrumbs"
+            v-bind="breadcrumbs"
+          ></RplBreadcrumbs>
         </slot>
       </template>
       <template #sidebar>
         <slot v-if="page.sidebar" name="sidebar" v-bind="page.sidebar">
           <RplContactUs v-if="page.sidebar" v-bind="page.sidebar?.contact" />
-          <RplVerticalNav v-if="page.sidebar" v-bind="page.sidebar?.contact" title="Section name" :items="[]" />
+          <RplVerticalNav
+            v-if="page.sidebar"
+            v-bind="page.sidebar?.contact"
+            title="Section name"
+            :items="[]"
+          />
         </slot>
       </template>
       <template #footer>
@@ -35,7 +47,11 @@
       <template #aboveHeader>
         <RplIconSprite />
         <slot v-if="site && site.alerts" name="aboveHeader">
-          <RplAlert v-for="alert in site.alerts" v-bind="alert" :key="alert.alertId" />
+          <RplAlert
+            v-for="alert in site.alerts"
+            v-bind="alert"
+            :key="alert.alertId"
+          />
         </slot>
       </template>
       <template #primaryNav>
@@ -45,7 +61,7 @@
       </template>
       <template #body>
         <!-- TODO: Add error handling in Error component -->
-        <h1>{{pageError.data?.error?.message}}</h1>
+        <h1>{{ pageError.data?.error?.message }}</h1>
       </template>
       <template #footer>
         <slot name="footer">
@@ -58,28 +74,35 @@
 
 <script setup lang="ts">
 // @ts-ignore
-import { useRoute, useRuntimeConfig, useFetch, useHead, useSiteTheme } from '#imports'
+import {
+  useRoute,
+  useRuntimeConfig,
+  useFetch,
+  useHead,
+  useSiteTheme
+} from '#imports'
 import { computed } from 'vue'
 import { pascalCase } from 'change-case'
 
 const route = useRoute()
 const config = useRuntimeConfig()
 // @ts-ignore
-const [{ data: site, error: siteError }, { data: page, error: pageError }] = await Promise.all([
-  useFetch('/api/tide/site', {
-    baseURL: config.API_URL || '',
-    params: {
-      id: config.SITEID
-    }
-  }),
-  useFetch('/api/tide/page', {
-    baseURL: config.API_URL || '',
-    params: {
-      path: route.path,
-      site: config.SITEID
-    }
-  })
-])
+const [{ data: site, error: siteError }, { data: page, error: pageError }] =
+  await Promise.all([
+    useFetch('/api/tide/site', {
+      baseURL: config.API_URL || '',
+      params: {
+        id: config.SITEID
+      }
+    }),
+    useFetch('/api/tide/page', {
+      baseURL: config.API_URL || '',
+      params: {
+        path: route.path,
+        site: config.SITEID
+      }
+    })
+  ])
 
 const componentName = computed(
   () => page.value && `Tide${pascalCase(page.value.type)}`
@@ -88,7 +111,10 @@ const componentName = computed(
 // TODO: Will need to implement breadcrumb business logic
 const breadcrumbs = computed(() => {
   return {
-    items: [{ label: 'Home', url: '/' }, { label: 'Page title', url: '/page-title' }]
+    items: [
+      { label: 'Home', url: '/' },
+      { label: 'Page title', url: '/page-title' }
+    ]
   }
 })
 
@@ -96,11 +122,11 @@ const style = useSiteTheme(site.value?.theme)
 useHead({
   title: page.value?.title,
   htmlAttrs: {
-    lang: page.value?.lang || 'en-AU',
+    lang: page.value?.lang || 'en-AU'
   },
   style: style && [
     {
-      children: `body { ${style} }`,
+      children: `body { ${style} }`
     }
   ],
   meta: [
@@ -110,22 +136,4 @@ useHead({
     }
   ]
 })
-
-
-
 </script>
-
-<style>
-.rpl-tide-page {
-  &__header {
-    padding: var(--rpl-sp-5);
-  }
-
-  &__body {
-    padding: var(--rpl-sp-5);
-    margin-left: auto;
-    margin-right: auto;
-  }
-
-}
-</style>

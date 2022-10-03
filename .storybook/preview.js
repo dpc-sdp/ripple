bdebb6f76ae22f6c809aef89714e2d0fd9ea3c2b
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-10-03 13:17:27 +1100

    refactor(@dpc-sdp/ripple-ui-core): replaced storybook backgrounds with a custom decorator
    the default backgrounds were causing issues with the axe tests

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -6,6 +6,7 @@ import { withDesign } from 'storybook-addon-designs'
 import VueSocialSharing from 'vue-social-sharing'
 import { app } from '@storybook/vue3'
 import themes from './themes.json'
+import withBackground from './utils/withBackground'
 // Storybook specific CSS
 import './storybook.css'
 
@@ -30,34 +31,19 @@ export const parameters = {
       date: /Date$/
     }
   },
-  backgrounds: {
-    default: 'light',
-    values: [
-      {
-        name: 'light',
-        value: 'white',
-      },
-      {
-        name: 'gray',
-        value: 'var(--rpl-clr-neutral-100)',
-      },
-      {
-        name: 'reverse',
-        value: 'var(--rpl-clr-primary-alt)',
-      },
-    ],
-  },
   designTokensCss: {
     label: "Themes",
     persistData: true,
     themes
   }
 }
+
 export const decorators = [
   withCssResources,
   (story) => ({
     components: { story, RplIconSprite },
     template: '<div><RplIconSprite /><story /></div>'
   }),
-  withDesign
+  withBackground,
+  withDesign,
 ]

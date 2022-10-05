cafec8e478c9b64af321c71dd622403e280fdbf4
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-10-05 14:19:55 +1100

    fix(@dpc-sdp/ripple-ui-core): :bug: update vertical nav layout story data

--- packages/ripple-ui-core/src/components/layout/storydata.ts
+++ packages/ripple-ui-core/src/components/layout/storydata.ts
@@ -1,34 +1,71 @@
 export const verticalNavItems = [
-  { text: 'First level', items: [
-    { text: 'First level repeat', url: '#' },
-    { text: 'Second level', url: '#', items: [
-      { text: 'Third level link with some text that will need to wrap', url: '#', items: [
-        { text: 'Fourth level', url: '#' }
-      ]}
-    ]},
-    { text: 'Second level active', url: '#' },
-    { text: 'Second level', url: '#' }
-  ]},
-  { text: 'First level', items: [
-    { text: 'First level repeat', url: '#' },
-    { text: 'Second level', url: '#', items: [
-      { text: 'Third level link with some text that will need to wrap', url: '#', items: [
-        { text: 'Fourth level', url: '#' }
-      ]}
-    ]},
-    { text: 'Second level', url: '#' },
-    { text: 'Second level', url: '#' }
-  ]},
-  { text: 'First level no children', url: '#' },
-  { text: 'First level no children active', url: '#' },
-  { text: 'First level', items: [
-    { text: 'First level repeat', url: '#' },
-    { text: 'Second level', url: '#', items: [
-      { text: 'Third level link with some text that will need to wrap', url: '#', items: [
-        { text: 'Fourth level', url: '#' }
-      ]}
-    ]},
-    { text: 'Second level', url: '#' },
-    { text: 'Second level', url: '#' }
-  ]}
+  {
+    id: '1',
+    text: 'First level',
+    active: true,
+    items: [
+      { id: '2', text: 'First level repeat', url: '#' },
+      {
+        id: '3',
+        text: 'Second level',
+        url: '#',
+        items: [
+          {
+            id: '4',
+            text: 'Third level link with some text that will need to wrap',
+            url: '#',
+            items: [{ id: '5', text: 'Fourth level', url: '#' }]
+          }
+        ]
+      },
+      { id: '6', text: 'Second level active', url: '#', active: true },
+      { id: '7', text: 'Second level', url: '#' }
+    ]
+  },
+  {
+    id: '8',
+    text: 'First level',
+    items: [
+      { id: '9', text: 'First level repeat', url: '#' },
+      {
+        id: '10',
+        text: 'Second level',
+        url: '#',
+        items: [
+          {
+            id: '11',
+            text: 'Third level link with some text that will need to wrap',
+            url: '#',
+            items: [{ id: '12', text: 'Fourth level', url: '#' }]
+          }
+        ]
+      },
+      { id: '13', text: 'Second level', url: '#' },
+      { id: '14', text: 'Second level', url: '#' }
+    ]
+  },
+  { id: '15', text: 'First level no children', url: '#' },
+  { id: '16', text: 'First level no children active', url: '#', active: true },
+  {
+    id: '17',
+    text: 'First level',
+    items: [
+      { id: '18', text: 'First level repeat', url: '#' },
+      {
+        id: '19',
+        text: 'Second level',
+        url: '#',
+        items: [
+          {
+            id: '20',
+            text: 'Third level link with some text that will need to wrap',
+            url: '#',
+            items: [{ id: '21', text: 'Fourth level', url: '#' }]
+          }
+        ]
+      },
+      { id: '22', text: 'Second level', url: '#' },
+      { id: '23', text: 'Second level', url: '#' }
+    ]
+  }
 ]

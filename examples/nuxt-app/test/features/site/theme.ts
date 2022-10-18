f4c9d2b921737f924435400e1b3ce154d42ba875
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-10-18 15:40:24 +1100

    test(@dpc-sdp/ripple-ui-core): :white_check_mark: update site theme test

--- examples/nuxt-app/test/features/site/theme.ts
+++ examples/nuxt-app/test/features/site/theme.ts
@@ -3,6 +3,10 @@ import { Then } from '@badeball/cypress-cucumber-preprocessor'
 Then(
   'the site header background color should be {string}',
   (colour: string) => {
-    cy.get(`.rpl-primary-nav`).should('have.css', 'background-color', colour)
+    cy.get(`.rpl-primary-nav__inner`).should(
+      'have.css',
+      'background-color',
+      colour
+    )
   }
 )

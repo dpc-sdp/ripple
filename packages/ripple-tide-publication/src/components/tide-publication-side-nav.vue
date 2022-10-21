f901f4f8884c303cad950f1401c181a42f00951e
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-10-21 18:14:00 +1100

    fix: :green_heart: update fixture

--- packages/ripple-tide-publication/src/components/tide-publication-side-nav.vue
+++ packages/ripple-tide-publication/src/components/tide-publication-side-nav.vue
@@ -3,7 +3,7 @@ export default { name: 'TidePublicationSideNav' }
 </script>
 
 <template>
-  <div class="rpl-type-label">[ SIDE NAV ]</div>
+  <div class="rpl-type-label rpl-u-margin-b-6">[ SIDE NAV ]</div>
 </template>
 
 <script setup lang="ts"></script>

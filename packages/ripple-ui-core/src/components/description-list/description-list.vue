f62278b2d807c44643dc1a94df000c7d23a64c65
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2022-11-02 15:49:19 +1100

    feat(@dpc-sdp/ripple-ui-core): changed the way the inline description list is handled

--- packages/ripple-ui-core/src/components/description-list/description-list.vue
+++ packages/ripple-ui-core/src/components/description-list/description-list.vue
@@ -5,17 +5,33 @@ export default { name: 'RplDescriptionList' }
 <script setup lang="ts">
 import { RplDescriptionListItem } from './constants'
 
-defineProps<{
+interface Props {
+  inline: boolean
   items: Array<RplDescriptionListItem>
-}>()
+}
+
+withDefaults(defineProps<Props>(), {
+  inline: false,
+  items: () => []
+})
 </script>
 
 <template>
-  <dl class="rpl-description-list rpl-type-p">
-    <template v-for="row in items">
-      <template v-for="(description, term) in row" :key="term">
-        <dt class="rpl-description-list__term">{{ term }}</dt>
-        <dd class="rpl-description-list__description">{{ description }}</dd>
+  <dl
+    :class="{
+      'rpl-description-list': true,
+      'rpl-description-list--inline': inline,
+      'rpl-type-p': true
+    }"
+  >
+    <template v-for="row in items" :key="row.term">
+      <div v-if="inline" class="rpl-description-list__inline-wrap">
+        <dt class="rpl-description-list__term">{{ row.term }}</dt>
+        <dd class="rpl-description-list__description">{{ row.description }}</dd>
+      </div>
+      <template v-else>
+        <dt class="rpl-description-list__term">{{ row.term }}</dt>
+        <dd class="rpl-description-list__description">{{ row.description }}</dd>
       </template>
     </template>
   </dl>

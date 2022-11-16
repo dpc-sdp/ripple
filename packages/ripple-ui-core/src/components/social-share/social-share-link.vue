561ff91bf9d7a68dc87c5f8b241501d51a06fefc
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-11-16 11:29:39 +1100

    refactor(@dpc-sdp/ripple-ui-core): :recycle: use RplTextLink with default url

--- packages/ripple-ui-core/src/components/social-share/social-share-link.vue
+++ packages/ripple-ui-core/src/components/social-share/social-share-link.vue
@@ -5,6 +5,7 @@ export default { name: 'RplSocialShareLink' }
 <script setup lang="ts">
 import { computed } from 'vue'
 import RplIcon from '../icon/icon.vue'
+import RplTextLink from '../text-link/text-link.vue'
 import { RplSocialShareNetworks } from './constants'
 import { usePopupWindow } from '../../composables/usePopupWindow'
 
@@ -18,30 +19,26 @@ const props = defineProps<Props>()
 
 const key = computed(() => props.network.toLowerCase())
 
-const shareTemplate = () =>
+const shareTemplate = computed(() =>
   RplSocialShareNetworks[props.network]
     .replace('$t', encodeURIComponent(props.title))
     .replace('$u', encodeURIComponent(props.url))
+)
 
-const share = usePopupWindow(shareTemplate(), key.value)
+const share = usePopupWindow(shareTemplate.value, key.value)
 </script>
 
 <template>
-  <div
-    class="
-      rpl-social-share-link
-      rpl-text-link
-      rpl-u-focusable-inline
-      rpl-type-p-small
-    "
-    tabindex="0"
+  <RplTextLink
+    :url="shareTemplate"
     :aria-label="`Share this page on ${network}`"
+    class="rpl-social-share-link rpl-type-p-small"
     @click.prevent="share"
     @keydown.enter="share"
   >
     <RplIcon class="rpl-social-share__icon" :name="`icon-${key}`"></RplIcon>
     <span>{{ network }}</span>
-  </div>
+  </RplTextLink>
 </template>
 
 <style src="./social-share.css" />

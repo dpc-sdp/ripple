bdce18d00101f323cdadf1bea0b9a1d53a66cc52
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-11-18 14:18:14 +1100

    feat(@dpc-sdp/ripple-ui-core): :sparkles: add image variants to media-embed

--- packages/ripple-ui-core/src/components/media-embed/media-embed.vue
+++ packages/ripple-ui-core/src/components/media-embed/media-embed.vue
@@ -4,12 +4,21 @@ export default { name: 'RplMediaEmbed' }
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
+import {
+  RplMediaEmbedTypes,
+  RplMediaEmbedVariants,
+  RplMediaEmbedSizes
+} from './constants'
+import RplImage from '../image/image.vue'
 import RplIcon from '../icon/icon.vue'
 import RplMediaFullscreen from '../media-fullscreen/media-fullscreen.vue'
 import RplContent from '../content/content.vue'
 import RplExpandable from '../expandable/expandable.vue'
 
 interface Props {
+  type: RplMediaEmbedTypes
+  variant?: RplMediaEmbedVariants
+  size?: RplMediaEmbedSizes
   title: string
   src: string
   showTitle?: boolean
@@ -22,17 +31,56 @@ interface Props {
 }
 
 const props = withDefaults(defineProps<Props>(), {
-  showTitle: true,
+  variant: undefined,
+  size: undefined,
+  showTitle: false,
   transcriptUrl: undefined,
   caption: undefined,
   sourceCaption: undefined,
-  allowFullscreen: true,
+  allowFullscreen: false,
   dataContent: undefined,
   downloadUrl: undefined
 })
 
 const isFullScreenOpen = ref(false)
 const isDataContentOpen = ref(false)
+
+const imageAspect = computed(() => {
+  if (props.type != 'image') {
+    return undefined
+  }
+
+  switch (props.variant) {
+    case 'landscape':
+      return 'wide'
+    case 'portrait':
+      return 'full'
+    case 'square':
+      return 'square'
+    case 'avatar':
+      return 'square'
+    default:
+      return undefined
+  }
+})
+
+const imageClasses = computed(() => {
+  if (props.type != 'image') {
+    return undefined
+  }
+
+  let classes = 'rpl-media-embed__image '
+
+  if (props.variant) {
+    classes += `rpl-media-embed__image--${props.variant} `
+  }
+
+  if (props.size) {
+    classes += `rpl-media-embed__image--${props.size} `
+  }
+
+  return classes
+})
 
 const isActionsListEmpty = computed(() => {
   if (
@@ -57,12 +105,20 @@ const isActionsListEmpty = computed(() => {
 
     <!-- Figure (iframe, caption, source info) -->
     <figure class="rpl-media-embed__figure">
-      <iframe
-        class="rpl-media-embed__iframe"
-        :title="props.title"
+      <!-- Image -->
+      <RplImage
+        v-if="props.type == 'image'"
         :src="props.src"
-      ></iframe>
+        :alt="props.caption"
+        :aspect="imageAspect"
+        :circle="props.variant == 'avatar'"
+        :class="imageClasses"
+      />
 
+      <!-- Video -->
+      <div v-else-if="props.type == 'video'">[VIDEO EMBED WILL GO HERE]</div>
+
+      <!-- Caption and source caption -->
       <figcaption
         v-if="props.caption || props.sourceCaption"
         class="rpl-media-embed__figcaption"

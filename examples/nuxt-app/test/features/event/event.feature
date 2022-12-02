5819c6ebe931614b19b57edf587fd8c3207d475b
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-12-02 16:44:38 +1100

    test: :white_check_mark: fix breaking tests, update fixtures

--- examples/nuxt-app/test/features/event/event.feature
+++ examples/nuxt-app/test/features/event/event.feature
@@ -2,9 +2,11 @@ Feature: Event page
 
   Example of mocked page
 
-  Example: On load
-    Given the mock server has started
+  Background:
     And the endpoint "/api/tide/page" with query "?path=/sample-event&site=8888" returns fixture "/event/sample-event" with status 200
     And the endpoint "/api/tide/site" with query "?id=8888" returns fixture "/site/reference" with status 200
+
+  @mockserver
+  Example: On load
     Given I visit the page "/sample-event"
-    Then the title should be "Sample Event"
+    Then the title should be "Demo Event"
--- examples/nuxt-app/test/features/event/event.ts
+++ examples/nuxt-app/test/features/event/event.ts
@@ -1,5 +1,15 @@
-import { Then } from '@badeball/cypress-cucumber-preprocessor'
+import { Then, Before, After } from '@badeball/cypress-cucumber-preprocessor'
+
+Before({ tags: '@mockserver' }, () => {
+  cy.log('the mock server has started')
+  cy.task('startMockServer')
+})
+
+After({ tags: '@mockserver' }, () => {
+  cy.log('the mock server has stopped')
+  cy.task('stopMockServer')
+})
 
 Then('the title should be {string}', (title: string) => {
-  cy.get('[data-cy="title"]').should('have.text', title)
+  cy.get('[data-cy="hero-title"]').should('have.text', title)
 })

fd1d16e2fc2614163a9a691a50204af3a114cb52
David Featherston <david.featherstone@dpc.vic.gov.au>
2022-12-06 15:35:34 +1100

    feat(@dpc-sdp/ripple-tide-api): add margin to topic tags (same margin as updated date)

--- packages/ripple-tide-api/src/nuxt/components/BaseLayout.vue
+++ packages/ripple-tide-api/src/nuxt/components/BaseLayout.vue
@@ -34,7 +34,7 @@
     </template>
     <template #body="{ hasSidebar }">
       <slot name="body" :hasSidebar="hasSidebar"></slot>
-      <div data-cy="topic-tags">
+      <div data-cy="topic-tags" class="rpl-u-margin-t-6">
         <RplChip
           v-for="tag in topicTags"
           :key="tag.url"

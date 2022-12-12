f7dfff87a9a42e18399c943938ec88a757e7271d
Christopher Campbell <christopher.campbell@dpc.vic.gov.au>
2022-12-12 13:26:03 +1100

    test(@dpc-sdp/ripple-ui-core): :white_check_mark: change no-unused-vars back to warn from error

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/no-explicit-any': 'off', // allow explicit any types for now
     '@typescript-eslint/no-unused-vars': [
-      'error',
+      'warn',
       {
         varsIgnorePattern: 'props'
       }

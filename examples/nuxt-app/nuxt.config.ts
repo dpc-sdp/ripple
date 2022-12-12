3cdc65c159fba8803676613db1da8df674b7389a
Jason Smith <jason.smith@dpc.vic.gov.au>
2022-12-12 16:28:21 +1100

    test: :white_check_mark: add missing cy test env, update tests for consistency

--- examples/nuxt-app/nuxt.config.ts
+++ examples/nuxt-app/nuxt.config.ts
@@ -1,4 +1,9 @@
 import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      API_URL: ''
+    }
+  },
   modules: ['@dpc-sdp/ripple-tide-api/nuxt']
 })

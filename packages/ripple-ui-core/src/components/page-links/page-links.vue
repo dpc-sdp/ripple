57de4090f0e521e4cfb336ce2a0f7f3b5ebe14a8
David Featherston <david.featherstone@dpc.vic.gov.au>
2023-01-03 10:52:30 +1100

    fix(@dpc-sdp/ripple-ui-core): page links disappeared from storybook

--- packages/ripple-ui-core/src/components/page-links/page-links.vue
+++ packages/ripple-ui-core/src/components/page-links/page-links.vue
@@ -20,7 +20,7 @@ withDefaults(defineProps<Props>(), {
 <template>
   <nav class="rpl-page-links">
     <RplPageLinksItem
-      v-if="prev && prev.text"
+      v-if="prev && prev.url"
       :link="prev"
       :label="prev.text || 'Previous'"
       icon-name="icon-arrow-left"
@@ -28,7 +28,7 @@ withDefaults(defineProps<Props>(), {
       {{ prev.description }}
     </RplPageLinksItem>
     <RplPageLinksItem
-      v-if="next && next.text"
+      v-if="next && next.url"
       :link="next"
       :label="next.text || 'Next'"
       icon-name="icon-arrow-right"

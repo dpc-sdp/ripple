7dc6dd6dd8ac9ecad3b6256afd6e4617860550e3
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2023-01-06 17:02:16 +1100

    fix: :memo: add comments for example app usage

--- examples/nuxt-app/nuxt.config.ts
+++ examples/nuxt-app/nuxt.config.ts
@@ -5,12 +5,15 @@ export default defineNuxtConfig({
       API_URL: '',
       tide: {
         contentApi: {
+          // These can be overridden via environment variables eg: NUXT_PUBLIC_TIDE_CONTENT_API_BASE_URL
+          // See https://nuxt.com/docs/api/configuration/nuxt-config#runtimeconfig for more info
           baseUrl: 'https://develop.content.reference.sdp.vic.gov.au',
           site: '8888'
         }
       }
     }
   },
+  // See https://nuxt.com/docs/getting-started/layers - each content type is a added as a Nuxt layer
   extends: [
     '@dpc-sdp/nuxt-ripple',
     '@dpc-sdp/ripple-tide-event',

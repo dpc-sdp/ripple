4b4345c85be60b6f028b11bf1e6e530ad5faacd9
Jeff Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2023-01-12 16:11:47 +1100

    Revert "[SDPAP-7121] - Removed of "Campaign with video" functionality from secondary campaign (#1271)"
    This reverts commit ae2f417729d5d209ca69178c092ac786eda971db.

--- packages/ripple-nuxt-tide/modules/profile/tide.config.js
+++ packages/ripple-nuxt-tide/modules/profile/tide.config.js
@@ -3,6 +3,7 @@ module.exports = {
     profile: [
       'field_profile_category',
       'field_landing_page_c_secondary.field_block_image.field_media_image',
+      'field_landing_page_c_secondary.field_block_embedded_video',
       'field_landing_page_contact.field_paragraph_phones',
       'field_landing_page_contact.field_paragraph_social_media'
     ]

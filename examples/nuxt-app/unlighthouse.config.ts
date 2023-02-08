e7aa9e7640daa306db0660ffbcf25c73d9fa3f6a
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2023-02-08 18:49:50 +1100

    ci: lower perf budget

--- examples/nuxt-app/unlighthouse.config.ts
+++ examples/nuxt-app/unlighthouse.config.ts
@@ -8,7 +8,7 @@ export default {
   urls: mockRoutes.map((route: { path: string }) => route.path),
   ci: {
     budget: {
-      performance: 95,
+      performance: 90,
       accessibility: 90,
       'best-practices': 90,
       seo: 80

847d14b74bd9703c779817a2bfcdd191e744b6a9
David Featherston <david.featherstone@dpc.vic.gov.au>
2023-03-06 11:21:31 +1100

    docs(docs): remove commented out lines

--- packages/ripple-ui-core/src/index.ts
+++ packages/ripple-ui-core/src/index.ts
@@ -4,13 +4,3 @@ export { formatDateRange } from './lib/formatDateRange'
 export { default as getGrantStatus } from './utils/getGrantStatus'
 export { formatDate } from './lib/helpers'
 export { RplIconGroups } from './components/icon/constants'
-
-// export {
-//   tokens,
-//   getColorSwatches,
-//   getColorSwatchCollection
-// } from './lib/colors'
-
-// import rplColors from './../tokens/settings/color.yaml'
-// import theme from './../tokens/settings/theme.yaml'
-// export const tokens = { ...theme, ...rplColors }

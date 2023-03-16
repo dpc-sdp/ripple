5b0c6e18966486d49e27757dcce7b9609722311c
David Featherston <david.featherstone@dpc.vic.gov.au>
2023-03-16 15:34:08 +1100

    feat(@dpc-sdp/nuxt-ripple-analytics): wip

--- packages/nuxt-ripple-analytics/lib/index.ts
+++ packages/nuxt-ripple-analytics/lib/index.ts
@@ -5,10 +5,10 @@ export default {
     return (payload: any) => {
       gaEvent({
         event: payload.action === 'open' ? 'open_accordion' : 'close_accordion',
-        element_id: payload.id,
-        element_text: payload.label,
-        name: payload.name,
-        platform_event: 'rpl-accordion/toggleAll',
+        element_id: payload?.id,
+        element_text: payload?.label,
+        name: payload?.name,
+        platform_event: 'toggleAll',
         component: 'rpl-accordion'
       })
     }

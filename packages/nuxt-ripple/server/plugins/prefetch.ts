bad47a68e77bf590113c8d44da26a554ff6918bf
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2023-04-18 16:46:16 +1000

    perf(@dpc-sdp/nuxt-ripple): :zap: disable link prefetching

---
+++ packages/nuxt-ripple/server/plugins/prefetch.ts
@@ -0,0 +1,31 @@
+import * as cheerio from 'cheerio'
+import type { NitroApp } from 'nitropack'
+
+// fix type stub - See https://github.com/nuxt/nuxt/issues/18556
+export type NitroAppPlugin = (nitro: NitroApp) => void
+function defineNitroPlugin(def: NitroAppPlugin): NitroAppPlugin {
+  return def
+}
+
+export default defineNitroPlugin((nitroApp) => {
+  nitroApp.hooks.hook('render:html', (html) => {
+    /**
+     * @WORKAROUND: remove auto generated <link rel="prefetch" /> tags from the head
+     * currently it automatically adds prefetch tags for all assets imported in the page
+     */
+    // https://github.com/nuxt/nuxt/issues/18376#issuecomment-1431318970
+    // https://github.com/nuxt/nuxt/issues/18376
+
+    html.head = html.head.map((head) => {
+      const $ = cheerio.load(head)
+      $('link[rel=prefetch]').each(function () {
+        $(this).remove()
+      })
+      $('link[rel=preload]').each(function () {
+        $(this).remove()
+      })
+
+      return $.root().find('head').contents()
+    })
+  })
+})

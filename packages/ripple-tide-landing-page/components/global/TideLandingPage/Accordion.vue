588321dd8e3dd38b0e497e915e23cb291fbc34bf
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2023-04-28 10:43:15 +1000

    fix(@dpc-sdp/ripple-tide-landing-page): removed double accordion title

--- packages/ripple-tide-landing-page/components/global/TideLandingPage/Accordion.vue
+++ packages/ripple-tide-landing-page/components/global/TideLandingPage/Accordion.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 defineProps<{
   id: string
-  title: string
   numbered: boolean
   items: Array<any>
 }>()
 </script>
 
 <template>
-  <h2 v-if="title" class="rpl-type-h2">{{ title }}</h2>
   <RplAccordion :id="id" :items="items" :numbered="numbered" />
 </template>

bb55d56ea81a0d5bcfe8958074441a3cf60249fb
David Featherston <david.featherstone@dpc.vic.gov.au>
2023-05-29 09:00:46 +1000

    fix(@dpc-sdp/ripple-ui-core): fix complex image unit test

--- packages/ripple-tide-landing-page/mapping/components/complex-image/complex-image-mapping.test.ts
+++ packages/ripple-tide-landing-page/mapping/components/complex-image/complex-image-mapping.test.ts
@@ -10,12 +10,15 @@ const result: TideDynamicPageComponent<ITideComplexImage> = {
     title: 'Complex Image Title',
     sourceCaption: 'Complex Image Source',
     dataContent: '<p>Image Data</p>',
+    dataLabel: 'Complex Image Additional data',
     src: 'https://develop.content.reference.sdp.vic.gov.au/sites/default/files/tide_demo_content/2018-19-State-Budget.jpg',
     downloadUrl:
       'https://develop.content.reference.sdp.vic.gov.au/sites/default/files/tide_demo_content/2018-19-State-Budget.jpg',
+    downloadLabel: 'Complex Image Download',
     type: 'image',
     variant: 'complex',
     allowFullscreen: true,
+    fullscreenLabel: 'Complex Image Fullscreen',
     showTitle: true
   }
 }

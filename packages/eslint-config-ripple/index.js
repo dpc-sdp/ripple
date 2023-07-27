8f178e02caa17b7e92a37876e0ed53be907123fe
Jason Smith <jason.smith@dpc.vic.gov.au>
2023-07-27 15:29:14 +1000

    refactor: :recycle: match existing CI state

--- packages/eslint-config-ripple/index.js
+++ packages/eslint-config-ripple/index.js
@@ -5,22 +5,36 @@ module.exports = {
     node: true,
     'vue/setup-compiler-macros': true
   },
-  extends: ['prettier', 'plugin:storybook/recommended', '@nuxt/eslint-config'],
-  rules: {
-    // override/add rules settings here
-    'vue/multi-word-component-names': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'vue/first-attribute-linebreak': 'off',
-    'vue/attribute-hyphenation': 'off',
-    'vue/no-v-html': 'off',
-    'no-prototype-builtins': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
-    '@typescript-eslint/no-explicit-any': 'off', // allow explicit any types for now
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        varsIgnorePattern: 'props'
+  extends: ['@nuxt/eslint-config'],
+  overrides: [
+    {
+      files: ['*.{vue,ts,js}'],
+      rules: {
+        // Override/add rules settings here
+        'no-prototype-builtins': 'off',
+        'vue/attribute-hyphenation': 'off',
+        'vue/first-attribute-linebreak': 'off',
+        'vue/max-attributes-per-line': 'off',
+        'vue/multi-word-component-names': 'off',
+        'vue/no-v-html': 'off',
+        '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/no-explicit-any': 'off', // allow explicit any types for now
+        '@typescript-eslint/no-unused-vars': [
+          'warn',
+          {
+            varsIgnorePattern: 'props'
+          }
+        ],
+
+        // Compatibility rules - remove when CI passes
+        'no-extra-semi': 'off',
+        'prefer-const': 'off',
+        'vue/html-closing-bracket-newline': 'off',
+        'vue/html-indent': 'off',
+        'vue/html-self-closing': 'off',
+        'vue/multiline-html-element-content-newline': 'off',
+        'vue/singleline-html-element-content-newline': 'off'
       }
-    ]
-  }
+    }
+  ]
 }

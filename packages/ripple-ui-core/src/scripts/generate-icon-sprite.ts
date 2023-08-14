9489903fbdb4004a68e383a0f6f2ffe514a2e3cc
Jason Smith <jason.smith@dpc.vic.gov.au>
2023-08-14 16:00:20 +1000

    refactor(@dpc-sdp/ripple-ui-core): :recycle: move icons to core, update generate script

--- packages/ripple-ui-core/src/scripts/generate-icon-sprite.ts
+++ packages/ripple-ui-core/src/scripts/generate-icon-sprite.ts
@@ -1,7 +1,13 @@
-import { saveSprite } from './../lib/svg-icon-utils'
+import { saveSprite } from './../lib/svg-icon-utils.js'
 import { join } from 'pathe'
 
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 saveSprite(
-  join(__dirname, './../src/assets/icons/core'),
-  join(__dirname, './../src/assets/icons')
+  join(__dirname, './../../src/assets/icons/core'),
+  join(__dirname, './../../src/assets/icons')
 )

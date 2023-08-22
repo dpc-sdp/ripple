52970c235af0a513a72c0e2283b8a60357fa9453
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2023-08-22 17:08:22 +1000

    refactor(@dpc-sdp/ripple-tide-search): :sparkles: use backend json field

--- packages/ripple-tide-search/mapping/components/custom-collection/custom-collection-mapping.ts
+++ packages/ripple-tide-search/mapping/components/custom-collection/custom-collection-mapping.ts
@@ -1,5 +1,5 @@
 import { TideDynamicPageComponent } from '@dpc-sdp/ripple-tide-api/types'
-import { getSearchListingConfig } from './../../utils'
+import { parseJSONField } from './../../utils'
 
 export const customCollectionMapping = (
   field
@@ -8,49 +8,7 @@ export const customCollectionMapping = (
     component: 'TideCustomCollection',
     id: field.drupal_internal__id.toString(),
     props: {
-      // ...getSearchListingConfig(field.field_custom_collection_config)
-      // TODO: This is hard coded here for demo purposes until we can update the JSON schema on the backend to support the fields here
-      searchListingConfig: {
-        searchProvider: 'elasticsearch',
-        index: 'sdp_data_pipelines_scl',
-        resultsPerPage: 5,
-        labels: {
-          submit: 'Search',
-          placeholder: 'Enter suburb, postcode, streetname or offence location'
-        }
-      },
-      queryConfig: {
-        multi_match: {
-          query: '{{query}}',
-          fields: ['suburb^3', 'street^2', 'offence_location']
-        }
-      },
-      resultsConfig: {
-        layout: {
-          component: 'TideSearchResultsTable',
-          props: {
-            columns: [
-              {
-                label: 'Suburb',
-                key: 'suburb'
-              },
-              {
-                label: 'Location',
-                key: 'street'
-              },
-              {
-                label: 'Last annual test',
-                key: 'last_annual_test'
-              },
-              {
-                label: 'Certificate',
-                key: 'certificate',
-                component: 'CSLCertificateLink'
-              }
-            ]
-          }
-        }
-      }
+      ...parseJSONField(field.field_custom_collection_config)
     }
   }
 }

be2a9af799f1bb10ae0a62b91f061bd392585da9
David Featherston <david.featherstone@dpc.vic.gov.au>
2023-08-24 13:25:07 +1000

    feat(@dpc-sdp/ripple-tide-search): column classes, array-ify-ing, filter empty params
    add support for column classes, only array-ify TideSearchFilterDropdown, don't push empty values to the URL

--- packages/ripple-tide-search/composables/useTideSearch.ts
+++ packages/ripple-tide-search/composables/useTideSearch.ts
@@ -120,9 +120,11 @@ export default (
   const userFilters = computed(() => {
     return Object.keys(filterForm.value).map((key: string) => {
       const itm = userFilterConfig.find((itm: any) => itm.id === key)
+      let filterVal = filterForm.value[key]
 
-      const filterVal =
-        filterForm.value[key] && Array.from(filterForm.value[key])
+      if (itm.component === 'TideSearchFilterDropdown') {
+        filterVal = filterForm.value[key] && Array.from(filterForm.value[key])
+      }
 
       // Need to work out if form has value - will be different for different controls
       const hasValue = (v: unknown) => {
@@ -321,12 +323,17 @@ export default (
    * Updates the URL to trigger a new search, always returns to page 1 to avoid empty pages
    */
   const submitSearch = async () => {
+    const filterFormValues = Object.fromEntries(
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      Object.entries(filterForm.value).filter(([key, value]) => value)
+    )
+
     await navigateTo({
       path: route.path,
       query: {
         page: 1,
         q: searchTerm.value || undefined,
-        ...filterForm.value
+        ...filterFormValues
       }
     })
   }

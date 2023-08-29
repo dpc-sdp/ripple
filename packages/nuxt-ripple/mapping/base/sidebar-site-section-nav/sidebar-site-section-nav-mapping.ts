4d6a34692bb657c75b62f4e09f9903ca4249828e
Dylan Kelly <dylan.kelly@dpc.vic.gov.au>
2023-08-29 12:46:21 +1000

    fix(@dpc-sdp/ripple-tide-api): :bug: fetch menu for unpublished pages

--- packages/nuxt-ripple/mapping/base/sidebar-site-section-nav/sidebar-site-section-nav-mapping.ts
+++ packages/nuxt-ripple/mapping/base/sidebar-site-section-nav/sidebar-site-section-nav-mapping.ts
@@ -1,24 +1,16 @@
-import { TidePageApi } from '@dpc-sdp/ripple-tide-api'
+import { TidePageApi, getSiteSection } from '@dpc-sdp/ripple-tide-api'
 
 export const map = async (src, tidePageApi: TidePageApi) => {
   if (!src.field_show_site_section_nav) {
     return null
   }
 
-  // The section/site id comes from the drupal 'route' api
-  const siteId = tidePageApi.sectionId
+  // With the correct site/section id, we can now choose the correct site data from 'field_node_site'
+  const siteData = getSiteSection(tidePageApi.sectionId, src)
 
-  // For share links and preview we dont have the sectionId so cant fetch the menu
-  if (!siteId) {
-    // TODO : The route endpoint loops through all sites and chooses the last one, we should do this here only
-    // this will be a performance hit but we probably dont care for unpublished items
+  if (!siteData) {
     return null
   }
-
-  // With the correct site/section id, we can now choose the correct site data from 'field_node_site'
-  const siteData = src.field_node_site.find((site) => {
-    return `${site.drupal_internal__tid}` === siteId
-  })
 
   // Finally, this site data will contain a reference to the menu we need to fetch
   const menuData = siteData.field_site_main_menu

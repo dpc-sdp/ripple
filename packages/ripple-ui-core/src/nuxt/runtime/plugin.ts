2bdc083c2071a00401a443fe142f741e61348640
David Featherston <david.featherstone@dpc.vic.gov.au>
2023-09-15 09:58:04 +1000

    feat(@dpc-sdp/ripple-ui-core): allow supplying custom rpl icons

--- packages/ripple-ui-core/src/nuxt/runtime/plugin.ts
+++ packages/ripple-ui-core/src/nuxt/runtime/plugin.ts
@@ -1,10 +1,13 @@
+import icons from '#icons'
 import { rplEventBus } from './../../lib/eventbus'
 /* @ts-ignore */
 import { defineNuxtPlugin } from '#imports'
+
 /* @ts-ignore */
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use({
     install: (app) => {
+      app.provide('rplIcons', icons)
       app.provide('$rplEvent', rplEventBus)
     }
   })

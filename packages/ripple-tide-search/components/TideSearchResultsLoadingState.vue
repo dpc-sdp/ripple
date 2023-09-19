5ed78686cef18fac8391bf0ca6a9e01f58b291d8
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2023-09-19 10:33:13 +1000

    feat(@dpc-sdp/ripple-tide-search): added sort dropdown to custom collection
    also added loading state and tidied up error display

---
+++ packages/ripple-tide-search/components/TideSearchResultsLoadingState.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="{ 'tide-search-results--loading': isActive }">
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  isActive?: boolean
+}
+
+withDefaults(defineProps<Props>(), {
+  isActive: false
+})
+</script>
+
+<style>
+.tide-search-results--loading {
+  opacity: 0.5;
+  pointer-events: none;
+}
+</style>

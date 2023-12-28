acc30c915af441831a7f74d7a58694530c4fc270
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2023-12-29 08:55:56 +1100

    fix(@dpc-sdp/nuxt-ripple): fixed 'A composable that requires access to the Nuxt instance...' error

--- packages/nuxt-ripple/composables/use-tide-site.ts
+++ packages/nuxt-ripple/composables/use-tide-site.ts
@@ -4,8 +4,9 @@ export const useTideSite = async (id?: number): Promise<TideSiteData> => {
   const { public: config } = useRuntimeConfig()
   const siteId = id || config.tide?.site
   const { data: siteData } = useNuxtData(`site-${siteId}`)
+  const nuxt = useNuxtApp()
 
-  let sectionCacheTags
+  let sectionCacheTags: string | null = null
 
   if (!siteData.value) {
     const { data, error } = await useFetch('/api/tide/site', {
@@ -26,7 +27,7 @@ export const useTideSite = async (id?: number): Promise<TideSiteData> => {
 
     // Section.io cache tags must be set on the response header to invalidate the cache after a change in drupal
     if (sectionCacheTags) {
-      useMergeSectionTags(sectionCacheTags)
+      nuxt.runWithContext(() => useMergeSectionTags(sectionCacheTags))
     }
 
     return data.value

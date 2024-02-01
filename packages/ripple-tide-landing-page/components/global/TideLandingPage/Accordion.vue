38cf70847f65e47d98594a5e5b23b107e4fed7c1
David Featherston <david.featherstone@dpc.vic.gov.au>
2024-02-02 10:04:42 +1100

    feat(@dpc-sdp/ripple-tide-landing-page): add accordion descriptions

--- packages/ripple-tide-landing-page/components/global/TideLandingPage/Accordion.vue
+++ packages/ripple-tide-landing-page/components/global/TideLandingPage/Accordion.vue
@@ -2,11 +2,13 @@
 defineProps<{
   id: string
   title?: string
+  description?: string
   numbered: boolean
   items: Array<any>
 }>()
 </script>
 
 <template>
+  <RplContent v-if="description" :html="description" class="rpl-u-margin-b-4" />
   <RplAccordion :id="id" :items="items" :numbered="numbered" :title="title" />
 </template>

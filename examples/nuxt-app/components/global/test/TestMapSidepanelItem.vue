ee500604b25f49914a7d57a2e24ebe2271e73041
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2024-04-19 09:56:29 +1000

    test(@dpc-sdp/ripple-tide-search): cleaned up display of titles in map tests

--- examples/nuxt-app/components/global/test/TestMapSidepanelItem.vue
+++ examples/nuxt-app/components/global/test/TestMapSidepanelItem.vue
@@ -9,7 +9,7 @@ const props = defineProps<Props>()
 </script>
 
 <template>
-  <RplMapSidePanelItem class="tide-search-result" :title="result.test_title">
+  <RplMapSidePanelItem class="tide-search-result" :title="result.test_title[0]">
     <p class="rpl-type-p-small">{{ result.description }}</p>
   </RplMapSidePanelItem>
 </template>

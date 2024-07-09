fed98ed317ad26f2fd43b0a96c6aa5f9bbafe7bf
David Featherston <david.featherstone@dpc.vic.gov.au>
2024-07-09 16:19:35 +1000

    feat(@dpc-sdp/ripple-tide-search): allow specifying the url text key

--- packages/ripple-tide-search/components/global/TideSearchListing/TideSearchListingTableUrl.vue
+++ packages/ripple-tide-search/components/global/TideSearchListing/TideSearchListingTableUrl.vue
@@ -1,5 +1,5 @@
 <template>
-  <RplTextLink v-if="value" :url="value">{{ value }}</RplTextLink>
+  <RplTextLink v-if="url" :url="url">{{ value }}</RplTextLink>
 </template>
 
 <script setup lang="ts">
@@ -12,8 +12,21 @@ interface Props {
 
 const props = defineProps<Props>()
 
-const value = computed(() => {
+const url = computed(() => {
   const result = getSearchResultValue(props.item, props.column.objectKey, true)
   return Array.isArray(result) ? result[0] : result
+})
+
+const value = computed(() => {
+  if (props.column?.objectTextKey) {
+    const result = getSearchResultValue(
+      props.item,
+      props.column.objectTextKey,
+      true
+    )
+    return Array.isArray(result) ? result[0] : result
+  }
+
+  return url.value
 })
 </script>

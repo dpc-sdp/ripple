1b64b60257e0cafd55f94dc651f786f4dc0b374b
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2025-01-14 12:44:03 +1100

    feat(@dpc-sdp/ripple-tide-webform): send basic auth for webform submit if provided

--- packages/ripple-tide-webform/nuxt.config.ts
+++ packages/ripple-tide-webform/nuxt.config.ts
@@ -3,6 +3,10 @@ import { defineNuxtConfig } from 'nuxt/config'
 export default defineNuxtConfig({
   runtimeConfig: {
     tide: {
+      webformSubmit: {
+        username: '',
+        password: ''
+      },
       captchaSecret: {
         // Placeholder for captcha id: secret mapping
       }

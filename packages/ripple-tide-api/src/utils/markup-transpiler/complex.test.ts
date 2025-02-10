0aba2a1be37d972e3ead214f824bb860cc9c2633
Jason Smith <jason.smith@dgs.vic.gov.au>
2025-02-10 15:34:44 +1100

    refactor: :recycle: use stubEnv

--- packages/ripple-tide-api/src/utils/markup-transpiler/complex.test.ts
+++ packages/ripple-tide-api/src/utils/markup-transpiler/complex.test.ts
@@ -1,9 +1,11 @@
-import { expect, describe, it } from 'vitest'
+import { expect, describe, it, vi } from 'vitest'
 import markupTranspiler from './index'
 import fixture from './complex.fixture.js'
 
-process.env.NUXT_PUBLIC_TIDE_BASE_URL =
+vi.stubEnv(
+  'NUXT_PUBLIC_TIDE_BASE_URL',
   'https://develop.content.reference.sdp.vic.gov.au/'
+)
 
 describe('ripple-tide-api/utils/markup-transpiler/cheerio', () => {
   it('runs plugins on complex markup', () => {

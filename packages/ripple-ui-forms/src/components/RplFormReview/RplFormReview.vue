a26b5f867dafb0420b739172f3941f97e875a4c1
David Featherston <david@lambry.com>
2025-03-13 12:16:06 +1100

    feat(@dpc-sdp/ripple-ui-forms): add extra review checks

--- packages/ripple-ui-forms/src/components/RplFormReview/RplFormReview.vue
+++ packages/ripple-ui-forms/src/components/RplFormReview/RplFormReview.vue
@@ -47,8 +47,10 @@ const shouldDisplayField = (field: any, values: any) => {
 }
 
 const getOptionLabel = (field: any, value: any) => {
+  if (!field.options || !Array.isArray(field.options)) return value
+
   return (
-    field.options?.find(
+    field.options.find(
       (option: { value: string; label: string }) => option.value === value
     )?.label || value
   )
@@ -81,6 +83,8 @@ const processSchemaItems = (
   schema: FormKitSchemaNode[],
   values: any
 ): FormReviewItemValue[] => {
+  if (!Array.isArray(schema)) return []
+
   return schema
     .filter((field: any) => shouldDisplayField(field, values))
     .flatMap((field: any) => {
@@ -102,12 +106,14 @@ const items = computed<FormReviewItem[]>(() => {
   if (form.multiStep) {
     values = values[form.stepsId] || values
 
-    const stepItems = form.schema.map((step) => ({
-      key: step.key,
-      step: step.key,
-      name: step.title,
-      items: processSchemaItems(step.schema, values[step.key])
-    }))
+    const stepItems = form.schema
+      .filter((i) => i.$step)
+      .map((step) => ({
+        key: step.key,
+        step: step.key,
+        name: step.title,
+        items: processSchemaItems(step.schema, values[step.key])
+      }))
 
     return props.omitFinalStep ? stepItems.slice(0, -1) : stepItems
   }

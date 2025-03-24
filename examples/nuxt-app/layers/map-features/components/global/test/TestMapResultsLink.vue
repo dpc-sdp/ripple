cb0cef86dfc2cc9294955af121427ef0ccb06a33
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2025-03-25 09:36:50 +1100

    test(@dpc-sdp/ripple-tide-search): fixed click_search_result test for maps

---
+++ examples/nuxt-app/layers/map-features/components/global/test/TestMapResultsLink.vue
@@ -0,0 +1,12 @@
+<template>
+  <TideSearchResultLink :url="item.url[0]" class="rpl-type-p">
+    {{ item.test_title[0] }}
+  </TideSearchResultLink>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  item: any
+}
+withDefaults(defineProps<Props>(), {})
+</script>

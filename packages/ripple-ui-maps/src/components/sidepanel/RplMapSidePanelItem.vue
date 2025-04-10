35da6c02c7ae7f1f30d2868c4f03f5780abf89bd
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2025-04-10 14:26:36 +1000

    refactor(@dpc-sdp/ripple-ui-maps): improved a11y of map sidepanel markup

--- packages/ripple-ui-maps/src/components/sidepanel/RplMapSidePanelItem.vue
+++ packages/ripple-ui-maps/src/components/sidepanel/RplMapSidePanelItem.vue
@@ -1,36 +1,42 @@
 <template>
   <component
     :is="el"
-    role="button"
-    tabIndex="0"
     :class="{
       'rpl-map-side-panel__item': true,
-      'rpl-map-side-panel__item--active': isActive,
-      'rpl-u-focusable-within': true
+      'rpl-map-side-panel__item--active': isActive
     }"
-    @click="handleClick"
   >
-    <div
-      v-if="$slots.meta"
-      class="rpl-map-side-panel__item-meta rpl-type-p-small"
+    <button
+      :aria-label="buttonLabel"
+      :class="{
+        'rpl-map-side-panel__item-inner': true,
+        'rpl-u-focusable-within': true
+      }"
+      @click="handleClick"
     >
-      <slot name="meta"></slot>
-    </div>
-    <div
-      class="rpl-map-side-panel__item-title rpl-type-p rpl-u-focusable-inline"
-    >
-      {{ title }}
-    </div>
-    <slot></slot>
-    <div
-      v-if="$slots.footer"
-      class="rpl-map-side-panel__item-footer rpl-type-p-small"
-    >
-      <slot name="footer"></slot>
-    </div>
+      <div
+        v-if="$slots.meta"
+        class="rpl-map-side-panel__item-meta rpl-type-p-small"
+      >
+        <slot name="meta"></slot>
+      </div>
+      <div
+        class="rpl-map-side-panel__item-title rpl-type-p rpl-u-focusable-inline"
+      >
+        {{ title }}
+      </div>
+      <slot></slot>
+      <div
+        v-if="$slots.footer"
+        class="rpl-map-side-panel__item-footer rpl-type-p-small"
+      >
+        <slot name="footer"></slot>
+      </div>
+    </button>
   </component>
 </template>
 <script setup lang="ts">
+import { computed } from 'vue'
 import { rplEventPayload, useRippleEvent } from '@dpc-sdp/ripple-ui-core'
 
 interface Props {
@@ -55,6 +61,10 @@ const { emitRplEvent } = useRippleEvent('rpl-map-side-panel', emit)
 const handleClick = () => {
   emitRplEvent('click', { action: 'click', value: props.data })
 }
+
+const buttonLabel = computed(() => {
+  return `View ${props.title} on map`
+})
 </script>
 
 <style src="./RplMapSidePanel.css"></style>

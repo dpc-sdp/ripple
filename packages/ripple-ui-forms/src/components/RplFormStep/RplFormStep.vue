c6ec0f01d709904c622a330a0c60b5d8a806a909
David Featherston <david@lambry.com>
2025-05-19 12:55:16 +1000

    style(@dpc-sdp/ripple-ui-forms): minor multistep css tweaks

--- packages/ripple-ui-forms/src/components/RplFormStep/RplFormStep.vue
+++ packages/ripple-ui-forms/src/components/RplFormStep/RplFormStep.vue
@@ -42,7 +42,7 @@
     <template #stepNext="{ handlers, node }">
       <RplButton
         v-if="!node.context.isLastStep"
-        class="rpl-form__step-next rpl-u-margin-t-6"
+        class="rpl-form__step-next"
         variant="filled"
         @click="handlers.next"
       >
@@ -82,7 +82,7 @@ interface Props {
 
 const props = withDefaults(defineProps<Props>(), {
   schema: undefined,
-  nextButton: 'Next',
+  nextButton: 'Continue',
   prevButton: 'Back',
   data: () => ({}),
   errors: () => []

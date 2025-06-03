6f939cd77430973166d29141720a192a45ed15eb
Jason Smith <jason.smith@dgs.vic.gov.au>
2025-06-03 10:18:07 +1000

    chore: :refactor: add link to issue, use import.meta.dev

--- examples/nuxt-app/server/routes/.well-known/appspecific/[...].ts
+++ examples/nuxt-app/server/routes/.well-known/appspecific/[...].ts
@@ -1,4 +1,6 @@
+// https://github.com/nuxt/nuxt/issues/31978#issuecomment-2900825080
+
 export default defineEventHandler(async (event) => {
-  if (useRuntimeConfig().public.nodeEnv !== 'production') return
+  if (import.meta.dev) return
   await proxyRequest(event, event.path)
 })

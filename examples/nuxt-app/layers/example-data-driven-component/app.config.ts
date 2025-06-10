f512122997716090809030421bc8152d10b9652b
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2025-06-10 15:14:04 +1000

    refactor(@dpc-sdp/ripple-tide-landing-page): removed useSearchUI from content collection

--- examples/nuxt-app/layers/example-data-driven-component/app.config.ts
+++ examples/nuxt-app/layers/example-data-driven-component/app.config.ts
@@ -1,8 +1,5 @@
 export default defineAppConfig({
   ripple: {
-    featureFlags: {
-      contentCollectionSearchConnector: 'elasticsearch'
-    },
     dataDrivenComponents: {
       // add key of field_data_driven_component and value of component name to render
       // eg: find_a_council_map: 'VicCouncilLookup'

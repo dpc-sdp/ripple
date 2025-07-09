4ae5fa69ff5f4e7549085bc82f532cb497f5b5b2
Jason Smith <jason.smith@dgs.vic.gov.au>
2025-07-09 12:50:13 +1000

    fix: :recycle: change abbrev pattern and remove comma

--- packages/ripple-ui-core/src/lib/helpers.ts
+++ packages/ripple-ui-core/src/lib/helpers.ts
@@ -1,4 +1,7 @@
-import { format, isValid } from 'date-fns'
+import { format, isValid, setDefaultOptions } from 'date-fns'
+import { enAU } from 'date-fns/locale'
+
+setDefaultOptions({ locale: enAU })
 
 export const distanceAsPercentage = (point: number, total: number): number => {
   if (point < 0) {
@@ -26,7 +29,11 @@ export const formatDate = (
   if (options?.dateStyle === 'medium') {
     tokens.push('d MMM yyyy, h:mm aaa')
   } else if (options?.dateStyle === 'full') {
-    tokens.push("EEEE, d MMMM yyyy 'at' h:mm aaa")
+    tokens.push('EEEE d MMMM yyyy')
+
+    if (options?.timeStyle === undefined || options?.timeStyle === 'short') {
+      tokens.push("'at' h:mm aaa")
+    }
   } else {
     if (options?.weekday === 'long') {
       tokens.push('EEEE')
@@ -64,11 +71,15 @@ export const formatDate = (
       input.toLocaleString('en', { timeZone: options.timeZone })
     )
     return format(date, tokens.join(' '))
+      .replace('Jun ', 'June ')
+      .replace('Jul ', 'July ')
   } else {
     const date = new Date(
       input.toLocaleString('en', { timeZone: 'Australia/Melbourne' })
     )
     return format(date, tokens.join(' '))
+      .replace('Jun ', 'June ')
+      .replace('Jul ', 'July ')
   }
 }
 

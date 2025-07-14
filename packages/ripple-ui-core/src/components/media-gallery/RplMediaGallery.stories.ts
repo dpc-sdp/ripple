b63848e7ca9c662ff7e9c04b68fe8015aa15f275
David Featherston <david.featherstone@dpc.vic.gov.au>
2025-07-15 09:15:15 +1000

    feat(@dpc-sdp/ripple-ui-core): add default background to media embed and gallery

--- packages/ripple-ui-core/src/components/media-gallery/RplMediaGallery.stories.ts
+++ packages/ripple-ui-core/src/components/media-gallery/RplMediaGallery.stories.ts
@@ -94,3 +94,74 @@ export const SingleItem: Story = {
     ]
   }
 }
+
+export const NoBackgroundGallery: Story = {
+  name: 'No background / Gallery',
+  args: {
+    id: 'media-gallery-3',
+    background: false,
+    items: [
+      {
+        alt: 'Alt text',
+        title: 'Media title',
+        caption: 'Optional media caption content section',
+        thumbnail: svgPlaceholder({
+          width: 1200,
+          height: 800,
+          bgColor: '#ccc',
+          fgColor: '#444'
+        }),
+        image: svgPlaceholder({
+          width: 2400,
+          height: 1440,
+          bgColor: '#ccc',
+          fgColor: '#444'
+        })
+      },
+      {
+        alt: 'Alt text',
+        title: 'Media title',
+        caption: 'Some more text...',
+        thumbnail: svgPlaceholder({
+          width: 1200,
+          height: 800,
+          bgColor: '#aaa',
+          fgColor: '#666'
+        }),
+        image: svgPlaceholder({
+          width: 2400,
+          height: 1440,
+          bgColor: '#aaa',
+          fgColor: '#666'
+        })
+      }
+    ]
+  }
+}
+
+export const NoBackgroundSingleItem: Story = {
+  name: 'No Background / Single item',
+  args: {
+    id: 'media-gallery-4',
+    background: false,
+    items: [
+      {
+        alt: 'Alt text',
+        title: 'Media title',
+        caption: 'Optional media caption content section',
+        thumbnail: svgPlaceholder({
+          width: 1200,
+          height: 800,
+          bgColor: '#ccc',
+          fgColor: '#444'
+        }),
+        image: svgPlaceholder({
+          width: 2400,
+          height: 1440,
+          bgColor: '#ccc',
+          fgColor: '#444'
+        })
+      }
+    ]
+  }
+}

34af20e1a157de131b442f73561a3c4452e76912
Jeffrey Dowdle <jeffrey.dowdle@dpc.vic.gov.au>
2025-10-07 08:47:26 +1100

    fix(@dpc-sdp/ripple-tide-landing-page): added missing year to news cards

--- packages/ripple-tide-landing-page/components/global/TideLandingPage/CardSharedMeta.vue
+++ packages/ripple-tide-landing-page/components/global/TideLandingPage/CardSharedMeta.vue
@@ -71,9 +71,11 @@ const formattedDate = computed(() => {
 })
 
 const formattedPublishDate = computed(() =>
-  new Intl.DateTimeFormat('en-AU', { month: 'long', day: 'numeric' }).format(
-    new Date(props.meta.publishDate)
-  )
+  new Intl.DateTimeFormat('en-AU', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  }).format(new Date(props.meta.publishDate))
 )
 
 const now = new Date()

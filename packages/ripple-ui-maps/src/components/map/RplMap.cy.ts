35cb1ce56ce05fd26376eb9af8c22d4ef52fcbd7
David Featherston <david.featherstone@dpc.vic.gov.au>
2026-01-19 10:00:21 +1100

    feat(@dpc-sdp/ripple-ui-maps): add centerActivePin prop

--- packages/ripple-ui-maps/src/components/map/RplMap.cy.ts
+++ packages/ripple-ui-maps/src/components/map/RplMap.cy.ts
@@ -1,16 +1,67 @@
-import RplMap from './RplMap.vue'
+import featureData from './__fixture__/features.json'
 
-const baseProps = {
-  id: '1234'
+const coords = {
+  vic: [16193060, -4383467],
+  horsham: [15829631, -4399691]
 }
+const pins = { horsham: [77, 295] }
 
 describe('RplMap', () => {
   it('mounts', () => {
-    cy.mount(RplMap, {
+    cy.mountMap({
       props: {
-        ...baseProps
+        features: featureData,
+        provider: 'vicmap',
+        projection: 'EPSG:3857'
       }
     })
-    cy.get('.rpl-map').should('exist')
+    cy.get('.rpl-map canvas').should('exist')
+  })
+
+  it('centers on active pin', () => {
+    cy.mountMap({
+      props: {
+        features: featureData,
+        provider: 'vicmap',
+        projection: 'EPSG:3857',
+        clusteringDistance: 0
+      }
+    })
+    cy.get('.rpl-map').as('map')
+
+    cy.get('@map').find('canvas').click(pins['horsham'][0], pins['horsham'][1])
+    cy.wait(1500)
+
+    cy.get('@map')
+      .should('have.attr', 'data-center')
+      .then((center) => {
+        const [x, y] = center.split(',').map(parseFloat)
+        expect(x).to.be.closeTo(coords['horsham'][0], 1)
+        expect(y).to.be.closeTo(coords['horsham'][1], 1)
+      })
+  })
+
+  it('can disable centering on active pin', () => {
+    cy.mountMap({
+      props: {
+        features: featureData,
+        provider: 'vicmap',
+        projection: 'EPSG:3857',
+        clusteringDistance: 0,
+        centerActivePin: false
+      }
+    })
+    cy.get('.rpl-map').as('map')
+
+    cy.get('@map').find('canvas').click(pins['horsham'][0], pins['horsham'][1])
+    cy.wait(1000)
+
+    cy.get('@map')
+      .should('have.attr', 'data-center')
+      .then((center) => {
+        const [x, y] = center.split(',').map(parseFloat)
+        expect(x).to.be.closeTo(coords['vic'][0], 1)
+        expect(y).to.be.closeTo(coords['vic'][1], 1)
+      })
   })
 })

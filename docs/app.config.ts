b64b05f5c57efae42e3442b5eb9a9a9d45c305ba
David Featherston <david@lambry.com>
2026-01-27 09:57:14 +1100

    chore: post split content updates (#1345)

--- docs/app.config.ts
+++ docs/app.config.ts
@@ -18,7 +18,7 @@ export default defineAppConfig({
   description:
     'Make your service consistent with vic.gov.au digital branding. Learn from the research and experience of other service teams and avoid repeating work that’s already been done.',
   socials: {
-    github: 'dpc-sdp/ripple-framework'
+    github: 'dpc-sdp/ripple'
   },
   storybookBaseUrl: '/storybook',
   hideModulesSection: false

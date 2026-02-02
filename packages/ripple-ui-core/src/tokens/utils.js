e6450777f508d5f14a5bc09c38d7378ebf8a6eb7
Jason Smith <jason.smith@dgs.vic.gov.au>
2026-02-03 10:04:25 +1100

    feat: ✨ update to node 24 [SD-1246] (#1349)

--- packages/ripple-ui-core/src/tokens/utils.js
+++
@@ -1,9 +0,0 @@
-import './utils/grid.ts';
-import './utils/margin.ts';
-import './utils/padding.ts';
-import './utils/size.ts';
-/**
- * We're taking advantage of StyleDictionary's output pipeline to render some
- * repetitive utils (without requiring tokens).
- */
-//# sourceMappingURL=utils.js.map

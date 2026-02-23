25645d7eb0d28459c270fefcfdc8b7ee986a6123
David Featherston <david@lambry.com>
2026-02-24 09:54:16 +1100

    chore: update storybook and related deps (#1355)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    include: ['packages/**/*.{test,spec}.ts'],
+    exclude: ['**/node_modules/**'],
     coverage: {
+      provider: 'v8',
       include: ['packages/**/*.ts'],
       exclude: [
         '**/types.ts',
         '**/*.d.ts',
         'packages/**/*.config.ts',
-        'packages/nuxt-ripple-cli/**/*',
-        'packages/ripple-storybook/**/*',
-        'packages/ripple-test-utils/**/*'
+        'packages/ripple-storybook/**/*'
       ]
     }
   }

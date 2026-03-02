6d011d863e672940363595860d85238dc2c3b93a
David Featherston <david@lambry.com>
2026-03-03 09:49:24 +1100

    feat(@dpc-sdp/ripple-ui-forms): Adding RplFromFile component (#1354)
    * feat(@dpc-sdp/ripple-ui-form): file component
    
    * feat(@dpc-sdp/ripple-ui-form): update tests and stories
    
    * feat(@dpc-sdp/ripple-ui-form): add class, update placeholder
    
    * feat(@dpc-sdp/ripple-ui-form): update allowTypes
    
    * feat(@dpc-sdp/ripple-ui-form): update disabled states for file field
    
    * chore(@dpc-sdp/ripple-ui-form): update form story
    
    * chore(@dpc-sdp/ripple-ui-form): minor width update for file field
    
    * test(@dpc-sdp/ripple-ui-form): add extra onChange test
    
    * feat(@dpc-sdp/ripple-ui-form): design feedback

--- packages/ripple-ui-forms/src/components/StorybookInputFixture/StorybookInputFixture.vue
+++ packages/ripple-ui-forms/src/components/StorybookInputFixture/StorybookInputFixture.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import RplFormLabel from './../RplFormLabel/RplFormLabel.vue'
 import RplFormValidationError from './../RplFormValidationError/RplFormValidationError.vue'
-import { provide } from 'vue'
+import { computed, provide } from 'vue'
 
 interface Props {
   useFieldset?: boolean
@@ -9,14 +9,16 @@ interface Props {
   label?: string
   labelId?: string
   fieldId?: string
+  value?: string | string[]
 }
 
-withDefaults(defineProps<Props>(), {
+const props = withDefaults(defineProps<Props>(), {
   useFieldset: false,
   invalid: false,
   label: 'Label',
   labelId: null,
-  fieldId: null
+  fieldId: null,
+  value: null
 })
 
 const fakeError = { test: { value: 'Field is invalid' } }
@@ -24,6 +26,13 @@ const fakeError = { test: { value: 'Field is invalid' } }
 provide('form', {
   id: 'sb-form',
   name: 'SB Form'
+})
+
+const hasValue = computed(() => {
+  return (
+    (Array.isArray(props.value) && props.value.length) ||
+    (typeof props.value === 'string' && props.value)
+  )
 })
 </script>
 
@@ -53,4 +62,13 @@ provide('form', {
       </div>
     </div>
   </form>
+  <div v-if="hasValue">
+    <div class="rpl-type-p rpl-u-margin-t-8">
+      <h4 class="rpl-type-h4">Output value</h4>
+      <p v-if="typeof value === 'string'">{{ value }}</p>
+      <ul v-else>
+        <li v-for="(item, index) in value" :key="index">{{ item }}</li>
+      </ul>
+    </div>
+  </div>
 </template>

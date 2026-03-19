3a5483e90a2a896252a63773b3b688c9778b4b27
David Featherston <david@lambry.com>
2026-03-19 13:52:06 +1100

    chore: misc repo updates (#1365)
    * chore(@dpc-sdp/ripple-ui-maps): fix lint warning
    
    * chore: file and package cleanup
    
    * test: remove .only in tests and add lint check for .only
    
    * ci: run alpha release on workflow dispatch and only publish to chromatic on main and PRs
    
    * revert: chromatic publish condition

--- packages/eslint-config-ripple/index.js
+++ packages/eslint-config-ripple/index.js
@@ -42,6 +42,20 @@ module.exports = {
         'vue/multiline-html-element-content-newline': 'off',
         'vue/singleline-html-element-content-newline': 'off'
       }
+    },
+    {
+      files: ['*.test.{ts,js}', '*.cy.{ts,js}'],
+      rules: {
+        'no-restricted-syntax': [
+          'error',
+          {
+            selector:
+              ':matches(MemberExpression[object.name="it"], MemberExpression[object.name="test"], MemberExpression[object.name="describe"])[property.name="only"]',
+            message:
+              'Do not commit .only in test files, this prevents other tests from running.'
+          }
+        ]
+      }
     }
   ]
 }

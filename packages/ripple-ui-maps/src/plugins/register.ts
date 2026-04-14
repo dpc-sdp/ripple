c15e2f55c92bde0411c03de5aa5de85adf096916
Jeffrey Dowdle <jeffrey.dowdle@dgs.vic.gov.au>
2026-04-15 09:19:19 +1000

    chore: reverts vue3 update

--- packages/ripple-ui-maps/src/plugins/register.ts
+++ packages/ripple-ui-maps/src/plugins/register.ts
@@ -1,5 +1,5 @@
 import OpenLayersMap from 'vue3-openlayers'
-import 'vue3-openlayers/vue3-openlayers.css' // vue3-openlayers version < 1.0.0-*
+import 'vue3-openlayers/styles.css' // vue3-openlayers version < 1.0.0-*
 
 export default function registerRplMapsPlugin(vueApp, config) {
   vueApp.use(OpenLayersMap, {

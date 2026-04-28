0dca37227c2d4b3bf8ab50af939bd2ab4425c294
David Featherston <david@lambry.com>
2026-04-28 10:07:59 +1000

    feat(@dpc-sdp/ripple-ui-core): search result promo data (#1378)
    * feat(@dpc-sdp/ripple-ui-core): update search result meta
    
    * feat(@dpc-sdp/ripple-ui-core): add lower slot support for promo cards
    
    * feat(@dpc-sdp/ripple-ui-core): add support for contentBottom nav card slot

--- packages/ripple-ui-core/src/components/card/RplNavCard.stories.ts
+++ packages/ripple-ui-core/src/components/card/RplNavCard.stories.ts
@@ -8,9 +8,50 @@ type ExtendedNavCard = Partial<typeof RplNavCard> & {
   number: 1 | 2 | 4 | 8
 }
 
+const Template = (args: any) => ({
+  components: { RplNavCard, RplIcon, RplTag },
+  setup() {
+    const src = svgPlaceholder({
+      width: 1200,
+      height: 800,
+      bgColor: '#e76',
+      fgColor: '#a31'
+    })
+    const img =
+      args.graphicElement === 'Image inset' ||
+      args.graphicElement === 'Heading highlighted'
+        ? { src, alt: 'Alt text' }
+        : null
+    return { args, img }
+  },
+  template: `
+    <ul class="rpl-storybook__page rpl-grid">
+      <RplNavCard
+        v-for="index of args.number"
+        :key="index"
+        el="li"
+        v-bind="args"
+        class="rpl-col-12"
+        :image="img"
+        :highlight="args.graphicElement === 'Heading highlighted'"
+        :inset="args.graphicElement === 'Image inset'"
+      >
+        <template #meta>
+          ${args.meta}
+        </template>
+        ${args.default}
+        <template v-if="args.contentBottom" #contentBottom>
+          ${args.contentBottom}
+        </template>
+      </RplNavCard>
+    </ul>
+  `
+})
+
 export default {
   title: 'Core/Navigation/Card',
   component: RplNavCard,
+  render: Template,
   argTypes: {
     number: {
       control: { type: 'select' },
@@ -33,12 +74,7 @@ export default {
         '<RplTag variant="neutral" label="Grant"></RplTag><span class="rpl-card__status"><RplIcon class="rpl-icon--colour-error" name="icon-cancel-circle-filled"></RplIcon><span>Closed</span></span>'
       ]
     }
-  }
-} satisfies Meta<ExtendedNavCard>
-
-type Story = StoryObj<ExtendedNavCard>
-
-export const Nav: Story = {
+  },
   args: {
     number: 1,
     graphicElement: 'Image inset',
@@ -47,41 +83,17 @@ export const Nav: Story = {
     title: 'Small Business Ventilation Program',
     default:
       '<p>Funding for public-facing small businesses to purchase equipment and upgrades to improve ventilation and reduce the spread of COVID-19 and boost customer confidence.</p>'
-  },
-  render: (args) => ({
-    components: { RplNavCard, RplIcon, RplTag },
-    setup() {
-      const src = svgPlaceholder({
-        width: 1200,
-        height: 800,
-        bgColor: '#e76',
-        fgColor: '#a31'
-      })
-      const img =
-        args.graphicElement === 'Image inset' ||
-        args.graphicElement === 'Heading highlighted'
-          ? { src, alt: 'Alt text' }
-          : null
-      return { args, img }
-    },
-    template: `
-      <ul class="rpl-storybook__page rpl-grid">
-        <RplNavCard
-          v-for="index of args.number"
-          :key="index"
-          el="li"
-          v-bind="args"
-          class="rpl-col-12"
-          :image="img"
-          :highlight="args.graphicElement === 'Heading highlighted'"
-          :inset="args.graphicElement === 'Image inset'"
-        >
-          <template #meta>
-            ${args.meta}
-          </template>
-          ${args.default}
-        </RplNavCard>
-      </ul>
-    `
-  })
+  }
+} satisfies Meta<ExtendedNavCard>
+
+type Story = StoryObj<ExtendedNavCard>
+
+export const Nav: Story = {}
+
+export const NavWithHeadingHighlighted: Story = {
+  name: 'Nav/With content bottom',
+  args: {
+    contentBottom:
+      '<span class="rpl-card__icon-text"><RplIcon name="icon-pin" colour="default" /> Melbourne</span>'
+  }
 }

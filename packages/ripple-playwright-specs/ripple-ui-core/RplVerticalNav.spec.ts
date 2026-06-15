036e192a9c2a2f284c3f2d6d64999a56ccb0b36d
Jason Smith <jason.smith@dgs.vic.gov.au>
2026-06-15 11:16:06 +1000

    feat: :robot: complete migration

--- packages/ripple-playwright-specs/ripple-ui-core/RplVerticalNav.spec.ts
+++ packages/ripple-playwright-specs/ripple-ui-core/RplVerticalNav.spec.ts
@@ -45,14 +45,9 @@ test.describe(() => {
       props: { ...props, items: verticalNavExample3, toggleLevels: 3 }
     })
 
-    const item = component
-      .locator(
-        ':has(.rpl-vertical-nav__list-item:has-text("NESTED_ACTIVE_TEST"))'
-      )
-      .first()
-    await expect(item).toBeVisible()
-    const toggle = item.locator('.rpl-vertical-nav__toggle').first()
     const link = component.getByRole('link', { name: 'NESTED_ACTIVE_TEST' })
+    const toggle = component.getByLabel('Toggle NESTED_ACTIVE_TEST menu')
+    await expect(link).toBeVisible()
 
     await expect(link).toContainClass('rpl-vertical-nav__item--active')
     await expect(toggle).toHaveAttribute('aria-expanded', 'true')
@@ -68,14 +63,12 @@ test.describe(() => {
       props: { ...props, items: verticalNavExample3, toggleLevels: 2 }
     })
 
-    const item = component
-      .locator(
-        ':has(.rpl-vertical-nav__list-item:has-text("NESTED_ACTIVE_TEST"))'
-      )
-      .first()
-    await expect(item).toBeVisible()
-    const toggle = item.locator('.rpl-vertical-nav__toggle').first()
     const link = component.getByRole('link', { name: 'NESTED_ACTIVE_TEST' })
+    const item = link.locator('..')
+    await expect(item).toBeVisible()
+    const toggle = item.locator(
+      '.rpl-vertical-nav__header > .rpl-vertical-nav__toggle'
+    )
 
     await expect(link).toContainClass('rpl-vertical-nav__item--active')
     await expect(toggle).not.toBeAttached()
@@ -91,95 +84,91 @@ test.describe(() => {
       props: { ...props, items: verticalNavExample3, toggleLevels: 1 }
     })
 
-    const item = component
-      .locator(
-        ':has(.rpl-vertical-nav__list-item:has-text("NESTED_ACTIVE_TEST"))'
-      )
-      .first()
-    await expect(item).toBeVisible()
-    const toggle = item.getByLabel('Toggle First Level menu') //item.locator('.rpl-vertical-nav__toggle').first()
     const link = component.getByRole('link', { name: 'NESTED_ACTIVE_TEST' })
+    const item = link.locator('..')
+    await expect(item).toBeVisible()
+    const toggle = item.locator(
+      '.rpl-vertical-nav__header > .rpl-vertical-nav__toggle'
+    )
 
     await expect(link).toContainClass('rpl-vertical-nav__item--active')
     await expect(toggle).not.toBeAttached()
     await expect(
       component.locator('.rpl-vertical-nav__list--level-5')
     ).toBeVisible()
-
-    // cy.get('.rpl-vertical-nav__list-item')
-    //   .contains('NESTED_ACTIVE_TEST')
-    //   .parent()
-    //   .as('item')
-    // cy.get('@item').should('be.visible')
-    // cy.get('@item').find('.rpl-vertical-nav__toggle').should('not.exist')
-    // cy.get('@item').find('.rpl-vertical-nav__item').as('link')
-
-    // cy.get('@link').should('have.class', 'rpl-vertical-nav__item--active')
-
-    // cy.get('.rpl-vertical-nav__list--level-5').should('be.visible')
   })
 
-  // it('a deeply nested item is highlighted when active, and the parent items are expanded (toggleLevels: 0)', () => {
-  //   cy.mount(RplVerticalNav as any, {
-  //     props: { ...props, items: verticalNavExample3, toggleLevels: 0 }
-  //   })
+  test('a deeply nested item is highlighted when active, and the parent items are expanded (toggleLevels: 0)', async ({
+    mount
+  }) => {
+    const component = await mount(RplVerticalNav, {
+      props: { ...props, items: verticalNavExample3, toggleLevels: 0 }
+    })
 
-  //   cy.get('.rpl-vertical-nav__list-item')
-  //     .contains('NESTED_ACTIVE_TEST')
-  //     .parent()
-  //     .as('item')
-  //   cy.get('@item').find('.rpl-vertical-nav__toggle').should('not.exist')
-  //   cy.get('@item').find('.rpl-vertical-nav__item').as('link')
+    const link = component.getByRole('link', { name: 'NESTED_ACTIVE_TEST' })
+    const item = link.locator('..')
+    const toggle = item.locator(
+      '.rpl-vertical-nav__header > .rpl-vertical-nav__toggle'
+    )
 
-  //   cy.get('@link').should('have.class', 'rpl-vertical-nav__item--active')
-  // })
+    await expect(toggle).not.toBeAttached()
+    await expect(link).toContainClass('rpl-vertical-nav__item--active')
+  })
 
-  // it('toggles the display of top level nav items', () => {
-  //   cy.mount(RplVerticalNav as any, {
-  //     props: { ...props, items: verticalNavExample2 }
-  //   })
+  test('toggles the display of top level nav items', async ({ mount }) => {
+    const component = await mount(RplVerticalNav, {
+      props: { ...props, items: verticalNavExample2 }
+    })
 
-  //   cy.get('.rpl-vertical-nav__list-item').first().as('item')
+    const item = component.locator('.rpl-vertical-nav__list-item').first()
 
-  //   cy.get('@item').find('.rpl-expandable').should('be.hidden')
+    await expect(item.locator('.rpl-expandable')).toBeHidden()
 
-  //   cy.get('@item').find('.rpl-vertical-nav__toggle').click()
-  //   cy.get('@item').find('.rpl-expandable').should('be.visible')
+    await item.locator('.rpl-vertical-nav__toggle').click()
+    await expect(item.locator('.rpl-expandable')).toBeVisible()
 
-  //   cy.get('@item')
-  //     .find('.rpl-vertical-nav__list--level-5')
-  //     .should('be.visible')
-  // })
+    await expect(item.locator('.rpl-vertical-nav__list--level-5')).toBeVisible()
+  })
 
-  // it('toggles the display of nav items to 3 levels', () => {
-  //   cy.mount(RplVerticalNav as any, {
-  //     props: { ...props, items: verticalNavExample2, toggleLevels: 3 }
-  //   })
+  test('toggles the display of nav items to 3 levels', async ({ mount }) => {
+    const component = await mount(RplVerticalNav, {
+      props: { ...props, items: verticalNavExample2, toggleLevels: 3 }
+    })
 
-  //   cy.get('.rpl-vertical-nav__list-item').first().as('item')
+    const item = component.locator('.rpl-vertical-nav__list-item').first()
 
-  //   cy.get('@item').within(() => {
-  //     cy.get('[aria-label="Toggle Second level menu"]').should('be.hidden')
-  //     cy.get('[aria-label="Toggle First level menu"]').click()
-  //     cy.get('[aria-label="Toggle Second level menu"]').should('be.visible')
-  //     cy.get(
-  //       '[aria-label="Toggle Third level link with some text that will need to wrap menu"]'
-  //     ).should('be.hidden')
-  //     cy.get('[aria-label="Toggle Second level menu"]').click()
-  //     cy.get(
-  //       '[aria-label="Toggle Third level link with some text that will need to wrap menu"]'
-  //     ).should('be.visible')
-  //     cy.get(
-  //       '[aria-label="Toggle Third level link with some text that will need to wrap menu"]'
-  //     ).click()
-  //     cy.get(
-  //       '.rpl-vertical-nav__list--level-4, .rpl-vertical-nav__list--level-5'
-  //     ).should('be.visible')
+    await expect(item.getByLabel('Toggle Second level menu')).toBeHidden()
 
-  //     cy.get('[aria-label="Toggle First level menu"]').click()
-  //     cy.get('@item')
-  //       .find('.rpl-vertical-nav__list--level-2')
-  //       .should('be.hidden')
-  //   })
-  // })
+    await item.getByLabel('Toggle First level menu').click()
+    await expect(item.getByLabel('Toggle Second level menu')).toBeVisible()
+
+    await expect(
+      item.getByLabel(
+        'Toggle Third level link with some text that will need to wrap menu'
+      )
+    ).toBeHidden()
+
+    await item.getByLabel('Toggle Second level menu').click()
+    await expect(
+      item.getByLabel(
+        'Toggle Third level link with some text that will need to wrap menu'
+      )
+    ).toBeVisible()
+
+    await item
+      .getByLabel(
+        'Toggle Third level link with some text that will need to wrap menu'
+      )
+      .click()
+    await expect(
+      item
+        .locator(
+          '.rpl-vertical-nav__list--level-4, .rpl-vertical-nav__list--level-5'
+        )
+        .first()
+    ).toBeVisible()
+
+    await item.getByLabel('Toggle First level menu').click()
+    await expect(item.locator('.rpl-expandable').first()).toBeHidden()
+  })
 })

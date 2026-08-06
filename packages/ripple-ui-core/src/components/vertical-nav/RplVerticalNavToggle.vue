78badb52b10a32281ece5095eb6d5520b84ee423
David Featherston <david@lambry.com>
2026-08-06 11:05:35 +1000

    feat(@dpc-sdp/ripple-ui-core): allow mixing link/toggles with just toggles (#1427)

--- packages/ripple-ui-core/src/components/vertical-nav/RplVerticalNavToggle.vue
+++ packages/ripple-ui-core/src/components/vertical-nav/RplVerticalNavToggle.vue
@@ -3,16 +3,27 @@ import RplIcon from '../icon/RplIcon.vue'
 
 interface Props {
   text: string
+  showText?: boolean
 }
-defineProps<Props>()
+
+withDefaults(defineProps<Props>(), {
+  showText: false
+})
 </script>
 
 <template>
   <button
     type="button"
     :aria-label="`Toggle ${text} menu`"
-    class="rpl-vertical-nav__toggle rpl-u-focusable-block"
+    :class="{
+      'rpl-vertical-nav__toggle': true,
+      'rpl-u-focusable-block': true,
+      'rpl-vertical-nav__item': showText
+    }"
   >
+    <span v-if="showText" class="rpl-vertical-nav__toggle-text">
+      {{ text }}
+    </span>
     <span class="rpl-vertical-nav__toggle-icon" aria-hidden="true">
       <RplIcon name="icon-chevron-down" size="xs"></RplIcon>
     </span>

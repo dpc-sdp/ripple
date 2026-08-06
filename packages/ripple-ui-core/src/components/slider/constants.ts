2664296027ae5914f404487af92827925d85ec21
David Featherston <david@lambry.com>
2026-08-06 11:06:17 +1000

    feat(@dpc-sdp/ripple-ui-core): allow customizing the gap between slides (#1426)

--- packages/ripple-ui-core/src/components/slider/constants.ts
+++ packages/ripple-ui-core/src/components/slider/constants.ts
@@ -1,3 +1,5 @@
 import { RplBreakpoints } from '../../lib/breakpoints'
 
 export type RplSlidesPerView = RplBreakpoints | number | undefined
+
+export type RplSlideGap = RplBreakpoints | number | undefined
